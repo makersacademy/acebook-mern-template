2d2fb86fbd93b502b97e51d0593cdb77fcde42d8
Thomas Allen <tomallen@192.168.1.149>
2022-10-31 15:55:08 +0000

    reveresed GET posts test, added timestamp to Post objects, passes

--- api/models/post.js
+++ api/models/post.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-  message: String
+  message: String,
+  date: { type: Date, default: Date.now }
 });
 
 const Post = mongoose.model("Post", PostSchema);
