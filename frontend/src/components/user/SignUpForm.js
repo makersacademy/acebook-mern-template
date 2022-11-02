2a1dda94853343df88a182da5c082e00e753bf1f
Tauqeer Shoaib <tauqeershoaib@gmail.com>
2022-11-02 10:34:58 +0000

    fixed signup

--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -41,7 +41,7 @@ const SignUpForm = ({ navigate }) => {
     return (
       <form onSubmit={handleSubmit}>
         <p>Sign up to Acebook</p>
-        <input placeholder="Name" id="name" type='text' value={ name } onChange={handleEmailChange} />
+        <input placeholder="Name" id="name" type='text' value={ name } onChange={handleNameChange} />
           <br />
           <input placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
           <br />
