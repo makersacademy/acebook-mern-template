c10bb6229a5e0f96aa520a52402fdbc229ee3266
Tauqeer Shoaib <tauqeershoaib@gmail.com>
2022-11-03 14:38:43 +0000

    API tests pass except posts

--- api/models/post.js
+++ api/models/post.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PostSchema = new mongoose.Schema({
+  name: String,
   message: { type: String }, 
   date: {
     type: Date,
--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -3,7 +3,7 @@ import './Post.css';
 
 const Post = ({post}) => {
   return(
-    <article id='singlePost' data-cy="post" key={ post._id }> {post.message}, {post.date} </article>
+    <article id='singlePost' data-cy="post" key={ post._id}> {post.message}, {post.date}</article>
   )
 }
 
