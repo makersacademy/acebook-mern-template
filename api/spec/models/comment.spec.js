58da208c26256f1355582bf7a5a2cf8617472f00
Dave T <105549233+Dmum303@users.noreply.github.com>
2022-11-03 12:04:23 +0000

    testing comment fields

--- api/spec/models/comment.spec.js
+++ api/spec/models/comment.spec.js
@@ -1,46 +1,45 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 
-require("../mongodb_helper");
-var Comment = require("../../models/comment");
-// Comment needs Post, but this would make it an 
+require('../mongodb_helper');
+var Comment = require('../../models/comment');
+// Comment needs Post, but this would make it an
 // Integration test
 // var Post = require("../../models/post");
 
+describe('Comment model', () => {
+  beforeEach((done) => {
+    // Changed to posts
+    mongoose.connection.collections.comments.drop(() => {
+      done();
+    });
+  });
 
-describe("Comment model", () => {
-    beforeEach((done) => {
-      mongoose.connection.collections.comments.drop(() => {
+  it('has a comment', () => {
+    var comment = new Comment({ commentText: 'some comment' });
+    expect(comment.commentText).toEqual('some comment');
+  });
+
+  it('can list all comments', (done) => {
+    Comment.find((err, comments) => {
+      expect(err).toBeNull();
+      expect(comments).toEqual([]);
+      done();
+    });
+  });
+
+  it('can save a comment', (done) => {
+    var comment = new Comment({ commentText: 'some comment' });
+
+    comment.save((err) => {
+      expect(err).toBeNull();
+
+      Comment.find((err, comments) => {
+        expect(err).toBeNull();
+        console.log(comments[0]);
+        console.log(comments[0].commentText);
+        expect(comments[0].commentText).toEqual('some comment');
         done();
       });
     });
-
-    it("has a comment", () => {
-        var comment = new Comment({ commentText: "some comment" });
-        expect(comment.commentText).toEqual("some comment");
-      });
-
-      it("can list all comments", (done) => {
-        Comment.find((err, comments) => {
-          expect(err).toBeNull();
-          expect(comments).toEqual([]);
-          done();
-        });
-      });
-
-      it("can save a comment", (done) => {
-        var comment = new Comment({ commentText: "some comment" });
-    
-        comment.save((err) => {
-          expect(err).toBeNull();
-    
-          Comment.find((err, comments) => {
-            expect(err).toBeNull();
-    
-            expect(comments[0]).toMatchObject({ message: "some comment" });
-            done();
-          });
-        });
-      });
-
-
+  });
 });
