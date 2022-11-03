f4b9084014cb06b1c5df1496c1a0f92e4ebd179f
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-11-03 11:09:47 +0000

    Add picture to Users Schema

--- api/models/user.js
+++ api/models/user.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-// const defaultImage = 'default_image.jpg';
+const defaultImage = 'default_image.jpg';
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  // picture: { type: String, default: defaultImage },
+  picture: { type: String, default: defaultImage },
   // friends: [{ type: Schema.Types.ObjectId, ref: 'Users' }],
   // posts: [{ type: Schema.Types.ObjectId, ref: 'Posts' }],
 }, { timestamps: true });
