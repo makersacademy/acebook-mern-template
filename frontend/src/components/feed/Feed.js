bff0f87a77301496e02d53e381c64db04ed097eb
Chris <caston88@gmail.com>
2022-11-03 12:41:12 +0000

    FE-Add feature with passing tests for post creation

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import Post from '../post/Post'
+import "./Feed.css";
+
 
 const Feed = ({ navigate }) => {
   const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
-    if(token) {
+    if (token) {
       fetch("/posts", {
         headers: {
           'Authorization': `Bearer ${token}`
@@ -19,33 +22,65 @@ const Feed = ({ navigate }) => {
           setPosts(data.posts);
         })
     }
-  }, [])
-    
+  }, [token])
+
+  const handleSubmitPost = async () => {
+    if (message === '') return
+    let response = await fetch('/posts', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify({ message: message }),
+    })
+
+    if (response.status !== 201) {
+      navigate('/posts')
+    } else {
+      let data = await response.json()
+      window.localStorage.setItem("token", data.token)
+     }
+  };
+
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
 
   const logout = () => {
     window.localStorage.removeItem("token")
     navigate('/login')
   }
-  
-    if(token) {
-      console.log("if token is yes!")
-      return(
-        <>
-          <h2>Posts</h2>
-            <button onClick={logout}>
-              Logout
-            </button>
-          <div id='feed' role="feed">
-              {posts.map(
-                (post) => ( <Post post={ post } key={ post._id } /> )
-              )}
-          </div>
-        </>
-      )
-    } else {
-      navigate('/signin')
-      console.log("esle if no token at end")
-    }
+
+  if (token) {
+    return (
+      <>
+        <h2>Posts</h2>
+        <button id='logout-button'onClick={logout}>
+          Logout
+        </button>
+        <div id="message-box">
+        <form onSubmit={handleSubmitPost}>
+          <label>
+            Post a message:
+            <textarea placeholder="Message" id="message" value={message} onChange={handleMessageChange} />
+          </label>
+          <input id='submit' type="submit" value="Submit" />
+        </form>
+        </div>
+        <div id='feed' role="feed">
+          {posts.map(
+            (post) => (<Post post={post} key={post._id} />)
+          )}
+        </div>
+        <div id="kyle">
+          <img src='https://i.postimg.cc/T5vGJyXj/kyle.png' border='0' alt='kyle'/>
+        </div>
+      </>
+    )
+  } else {
+    navigate('/login')
+  }
 }
 
 export default Feed;
