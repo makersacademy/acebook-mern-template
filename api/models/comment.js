b1e679427623b3bbfdc1881d5c67b197bf3eaff8
Sarita <saritaradia65@gmail.com>
2022-11-03 11:36:27 +0000

    Removed posts part from comments

--- api/models/comment.js
+++ api/models/comment.js
@@ -5,10 +5,11 @@ const mongoose = require("mongoose");
 
 const CommentSchema = new mongoose.Schema({
     commentText: { type: String, required: true }, 
-    username: { type: String }, 
-    post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    post_created: { type: Date, default: Date.now } 
+    username: { type: String }
+    // Post commented out for now to keep things simple 
+    // post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
+    // user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    // post_created: { type: Date, default: Date.now } 
 });
 
 const Comment = mongoose.model("Comment", CommentSchema);
