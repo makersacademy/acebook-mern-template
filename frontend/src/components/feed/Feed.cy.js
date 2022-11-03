f19c9cb0e9c232f4592f55dbac09c7c8bc35c5bb
Chris <caston88@gmail.com>
2022-11-03 12:39:13 +0000

    FE-Add test for creating a new post

--- frontend/src/components/feed/Feed.cy.js
+++ frontend/src/components/feed/Feed.cy.js
@@ -3,9 +3,9 @@ const navigate = () => { }
 
 describe("Feed", () => {
   it("Calls the /posts endpoint and lists all the posts", () => {
+    
     window.localStorage.setItem("token", "fakeToken")
-    cy.mount(<Feed navigate={navigate} />)
-
+    
     cy.intercept('GET', '/posts', (req) => {
       req.reply({
         statusCode: 200,
@@ -15,13 +15,29 @@ describe("Feed", () => {
             { _id: 2, message: "Hello again, world" }
           ]
         }
-      })
-    }).as("getPosts")
+      , times: 1}) 
+        }).as("getPosts") 
 
+
+    cy.mount(<Feed navigate={navigate} />)
+    
     cy.wait("@getPosts").then(() => {
       cy.get('[data-cy="post"]')
         .should('contain.text', "Hello, world")
         .and('contain.text', "Hello again, world")
     })
   })
+
+  it.only("calls the /posts endpoint to create a new post", () => {
+    window.localStorage.setItem("token", "fakeToken")
+    
+    cy.mount(<Feed navigate={navigate}/>)
+    cy.intercept('POST', '/posts', { message: "OK" , times: 1}).as("postRequest")
+    cy.get("#message").type("I wonder what an owlPaca eats...");
+    cy.get("#submit").click()
+    cy.wait('@postRequest').then( interception => {
+      expect(interception.response.body.message).to.eq("OK")
+    })
+  }) 
+
 })
