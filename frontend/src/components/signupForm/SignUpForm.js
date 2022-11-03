ba9259fc51f4c940b0f5dd8d994a856dc55414ff
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-11-03 19:45:20 +0000

    Create Signup Form component

---
+++ frontend/src/components/signupForm/SignUpForm.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import "./SignUpForm.css";
+
+const SignUpForm = ({ navigate }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    fetch("/users", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName,
+      }),
+    }).then((response) => {
+      if (response.status === 201) {
+        navigate("/login");
+      } else {
+        navigate("/signup");
+      }
+    });
+  };
+
+  return (
+        <div className="signup-form-container">
+          <form onSubmit={handleSubmit} id="signup" class="form">
+            <h1 class="form-title">Signup</h1>
+            <input
+              placeholder="Email"
+              id="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              placeholder="Password"
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input
+              placeholder="First Name"
+              id="first-name"
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <input
+              placeholder="Last Name"
+              id="last-name"
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+            <input id="submit" type="submit" value="Submit" />
+          </form>
+        </div>
+  );
+};
+
+export default SignUpForm;
