1d0d9084c637ea89b48c4a45596ad75c739abc82
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-11-03 11:08:02 +0000

    Add FLnames and timestamps to user schema

--- api/models/user.js
+++ api/models/user.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
 
+// const defaultImage = 'default_image.jpg';
+
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
-});
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  // picture: { type: String, default: defaultImage },
+  // friends: [{ type: Schema.Types.ObjectId, ref: 'Users' }],
+  // posts: [{ type: Schema.Types.ObjectId, ref: 'Posts' }],
+}, { timestamps: true });
 
 const User = mongoose.model("User", UserSchema);
 
