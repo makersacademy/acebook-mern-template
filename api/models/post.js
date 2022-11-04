ee6d12a31d71a997b2cdc1c92d97e9b07a9d518d
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-04 18:53:48 +0000

    Modify post model to include required message pattern, add required pattern in Feed as otherwise no response received

--- api/models/post.js
+++ api/models/post.js
@@ -1,12 +1,19 @@
-const mongoose = require("mongoose");
-
+const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
-  message: String,
   timestamp: Date,
   // image: Buffer,
+  message: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^[a-zA-Z0-9~!@#()`;\-':,.?| ]*$/.test(v);
+      },
+    },
+    required: [true, "You can't submit an empty message, you idiot!"],
+  },
 });
 
-const Post = mongoose.model("Post", PostSchema);
+const Post = mongoose.model('Post', PostSchema);
 
 module.exports = Post;
--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,46 +1,56 @@
 import React, { useEffect, useState } from 'react';
-import Post from '../post/Post'
-import "./Feed.css";
-
+import Post from '../post/Post';
+import './Feed.css';
 
 const Feed = ({ navigate }) => {
   const [posts, setPosts] = useState([]);
-  const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [token, setToken] = useState(window.localStorage.getItem('token'));
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    if (token) {
-      fetch("/posts", {
-        headers: {
-          'Authorization': `Bearer ${token}`
-        }
-      })
-        .then(response => response.json())
-        .then(async data => {
-          window.localStorage.setItem("token", data.token)
-          setToken(window.localStorage.getItem("token"))
-          setPosts(data.posts);
-        })
-    }
-  }, [token])
+    fetchPosts();
+  }, []);
 
-  const handleSubmitPost = async () => {
-    if (message === '') return
+  const fetchPosts = () => {
+    if (token) {
+      fetch('/posts', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => response.json())
+        .then(async (data) => {
+          window.localStorage.setItem('token', data.token);
+          setToken(window.localStorage.getItem('token'));
+          setPosts(data.posts);
+        });
+    }
+  };
+
+  const handleSubmitPost = async (event) => {
+    event.preventDefault();
+
+    if (message === '') return;
+    if (!message.match(/^[a-zA-Z0-9~!@#()`;\-':,.?| ]*$/)) return;
+
     let response = await fetch('/posts', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
+        Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify({ message: message }),
-    })
+    });
+    console.log('submit');
 
     if (response.status !== 201) {
-      navigate('/posts')
+      navigate('/posts');
     } else {
-      let data = await response.json()
-      window.localStorage.setItem("token", data.token)
-     }
+      let data = await response.json();
+      window.localStorage.setItem('token', data.token);
+      fetchPosts();
+      setMessage('');
+    }
   };
 
   const handleMessageChange = (event) => {
@@ -48,39 +58,56 @@ const Feed = ({ navigate }) => {
   };
 
   const logout = () => {
-    window.localStorage.removeItem("token")
-    navigate('/login')
-  }
+    window.localStorage.removeItem('token');
+    navigate('/login');
+  };
 
   if (token) {
     return (
       <>
-        <h2>Posts</h2>
-        <button id='logout-button'onClick={logout}>
-          Logout
-        </button>
-        <div id="message-box">
-        <form onSubmit={handleSubmitPost}>
-          <label>
-            Post a message:
-            <textarea placeholder="Message" id="message" value={message} onChange={handleMessageChange} />
-          </label>
-          <input id='submit' type="submit" value="Submit" />
-        </form>
-        </div>
-        <div id='feed' role="feed">
-          {posts.map(
-            (post) => (<Post post={post} key={post._id} />)
-          ).reverse()}
-        </div>
-        <div id="kyle">
-          <img src='https://i.postimg.cc/T5vGJyXj/kyle.png' border='0' alt='kyle'/>
+        <div id="post-body">
+          <h2 id="posts-heading">Posts</h2>
+          <button id="logout-button" onClick={logout}>
+            Just leave.
+          </button>
+          <div id="message-box">
+            <form onSubmit={handleSubmitPost}>
+              <label id="post-a-message-label">
+                Spew some shit that no one cares about:
+              </label>
+              <textarea
+                placeholder="Message"
+                id="message"
+                value={message}
+                onChange={handleMessageChange}
+              />
+              <div id="message-button-container">
+                <input
+                  className="message-button"
+                  id="submit"
+                  type="submit"
+                  value=":@"
+                />
+                <div id="image-buttons">
+                  <button className="message-button" id="choose-file-button">
+                    Choose file of your ugly child
+                  </button>
+                  <button className="message-button" id="upload-file-button">
+                    Upload photo of your food no one cares about
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div id="feed" role="feed">
+            {posts.map((post) => <Post post={post} key={post._id} />).reverse()}
+          </div>
         </div>
       </>
-    )
+    );
   } else {
-    navigate('/login')
+    navigate('/login');
   }
-}
+};
 
 export default Feed;
