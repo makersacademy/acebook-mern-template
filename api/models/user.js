8afc86b4a1f3a3d049bc45d71322a45f7919e18c
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-04 17:30:32 +0000

    Add regex validation to email, password and usersName

--- api/models/user.js
+++ api/models/user.js
@@ -1,11 +1,35 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  usersName: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v); // Validator for email format.
+      },
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^[a-zA-Z0-9]{4,25}$/.test(v); // ONLY ALPHANUMERIC, at least 4 characters, max 25 characters.
+      },
+    },
+  },
+  usersName: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^[a-z ,.'-]*$/i.test(v); // No special characters, empty string allowed
+      },
+      message: 'Are you trying to include some weird characters?',
+    },
+  },
 });
 
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
