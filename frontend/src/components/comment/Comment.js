27f322a0e43e3962bc0b1db5e903b7acaf649c9f
Chris <caston88@gmail.com>
2022-11-05 13:55:30 +0000

    Add NewComment componment

--- frontend/src/components/comment/Comment.js
+++ frontend/src/components/comment/Comment.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import NewComment from '../newComment/NewComment'
 import "./Comment.css";
 
 const Comment = ({}) => {
@@ -11,13 +12,8 @@ return (
       </div>
       <div class="comment">{"<example comment>"}</div>
     </div>
-
-    <div class="new-comment-container">
-      <form class="new-comment-form">
-        <input placeholder="Respond to this joke of a post..." class="new-comment" type="text" />
-        <input role='submit-button' class='submit-button' type="submit" value="Retaliate" />
-      </form>
-    </div>
+    <NewComment />
+    
   </div>
 )
 }
