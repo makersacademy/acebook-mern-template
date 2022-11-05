64e6f0bf34afda05ad9990b5beeebd66ee7da7eb
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-05 10:00:33 +0000

    Add final test for errorHandlerMessage, passing

--- frontend/src/components/errorHandling/errorHandler.cy.js
+++ frontend/src/components/errorHandling/errorHandler.cy.js
@@ -25,6 +25,16 @@ describe('errorHandlerMessage', () => {
     );
   });
 
+  it('returns an error message when message contains invalid characters', () => {
+    const result = errorHandlerMessage('Hello, my name i{s Kyle, and I hate ');
+    expect(result).to.deep.equal(
+      <div>
+        You have introduced some invalid special characters, good luck next
+        time, you clearly need it.
+      </div>
+    );
+  });
+
   it("doesn't return anything if the given string is valid", () => {
     const result = errorHandlerMessage(
       'Hello, my name is Kyle, and I hate ##@!!!???.,'
