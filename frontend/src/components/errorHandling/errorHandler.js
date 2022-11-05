32740d1d6b5378d345fde5765a579b745b49827f
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-05 09:52:01 +0000

    Modify errorHandlerMessage to pass second test

--- frontend/src/components/errorHandling/errorHandler.js
+++ frontend/src/components/errorHandling/errorHandler.js
@@ -7,6 +7,13 @@ const errorHandlerMessage = (message) => {
         You need to write some text if you want to express your hate, you idiot.
       </div>
     );
+  } else if (!message.match(/^[a-zA-Z0-9~!@#()`;\-':,.?| ]*$/)) {
+    return (
+      <div>
+        You have introduced some invalid special characters, good luck next
+        time, you clearly need it.
+      </div>
+    );
   }
 };
 
