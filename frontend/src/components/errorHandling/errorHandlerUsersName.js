94ee0f98e92141f2b03f976ea81b4d3fdaa1e0ea
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-06 14:27:23 +0000

    Modify errorHandlerUsersName to add id to div tag and pass tests

--- frontend/src/components/errorHandling/errorHandlerUsersName.js
+++ frontend/src/components/errorHandling/errorHandlerUsersName.js
@@ -3,7 +3,7 @@ import React from 'react';
 const errorHandlerUsersName = (usersName) => {
   if (!usersName.match(/^[a-z ,.'-]*$/i)) {
     return (
-      <div>
+      <div id="usersname-error-invalid">
         Did you introduce some weird and not-allowed-at-all character as a name?
       </div>
     );
