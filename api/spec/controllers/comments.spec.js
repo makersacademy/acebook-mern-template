ef073b42b2c1c07f7135395321d26ccbb874ba91
Dave T <105549233+Dmum303@users.noreply.github.com>
2022-11-07 10:42:08 +0000

    Post comments backend completed

--- api/spec/controllers/comments.spec.js
+++ api/spec/controllers/comments.spec.js
@@ -55,116 +55,104 @@ describe('/posts', () => {
       expect(posts2[0].comments[0].text).toEqual('hello world');
     });
 
-    //     test('creates a new post', async () => {
-    //       await request(app)
-    //         .post('/posts')
-    //         .set('Authorization', `Bearer ${token}`)
-    //         .send({ message: 'hello world', token: token });
-    //       let posts = await Post.find();
-    //       expect(posts.length).toEqual(1);
-    //       expect(posts[0].message).toEqual('hello world');
-    //     });
+    test('returns a new token', async () => {
+      let response = await request(app)
+        .post('/comments')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          id: '635fee24ff8189b02bbc8cf6',
+          message: 'hello world',
+          token: token,
+        });
+      let newPayload = JWT.decode(response.body.token, process.env.JWT_SECRET);
+      let originalPayload = JWT.decode(token, process.env.JWT_SECRET);
+      expect(newPayload.iat > originalPayload.iat).toEqual(true);
+    });
+  });
 
-    //     test('returns a new token', async () => {
-    //       let response = await request(app)
-    //         .post('/posts')
-    //         .set('Authorization', `Bearer ${token}`)
-    //         .send({ message: 'hello world', token: token });
-    //       let newPayload = JWT.decode(response.body.token, process.env.JWT_SECRET);
-    //       let originalPayload = JWT.decode(token, process.env.JWT_SECRET);
-    //       expect(newPayload.iat > originalPayload.iat).toEqual(true);
-    //     });
-    //   });
+  describe('POST, when token is missing', () => {
+    test('responds with a 401', async () => {
+      let response = await request(app).post('/comments').send({
+        id: '635fee24ff8189b02bbc8cf6',
+        message: 'hello world',
+      });
+      expect(response.status).toEqual(401);
+    });
+  });
 
-    //   describe('POST, when token is missing', () => {
-    //     test('responds with a 401', async () => {
-    //       let response = await request(app)
-    //         .post('/posts')
-    //         .send({ message: 'hello again world' });
-    //       expect(response.status).toEqual(401);
-    //     });
-
-    //     test('a post is not created', async () => {
-    //       await request(app).post('/posts').send({ message: 'hello again world' });
-    //       let posts = await Post.find();
-    //       expect(posts.length).toEqual(0);
-    //     });
-
-    //     test('a token is not returned', async () => {
-    //       let response = await request(app)
-    //         .post('/posts')
-    //         .send({ message: 'hello again world' });
-    //       expect(response.body.token).toEqual(undefined);
-    //     });
-    //   });
-
-    //   describe('GET, when token is present', () => {
-    //     test('returns every post in the collection', async () => {
-    //       let post1 = new Post({ message: 'howdy!' });
-    //       let post2 = new Post({ message: 'hola!' });
-    //       await post1.save();
-    //       await post2.save();
-    //       let response = await request(app)
-    //         .get('/posts')
-    //         .set('Authorization', `Bearer ${token}`)
-    //         .send({ token: token });
-    //       let messages = response.body.posts.map((post) => post.message);
-    //       expect(messages).toEqual(['howdy!', 'hola!']);
-    //     });
-
-    //     test('the response code is 200', async () => {
-    //       let post1 = new Post({ message: 'howdy!' });
-    //       let post2 = new Post({ message: 'hola!' });
-    //       await post1.save();
-    //       await post2.save();
-    //       let response = await request(app)
-    //         .get('/posts')
-    //         .set('Authorization', `Bearer ${token}`)
-    //         .send({ token: token });
-    //       expect(response.status).toEqual(200);
-    //     });
-
-    //     test('returns a new token', async () => {
-    //       let post1 = new Post({ message: 'howdy!' });
-    //       let post2 = new Post({ message: 'hola!' });
-    //       await post1.save();
-    //       await post2.save();
-    //       let response = await request(app)
-    //         .get('/posts')
-    //         .set('Authorization', `Bearer ${token}`)
-    //         .send({ token: token });
-    //       let newPayload = JWT.decode(response.body.token, process.env.JWT_SECRET);
-    //       let originalPayload = JWT.decode(token, process.env.JWT_SECRET);
-    //       expect(newPayload.iat > originalPayload.iat).toEqual(true);
-    //     });
-    //   });
-
-    //   describe('GET, when token is missing', () => {
-    //     test('returns no posts', async () => {
-    //       let post1 = new Post({ message: 'howdy!' });
-    //       let post2 = new Post({ message: 'hola!' });
-    //       await post1.save();
-    //       await post2.save();
-    //       let response = await request(app).get('/posts');
-    //       expect(response.body.posts).toEqual(undefined);
-    //     });
-
-    //     test('the response code is 401', async () => {
-    //       let post1 = new Post({ message: 'howdy!' });
-    //       let post2 = new Post({ message: 'hola!' });
-    //       await post1.save();
-    //       await post2.save();
-    //       let response = await request(app).get('/posts');
-    //       expect(response.status).toEqual(401);
-    //     });
-
-    //     test('does not return a new token', async () => {
-    //       let post1 = new Post({ message: 'howdy!' });
-    //       let post2 = new Post({ message: 'hola!' });
-    //       await post1.save();
-    //       await post2.save();
-    //       let response = await request(app).get('/posts');
-    //       expect(response.body.token).toEqual(undefined);
-    //     });
+  test('a token is not returned', async () => {
+    let response = await request(app).post('/comments').send({
+      id: '635fee24ff8189b02bbc8cf6',
+      message: 'hello world',
+    });
+    expect(response.body.token).toEqual(undefined);
   });
 });
+//   describe('GET, when token is present', () => {
+//     test('returns every post in the collection', async () => {
+//       let post1 = new Post({ message: 'howdy!' });
+//       let post2 = new Post({ message: 'hola!' });
+//       await post1.save();
+//       await post2.save();
+//       let response = await request(app)
+//         .get('/posts')
+//         .set('Authorization', `Bearer ${token}`)
+//         .send({ token: token });
+//       let messages = response.body.posts.map((post) => post.message);
+//       expect(messages).toEqual(['howdy!', 'hola!']);
+//     });
+
+//     test('the response code is 200', async () => {
+//       let post1 = new Post({ message: 'howdy!' });
+//       let post2 = new Post({ message: 'hola!' });
+//       await post1.save();
+//       await post2.save();
+//       let response = await request(app)
+//         .get('/posts')
+//         .set('Authorization', `Bearer ${token}`)
+//         .send({ token: token });
+//       expect(response.status).toEqual(200);
+//     });
+
+//     test('returns a new token', async () => {
+//       let post1 = new Post({ message: 'howdy!' });
+//       let post2 = new Post({ message: 'hola!' });
+//       await post1.save();
+//       await post2.save();
+//       let response = await request(app)
+//         .get('/posts')
+//         .set('Authorization', `Bearer ${token}`)
+//         .send({ token: token });
+//       let newPayload = JWT.decode(response.body.token, process.env.JWT_SECRET);
+//       let originalPayload = JWT.decode(token, process.env.JWT_SECRET);
+//       expect(newPayload.iat > originalPayload.iat).toEqual(true);
+//     });
+//   });
+
+//   describe('GET, when token is missing', () => {
+//     test('returns no posts', async () => {
+//       let post1 = new Post({ message: 'howdy!' });
+//       let post2 = new Post({ message: 'hola!' });
+//       await post1.save();
+//       await post2.save();
+//       let response = await request(app).get('/posts');
+//       expect(response.body.posts).toEqual(undefined);
+//     });
+
+//     test('the response code is 401', async () => {
+//       let post1 = new Post({ message: 'howdy!' });
+//       let post2 = new Post({ message: 'hola!' });
+//       await post1.save();
+//       await post2.save();
+//       let response = await request(app).get('/posts');
+//       expect(response.status).toEqual(401);
+//     });
+
+//     test('does not return a new token', async () => {
+//       let post1 = new Post({ message: 'howdy!' });
+//       let post2 = new Post({ message: 'hola!' });
+//       await post1.save();
+//       await post2.save();
+//       let response = await request(app).get('/posts');
+//       expect(response.body.token).toEqual(undefined);
+//     });
