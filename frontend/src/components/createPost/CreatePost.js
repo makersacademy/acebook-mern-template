168b6f942873027e031e1f10d878dfecd732fea7
JoeOsborne77 <joeosborne77@gmail.com>
2022-11-07 14:22:10 +0000

    Image Functions Added to Create Post Component

--- frontend/src/components/createPost/CreatePost.js
+++ frontend/src/components/createPost/CreatePost.js
@@ -1,52 +1,48 @@
-import React, { useState } from 'react';
-import errorHandlerMessage from '../errorHandling/errorHandlerMessage';
+import React, { useState, useEffect } from "react";
+import errorHandlerMessage from "../errorHandling/errorHandlerMessage";
 import "./CreatePost";
 import { storage } from "./firebase";
 import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
 import { v4 } from "uuid";
-
+import "./CreatePost.css";
 
 const CreatePost = ({ navigate, fetchPosts }) => {
-  const token = window.localStorage.getItem("token")
-  const [message, setMessage] = useState('');
+  const token = window.localStorage.getItem("token");
+  const [message, setMessage] = useState("");
   const [imageUpload, setImageUpload] = useState(null);
   const [imageList, setImageList] = useState([]);
   const imageListRef = ref(storage, "images/");
 
-// useEffect(() => { <<<<<<<<<<<<<<< not sure if needed
-//     fetchPosts();
-//   }, []);
-
   const handleSubmitPost = async (event) => {
     event.preventDefault();
-    if (imageUpload === "" && message === "") return
+    if (imageUpload === "" && message === "") return;
     if (!message.match(/^[a-zA-Z0-9~!@#()`;\-':,.?| ]*$/)) return;
-   
+
     UploadImage();
- 
-    let response = await fetch('/posts', {
-      method: 'post',
+
+    let response = await fetch("/posts", {
+      method: "post",
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify({ message: message, imageUrls: imageList }),
-    })
+    });
 
     if (response.status !== 201) {
-      navigate('/posts')
+      navigate("/posts");
     } else {
-      let data = await response.json()
-      window.localStorage.setItem("token", data.token)
-      setMessage("")
-      fetchPosts()
+      let data = await response.json();
+      window.localStorage.setItem("token", data.token);
+      setMessage("");
+      fetchPosts();
     }
   };
 
   const handleMessageChange = (event) => {
     setMessage(event.target.value);
   };
-  
+
   const UploadImage = () => {
     if (imageUpload == null) return;
     const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
@@ -67,45 +63,39 @@ const CreatePost = ({ navigate, fetchPosts }) => {
     });
   }, []);
 
-
   return (
     <>
       <form id="submit-post-form" onSubmit={handleSubmitPost}>
-        <label id='post-a-message-label'>
+        <label id="post-a-message-label">
           Spew some shit that no one cares about:
         </label>
-        <textarea placeholder="Message" id="message" value={message} onChange={handleMessageChange} />
+        <textarea
+          placeholder="Message"
+          id="message"
+          value={message}
+          onChange={handleMessageChange}
+        />
         <div id="message-button-container">
-          <input
-            class="message-button"
-            id='submit'
-            type="submit"
-            value=":@" />
-            <div id="ErrorMessageMessage">{errorHandlerMessage(message)}</div>
-          <input
-            type="file"
-            onChange={(event) => {
-              setImageUpload(event.target.files[0]);
-            }}
-          />
+          <input class="message-button" id="submit" type="submit" value=":@" />
+          <div id="ErrorMessageMessage">
+            {errorHandlerMessage(message)}
+          </div>{" "}
+        </div>
+        <input
+          type="file"
+          onChange={(event) => {
+            setImageUpload(event.target.files[0]);
+          }}
+        />
         <button onClick={UploadImage}>Upload Photo</button> <br></br>
         {imageList
           .map((url) => {
-            return <img src={url} />;
+            return <img src={url} class="center" />;
           })
-          .reverse()}{" "}                                   
-//           <div id="image-buttons">
-//             <button class="message-button" id='choose-file-button'>
-//               Choose file of your ugly child
-//             </button>
-//             <button class="message-button" id='upload-file-button'>
-//               Upload photo of your food no one cares about
-//             </button>
-//           </div>
-//         </div>
+          .reverse()}{" "}
       </form>
     </>
-  )
-}
+  );
+};
 
 export default CreatePost;
--- frontend/src/components/createPost/firebase.js
+++ frontend/src/components/createPost/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,5 +19,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 export const storage = getStorage(app);
