83b8b34c09a477189313d57f86856cb02ca34a05
Joe Osborne <107399738+JoeOsborne77@users.noreply.github.com>
2022-11-07 12:37:18 +0000

    Update CreatePost.js

--- frontend/src/components/createPost/CreatePost.js
+++ frontend/src/components/createPost/CreatePost.js
@@ -1,24 +1,36 @@
 import React, { useState } from 'react';
 import errorHandlerMessage from '../errorHandling/errorHandlerMessage';
 import "./CreatePost";
+import { storage } from "./firebase";
+import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+import { v4 } from "uuid";
+
 
 const CreatePost = ({ navigate, fetchPosts }) => {
   const token = window.localStorage.getItem("token")
   const [message, setMessage] = useState('');
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageList, setImageList] = useState([]);
+  const imageListRef = ref(storage, "images/");
 
+// useEffect(() => { <<<<<<<<<<<<<<< not sure if needed
+//     fetchPosts();
+//   }, []);
 
   const handleSubmitPost = async (event) => {
     event.preventDefault();
-    if (message === '') return;
+    if (imageUpload === "" && message === "") return
     if (!message.match(/^[a-zA-Z0-9~!@#()`;\-':,.?| ]*$/)) return;
-    
+   
+    UploadImage();
+ 
     let response = await fetch('/posts', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`
       },
-      body: JSON.stringify({ message: message }),
+      body: JSON.stringify({ message: message, imageUrls: imageList }),
     })
 
     if (response.status !== 201) {
@@ -34,6 +46,27 @@ const CreatePost = ({ navigate, fetchPosts }) => {
   const handleMessageChange = (event) => {
     setMessage(event.target.value);
   };
+  
+  const UploadImage = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageList((prev) => [...prev, url]);
+      });
+    });
+  };
+
+  useEffect(() => {
+    listAll(imageListRef).then((res) => {
+      res.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageList((prev) => [...prev, url]);
+        });
+      });
+    });
+  }, []);
+
 
   return (
     <>
@@ -49,15 +82,27 @@ const CreatePost = ({ navigate, fetchPosts }) => {
             type="submit"
             value=":@" />
             <div id="ErrorMessageMessage">{errorHandlerMessage(message)}</div>
-          <div id="image-buttons">
-            <button class="message-button" id='choose-file-button'>
-              Choose file of your ugly child
-            </button>
-            <button class="message-button" id='upload-file-button'>
-              Upload photo of your food no one cares about
-            </button>
-          </div>
-        </div>
+          <input
+            type="file"
+            onChange={(event) => {
+              setImageUpload(event.target.files[0]);
+            }}
+          />
+        <button onClick={UploadImage}>Upload Photo</button> <br></br>
+        {imageList
+          .map((url) => {
+            return <img src={url} />;
+          })
+          .reverse()}{" "}                                   
+//           <div id="image-buttons">
+//             <button class="message-button" id='choose-file-button'>
+//               Choose file of your ugly child
+//             </button>
+//             <button class="message-button" id='upload-file-button'>
+//               Upload photo of your food no one cares about
+//             </button>
+//           </div>
+//         </div>
       </form>
     </>
   )
