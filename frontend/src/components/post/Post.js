4461d867069a8a7783959a4bb3c7ef119d0b9b0f
Chris <caston88@gmail.com>
2022-11-08 14:46:52 +0000

    Fix post image display

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import Comments from '../comments/Comments'
-import Hates from '../hates/Hates'
+import React from "react";
+import Comments from "../comments/Comments";
+import Hates from "../hates/Hates";
 import "./Post.css";
 
 const Post = ({ post }) => {
   return (
     <article data-cy="post" key={post._id}>
       <div class="header-container">
-        <img class="profile-image" src='https://iili.io/mVK9G2.png' alt='kyle' />
+        <img
+          class="profile-image"
+          src="https://iili.io/mVK9G2.png"
+          alt="kyle"
+        />
         <div class="name-and-time-container">
           <div class="username">{post.author.usersName}</div>
           <div class="post-time">{"{ Post time }"}</div>
@@ -16,16 +20,16 @@ const Post = ({ post }) => {
 
       <div class="message-container">
         <div class="message">{post.message}</div>
-        </div>
-      
-      <div class="post-image-container">
-        <img class="post-image" src={post.imageURL} alt='kyle' />
       </div>
-    
+
+      <div class="post-image-container">
+        <img class="post-image" src={post.imageURL} alt="" />
+      </div>
+
       <Hates />
       <Comments />
     </article>
-  )
-}
+  );
+};
 
 export default Post;
