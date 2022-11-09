5bacf109e43f75c0de9adb07327ae5f8f0efef8a
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-11-09 13:16:21 +0000

    Remove duplicate friends

--- api/controllers/friends.js
+++ api/controllers/friends.js
@@ -6,14 +6,19 @@ const FriendsController = {
   AddFriend: async (req, res) => {
     let user = await User.findById(req.user_id)
     let friendName = req.body.friend.split(' ')
-    console.log(friendName);
+
     let potentialFriend = await User.findOne({firstName: friendName[0], lastName: friendName[1]});
-    console.log(potentialFriend + ' <= search result');
-    if (potentialFriend) {
-      console.log("potentialFriend _id: " + potentialFriend._id)
+    console.log(potentialFriend);
+
+    const token = await TokenGenerator.jsonwebtoken(req.user_id);
+
+    if (!potentialFriend || user.friends.includes(potentialFriend._id)) {
+      res.status(201).json({ message: 'Friend not found', token: token });
+    }
+    
+    else {
       user.friends.push(potentialFriend._id);
       user.save();
-      const token = await TokenGenerator.jsonwebtoken(req.user_id);
       res.status(201).json({ message: 'OK', token: token });
     }
   }
