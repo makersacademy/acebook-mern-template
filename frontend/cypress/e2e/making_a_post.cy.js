3074b5251b19f068c2de614ae4c6d199f84db3e1
William Lines <williamlines42@gmail.com>
2022-12-01 10:24:50 +0000

    fixed broken tests

---
+++ frontend/cypress/e2e/making_a_post.cy.js
@@ -0,0 +1,13 @@
+describe("making a post", () => {
+  before(() => {
+    cy.signup("user@email.com", "12345678");
+    
+  });
+
+  it("can log in and create a post", () => {
+    cy.visit("/login");
+    cy.get("#email").type("user@email.com");
+    cy.get("#password").type("12345678");
+    cy.get("#submit").click();
+  })
+});
