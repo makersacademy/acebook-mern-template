879419cec31e8dcbfbe656885ed2eacdb56b779d
Tim Buller <111061411+tbuller@users.noreply.github.com>
2022-12-02 11:36:43 +0100

    Homepage complete with testing in end to end

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,36 +1,13 @@
 import React, { useEffect, useState } from "react";
 
-const Home = ({navigate}) => {
-
-  const handleLogin = async (event) => {
-    event.preventDefault();
-
-    fetch( '/', {
-      method: 'get',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ message: "OK" })
-    })
-      .then(response => {
-        if(response.status === 201) {
-          navigate('./login')
-        } else {
-          navigate('./')
-        }
-      })
-  }
-
-
-
-  
+const Home = ({navigate}) => {    
   return (
     <>
     <div>
       <h1>Hello there!</h1>
     </div>
     <div>
-        <body onSubmit={handleLogin}>
+        <body>
             <p>Select below where you'd like to go</p>
             <a href="/login"><button>Login</button></a>
             <a href="/signup"><button>Sign up</button></a>
