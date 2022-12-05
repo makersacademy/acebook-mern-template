e23bc9347d8039fd81ed95d1f4f1e580d954023a
Tom <tomfyfe85@gmail.com>
2022-12-05 11:42:06 +0000

    collected relevant data from post request to /likes

--- api/controllers/likes.js
+++ api/controllers/likes.js
@@ -3,8 +3,11 @@ const User = require("../models/post");
 const LikesController = {
   Create: (req, res) => {
     const data = req.body
-    console.log(`post_id is ${data.post_id}`);
-    console.log(`user_id is `)
+    const post_id = data.post_id
+    const user_id = req.user_id
+    
+    console.log(`post_id is ${post_id}`);
+    console.log(`user_id is ${user_id}`);
   }
 }
 
