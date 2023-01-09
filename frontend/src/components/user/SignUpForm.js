8f9c8ff6aaa71db067048b0e781eed223bf0a9c2
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-01-09 17:14:59 +0000

    Fixed login logs

--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -37,7 +37,7 @@ const SignUpForm = ({ navigate }) => {
       <form onSubmit={handleSubmit}>
           <input placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
           <input placeholder="Password" id="password" type='password' value={ password } onChange={handlePasswordChange} />
-        <input id='submit' type="submit" value="Submit" />
+          <input id='submit' type="submit" value="Submit" />
       </form>
     );
 }
