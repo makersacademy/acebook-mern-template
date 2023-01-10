783fe0303407ce044b4f9d9610f770231829d42d
JudeA94 <ashworthjude@gmail.com>
2023-01-10 21:55:12 +0000

    home component tests complete

--- frontend/src/components/home/Home.cy.js
+++ frontend/src/components/home/Home.cy.js
@@ -1,2 +1,24 @@
+import Home from './Home';
 
+describe('Home', () => {
+  beforeEach(() => {
+    cy.mount(
+        <Home />
+    );
+  });
+  it('renders the correct text', () => {
+    cy.contains('Home');
+    cy.contains('Log in');
+    cy.contains('Sign up');
+  });
 
+  it('links to the login page', () => {
+    cy.get('#login').click();
+    cy.url().should('include', '/login');
+  });
+
+  it('links to the signup page', () => {
+    cy.get('#sign-up').click();
+    cy.url().should('include', '/signup');
+  });
+});
--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,15 +1,18 @@
 import { Link } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const Home = () => {
   return ( 
     <div className="homepage-contents">
       <h1>Home</h1>
+      <Router>
       <Link to="/login">
         <button id="login">Log in</button>
       </Link>
       <Link to="/signup">
         <button id="sign-up">Sign up</button>
       </Link>
+      </Router>
     </div>
    );
 }
