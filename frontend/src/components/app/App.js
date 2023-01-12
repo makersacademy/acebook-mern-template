5b217887c47f61dd236e2950a63bfd1a635dc352
Aimee <aimeehorwood@hotmail.com>
2023-01-12 12:04:10 +0000

    Adds functioning navbar depending on whether the user is logged in or not

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,31 +1,33 @@
-import './App.css';
-import LoginForm from '../auth/LoginForm'
-import SignUpForm from '../user/SignUpForm'
-import React, { useState } from 'react';
-import Feed from '../feed/Feed'
-import Home from '../home/Home'
-import Navbar from '../navbar/Navbar'
+import "./App.css";
+import LoginForm from "../auth/LoginForm";
+import SignUpForm from "../user/SignUpForm";
+import React, { useState } from "react";
+import Feed from "../feed/Feed";
+import Home from "../home/Home";
+import Navbar from "../navbar/Navbar";
 
-import {
-  useNavigate,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { useNavigate, Routes, Route } from "react-router-dom";
 
 const App = () => {
-    return (
-      <>
+  return (
+    <>
       <Navbar />
       <div className="container">
         <Routes>
-          <Route path='/' element={<Home navigate={ useNavigate() }/>}/>
-          <Route path='/posts'  element={<Feed navigate={ useNavigate() }/>}/>
-          <Route path='/login'  element={<LoginForm  navigate={ useNavigate() }/>}/>
-          <Route path='/signup' element={<SignUpForm navigate={ useNavigate() }/>}/>
+          <Route path="/" element={<Home navigate={useNavigate()} />} />
+          <Route path="/posts" element={<Feed navigate={useNavigate()} />} />
+          <Route
+            path="/login"
+            element={<LoginForm navigate={useNavigate()} />}
+          />
+          <Route
+            path="/signup"
+            element={<SignUpForm navigate={useNavigate()} />}
+          />
         </Routes>
-        </div>
-      </>
-    );
-}
+      </div>
+    </>
+  );
+};
 
 export default App;
--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -2,24 +2,45 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className= "links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/posts">Posts</Link>
-        </li>
-        <li>
-          <Link to="/profile">My Profile</Link>
-        </li>
-        <li>
-          <Link to="/logout">Logout</Link>
-        </li>
-      </div>
-    </nav>
-  );
+  const handleLogout = () => {
+    window.localStorage.removeItem("token");
+  };
+
+  if (window.localStorage.getItem("token")) {
+    return (
+      <nav className="navbar">
+        <div className="links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link to="/profile">My Profile</Link>
+          </li>
+          <li>
+            <Link to="/" onClick={handleLogout}>
+              Logout
+            </Link>
+          </li>
+        </div>
+      </nav>
+    );
+  } else {
+    return (
+      <nav className="navbar">
+        <div className="links">
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+        </div>
+      </nav>
+    );
+  }
 };
 
 export default Navbar;
