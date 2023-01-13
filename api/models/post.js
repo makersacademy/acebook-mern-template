2870574781afdefe3711fc3c06c1dd37fc58f129
JudeA94 <ashworthjude@gmail.com>
2023-01-13 10:22:38 +0000

    more sign up info

--- api/models/post.js
+++ api/models/post.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
   message: String,
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
   likes: Array,
-  comments: Array
-});
+  comments: Array,
+}, { timestamps: true });
 
 const Post = mongoose.model("Post", PostSchema);
 
--- api/models/user.js
+++ api/models/user.js
@@ -8,6 +8,7 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true},
   password: { type: String, required: true },
   aboutMe: {type: String},
+  friendRequests: { type: Array},
   friends: { type: Array }
 });
 
