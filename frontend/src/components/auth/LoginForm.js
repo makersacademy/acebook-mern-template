aac638841e19549d31b32d2be1080b534fb71af2
Graeme Paterson <ghpaterson@gmail.com>
2023-01-13 17:24:12 +0000

    link to signup and initial css styling

--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const LogInForm = ({ navigate }) => {
   const [email, setEmail] = useState("");
@@ -36,26 +37,54 @@ const LogInForm = ({ navigate }) => {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="Email"
-          id="email"
-          type="text"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        <input
-          placeholder="Password"
-          id="password"
-          type="password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        <input role="submit-button" id="submit" type="submit" value="Submit" />
-      </form>
-
+      <h1 className="text-3xl font-bold">Welcome to Acebook</h1>
+      {/* <div class="flex items-center justify-center h-screen w-full max-w-xs "> */}
+      <div class="bg-grey-lighter h-screen font-sans">
+        <div class="container mx-auto h-full flex justify-center items-center">
+          <form
+            class="bg-white shadow-md rounded px-32 pt-24 pb-32 mb-1"
+            onSubmit={handleSubmit}
+          >
+            <div class="mb-4">
+              <input
+                class="w-60 px-2 py-2"
+                placeholder="Email"
+                id="email"
+                type="text"
+                value={email}
+                onChange={handleEmailChange}
+              />
+            </div>
+            <div class="mb-6">
+              <input
+                class="w-60 px-2 py-2"
+                placeholder="Password"
+                id="password"
+                type="password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </div>
+            <div class="flex items-center justify-between mb-1">
+              <input
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
+                role="submit-button"
+                id="submit"
+                type="submit"
+                value="Submit"
+              />
+              <a
+                class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                href="/signup"
+                id="signup-link"
+                value="Signup here"
+              >
+                Signup here
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -1,45 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const SignUpForm = ({ navigate }) => {
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    fetch( '/users', {
-      method: 'post',
+    fetch("/users", {
+      method: "post",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email: email, password: password })
-    })
-      .then(response => {
-        if(response.status === 201) {
-          navigate('/login')
-        } else {
-          navigate('/signup')
-        }
-      })
-  }
+      body: JSON.stringify({ email: email, password: password }),
+    }).then((response) => {
+      if (response.status === 201) {
+        navigate("/login");
+      } else {
+        navigate("/signup");
+      }
+    });
+  };
 
   const handleEmailChange = (event) => {
-    setEmail(event.target.value)
-  }
+    setEmail(event.target.value);
+  };
 
   const handlePasswordChange = (event) => {
-    setPassword(event.target.value)
-  }
+    setPassword(event.target.value);
+  };
 
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Signup to AceBook</h1>
 
-    return (
       <form onSubmit={handleSubmit}>
-          <input placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
-          <input placeholder="Password" id="password" type='password' value={ password } onChange={handlePasswordChange} />
-          <input id='submit' type="submit" value="Submit" />
+        <input
+          placeholder="Email"
+          id="email"
+          type="text"
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <input
+          placeholder="Password"
+          id="password"
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <input id="submit" type="submit" value="Submit" />
+
+        <div>
+          <p>
+            Already a user? <a href="/login">Login</a>
+          </p>
+        </div>
       </form>
-    );
-}
+    </>
+  );
+};
 
 export default SignUpForm;
