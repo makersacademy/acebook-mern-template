f36c3e1cbf5ad988f3e6f7793221127ffc754bd1
williamjones <willj836@gmail.com>
2023-01-13 15:50:03 +0000

    formatted code

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,44 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import Post from '../post/Post'
-import "./Feed.css"
-import CreatePost from '../CreatePost/CreatePost';
+import React, { useEffect, useState } from "react";
+import Post from "../post/Post";
+import "./Feed.css";
+import CreatePost from "../CreatePost/CreatePost";
 
 const Feed = ({ navigate }) => {
   const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [updated, setUpdated] = useState(false);
 
   useEffect(() => {
-    if(token) {
+    if (token) {
       fetch("/posts", {
         headers: {
-          'Authorization': `Bearer ${token}`
-        }
+          Authorization: `Bearer ${token}`,
+        },
       })
-        .then(response => response.json())
-        .then(async data => {
-          window.localStorage.setItem("token", data.token)
-          setToken(window.localStorage.getItem("token"))
+        .then((response) => response.json())
+        .then(async (data) => {
+          window.localStorage.setItem("token", data.token);
+          setToken(window.localStorage.getItem("token"));
           setPosts(data.posts);
-        })
+          setUpdated(false);
+        });
     }
-  }, [posts, token])
-  
-    if(token) {
-      return(
-        <>
-          <CreatePost />
-          <h2>Posts</h2>
-          <div id='feed' role="feed">
-              {posts.map(
-                (post) => ( <Post post={ post } key={ post._id } /> )
-              )}
-          </div>
-        </>
-      )
-    } else {
-      navigate('/signin')
-    }
-}
+  }, [updated]);
+
+  if (token) {
+    return (
+      <>
+        <CreatePost setUpdated={setUpdated} />
+        <h2>Posts</h2>
+        <div id="feed" role="feed">
+          {posts.map((post) => (
+            <Post post={post} key={post._id} />
+          ))}
+        </div>
+      </>
+    );
+  } else {
+    navigate("/signin");
+  }
+};
 
 export default Feed;
-
