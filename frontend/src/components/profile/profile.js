4b046d71c814aef4bd4f0c28350887542b1a5835
Eliza Thomas <elizamthomas1994@gmail.com>
2023-01-13 16:04:46 +0000

    Added card element

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import profile_placeholder from './profile_placeholder.jpeg';
+import Card from '../Helpers/Card.js';
+import './profile.css';
 
 const Profile = () => {
 
 return(
   <div className='container'>
     <div>
-      <img src={profile_placeholder} style={{width: "50px", height: "50px"}} />
-      <h1>Username</h1>
+      <Card>
+        <img src={profile_placeholder} style={{width: "50px", height: "50px"}} />
+        <h1>Username</h1>
+      </Card>
     </div>
     <div>
       <button>Edit profile</button>
