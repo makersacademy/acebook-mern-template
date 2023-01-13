16924dfcc45585e7bac605194abac7724fa38d84
Nicola Hearn <nicolahearn@mastude5h3vq6l4.home>
2023-01-13 16:24:52 +0000

    adds posts to profile page

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Card from '../Helpers/Card'
 
 const Post = ({post}) => {
   return(
-    <article data-cy="post" key={ post._id }>{ post.message }</article>
+    <Card>
+      <article data-cy="post" key={ post._id }>{ post.message }</article>
+    </Card>
   )
 }
 
--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -2,21 +2,32 @@ import React from 'react'
 import profile_placeholder from './profile_placeholder.jpeg';
 import Card from '../Helpers/Card.js';
 import './profile.css';
+import Feed from '../feed/Feed'
 
 const Profile = () => {
 
 return(
-  <div className='container'>
-    <div>
-      <Card>
-        <img src={profile_placeholder} style={{width: "50px", height: "50px"}} />
-        <h1>Username</h1>
-      </Card>
+  <div>
+    <div className='container'>
+      <div>
+        <Card>
+          <img src={profile_placeholder} style={{width: "50px", height: "50px"}} />
+          <h1>Username</h1>
+        </Card>
+      </div>
+      <div>
+        <button>Edit profile</button>
+      </div>
     </div>
-    <div>
-      <button>Edit profile</button>
+    <div className='container'>
+      <div className='posts'>
+        <Card>
+          <Feed />
+        </Card>
+      </div>
     </div>
   </div>
+
 )
 
 }
