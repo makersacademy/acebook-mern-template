8c433ca3afe0bca8488203a8b898cafa2edb7588
williamjones <willj836@gmail.com>
2023-01-16 15:02:56 +0000

    model now includes array of user_ids that have liked the post + counter

--- api/models/post.js
+++ api/models/post.js
@@ -3,16 +3,19 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema(
   {
     message: {
-     type: String,
-     required: true 
+      type: String,
+      required: true,
     },
     author: {
-      // Expects to recieve a valid user ID from the User Schema 
-      type: mongoose.Schema.Types.ObjectId, ref: 'User',
-      required: true
-     },
+      // Expects to recieve a valid user ID from the User Schema
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    likeCount: { type: Number, default: 0 },
   },
-  { timestamps: true },
+  { timestamps: true }
 );
 
 const Post = mongoose.model("Post", PostSchema);
