3307721d5f2a82b9bc652cc1eb0d2f6346a1e007
JudeA94 <ashworthjude@gmail.com>
2023-01-16 15:15:36 +0000

    comments and likes routing

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -13,7 +13,6 @@ const PostsController = {
   },
   Create: (req, res) => {
     const post = new Post(req.body);
-    console.log(req.body);
     post.save(async (err) => {
       if (err) {
         throw err;
@@ -23,6 +22,26 @@ const PostsController = {
       res.status(201).json({ message: 'OK', token: token });
     });
   },
+  Update: async (req, res) => {
+    console.log(req.body)
+    post_id = req.params.id
+    console.log(post_id)
+    value = req.body.value
+    field = req.body.field
+    if (field === 'likes') {
+      update = await Post.findOneAndUpdate({_id: post_id},{$push: {likes: value}} )
+    } else if (field === 'comments') {
+      update = await Post.findOneAndUpdate({_id: post_id},{$push: {comments: value}} )
+    } else {
+      throw err;
+    }
+    const token = await TokenGenerator.jsonwebtoken(req.user_id)
+    res.status(200).json({ message: 'OK', token: token });
+  }
 };
 
 module.exports = PostsController;
+
+
+
+
