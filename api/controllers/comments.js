ade6639e446d1fc7132cc492478b783761c22365
Josh Carvell <j_carvell@hotmail.co.uk>
2023-01-16 14:36:27 +0000

    add comments controller file

---
+++ api/controllers/comments.js
@@ -0,0 +1,10 @@
+// Require the comment model
+const Comment = require('../models/comment')
+const TokenGenerator = require("../models/token_generator");
+
+const CommentsController = {
+
+  // Find all comments that are on a single post 
+
+  // Create a comment on a single post 
+}
