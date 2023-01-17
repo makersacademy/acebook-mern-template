5444ab906a5067c8494fb8a61bb5e64b354cbc40
Josh Carvell <j_carvell@hotmail.co.uk>
2023-01-17 15:56:59 +0000

    add create comment input field

--- frontend/src/components/CreateComment/CreateComment.js
+++ frontend/src/components/CreateComment/CreateComment.js
@@ -1,6 +1,18 @@
 const CreateComment = () => {
-  return (
-    
+  return ( 
+    <div className="form-container">
+      <form className="my-comment-form">
+        <input type="text"></input>
+      </form>
+    </div>
+    // Form
+      // Input field
+      // Starts as a text area
+      // size - one line, length of comment box (minus margin)
+      // Placeholder text: "Write a comment..."
+      // Input field value is changed as you type
+      // NO submit button, just enter: onKeyDown={handleKeyDown}
+        // (handleKeyDown then submits the comment when 'enter' is pressed)
   );
 }
  
--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Comment from "../comment/Comment";
+import CreateComment from "../CreateComment/CreateComment";
 import "./Post.css"
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
@@ -28,27 +29,31 @@ const Post = ({ post }) => {
   }
   
   return (
-    <div class="post-container">
-      <div class="post-card" data-cy="post" key={ post._id }>
-        <div class="card-header">
-          <div class="card-meta">
-            <h2 class="username">{ post.author.name }</h2>
-            <p class="timestamp">{formatDistanceToNow(new Date(post.createdAt), { addSuffix: true })}</p>
+    <div className="post-container">
+      <div className="post-card" data-cy="post" key={ post._id }>
+        <div className="card-header">
+          <div className="card-meta">
+            <h2 className="username">{ post.author.name }</h2>
+            <p className="timestamp">{formatDistanceToNow(new Date(post.createdAt), { addSuffix: true })}</p>
           </div>
         </div>
-        <p class="card-message">{ post.message }</p>
-        <div className="view-comments-container">
+        <p className="card-message">{ post.message }</p>
+        <div className="likes-and-comments-buttons-container">
+          <button className="comments-buttons" onClick={handleViewComments}>Add comment</button>
           {comments.length > 0 ?
-            <button className="view-comments-button" onClick={handleViewComments}>{ comments.length } comments</button> : 
-            <button className="view-comments-button">{ comments.length } comments</button>}
+            <button className="comments-buttons" onClick={handleViewComments}>{ comments.length } comments</button> : 
+            <button className="comments-buttons">{ comments.length } comments</button>}
         </div>
-      { comments && (viewComments === true) &&
-        <div id="comments-container">
-          {comments.map((comment) => (
-            <Comment comment={comment} key={comment._id} />
-          ))}
-        </div>
-      }
+        { comments && (viewComments === true) &&
+          <div id="comments-container">
+            <div className="add-comment-container">
+              < CreateComment/>
+            </div>
+            {comments.map((comment) => (
+              <Comment comment={comment} key={comment._id} />
+            ))}
+          </div>
+        }
       </div>
     </div>
   )
