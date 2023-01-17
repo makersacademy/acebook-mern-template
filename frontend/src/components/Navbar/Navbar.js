39d836eccb3236b6cf926636873d217799d824d6
mahaque <haquema@outlook.com>
2023-01-17 17:28:46 +0000

    fix create an account link button on login page

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,15 +1,27 @@
 import './Navbar.css';
-
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <header>
       <div className='container'>
         <div className='links'>
-          <a href='/posts'>Acebook</a>
+          <Link to='/posts'>
+            <button id="homebutton">Acebook</button>
+          </Link>
+          <Link to="/post">
+            <button id="Feed">View Feed</button>
+          </Link>
+          <Link to="/profile">
+            <button id="profile-page">My Profile</button>
+          </Link>
+          <Link to="/login">
+            <button id="logout">Logout</button>
+          </Link>
+          {/* <a href='/posts'>Acebook</a>
           <a href='/posts'>View Feed</a>
           <a href='/profile'>My Profile</a>
-          <a href='/login'>Logout</a>
+          <a href='/login'>Logout</a> */}
         </div>
       </div>
     </header>
