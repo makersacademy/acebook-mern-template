20e49ca8ef4667f8133f767b92dd82cd615b0952
Andy <ajscottlewis@gmail.com>
2023-01-18 15:55:35 +0000

    Make footer stick to bottom of screen, add footer to login and sign up page, style 404 page

--- frontend/src/components/NotFound/NotFound.js
+++ frontend/src/components/NotFound/NotFound.js
@@ -1,8 +1,13 @@
+import { Link } from 'react-router-dom';
 import "./NotFound.css";
 
 const NotFound = () => {
   return (
-    <div>404 error: page not found</div>
+    <div className="not-found-div">
+      <h1>404 error</h1>
+      <h2>The page you were looking for does not exist</h2>
+      <Link to="/">Click here to return to your news feed</Link>
+    </div>
   );
 }
  
