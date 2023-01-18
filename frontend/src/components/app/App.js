b61063426f8926b4786bd03c544c549976100e13
williamjones <willj836@gmail.com>
2023-01-18 12:25:41 +0000

    new route added for profile page
    also added ternary for if logged in and visit /login or /signup then redirected to /posts

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,41 +1,32 @@
 import './App.css';
 import LoginForm from '../auth/LoginForm'
 import SignUpForm from '../user/SignUpForm'
-import React, { useState } from 'react';
+import React from 'react';
 import Feed from '../feed/Feed'
 import Navbar from "../navbar/Navbar";
 import Footer from "../footer/footer";
-
+import Profile from "../profile/profile";
 import {
   useNavigate,
   Routes,
   Route,
   useLocation,
+  Navigate
 } from "react-router-dom";
-
-import { useEffect } from 'react';
 
 const App = () => {
   const navigate = useNavigate();
   const location = useLocation();
-
-  // below is beginnings of preventing
-  // access to /login and /signup when currently logged in
-  // will use useLocation
-
-  // useEffect(() => {
-  //   if (window.localStorage.getItem("token")) {
-  //     navigate('/posts');
-  //   }
-  // }, [navigate]);
-
+  const user = window.localStorage.getItem("user_id");
+  
   return (
     <>
       < Navbar navigate={ navigate }/>
         <Routes>
           <Route path='/posts'  element={<Feed navigate={ navigate }/>}/>
-          <Route path='/login'  element={<LoginForm  navigate={ navigate }/>}/>
-          <Route path='/signup' element={<SignUpForm navigate={ navigate }/>}/>
+          <Route path='/login'  element={!user ? <LoginForm  navigate={ navigate }/> : <Navigate to="/posts"></Navigate>}/>
+          <Route path='/signup' element={!user ? <SignUpForm navigate={ navigate }/> : <Navigate to="/posts"></Navigate>}/>
+          <Route path='/users/:user_id' element={<Profile navigate={ navigate }/>}/>
         </Routes>
         {location.pathname !== '/login' && location.pathname !== '/signup' && <Footer />}
     </>
