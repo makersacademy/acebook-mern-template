6ad20792da78eb428b1a10c8201c66475324a456
williamjones <willj836@gmail.com>
2023-01-18 12:24:30 +0000

    profile link added to navbar

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
-import './Navbar.css';
+import "./Navbar.css";
 
 const Navbar = ({ navigate }) => {
- const loggedIn = window.localStorage.getItem("token");
+  const loggedIn = window.localStorage.getItem("user_id");
 
   const handleLogout = () => {
     setTimeout(() => {
@@ -13,19 +13,22 @@ const Navbar = ({ navigate }) => {
       window.localStorage.removeItem("token");
       window.localStorage.removeItem("user_id");
     }
-    navigate('/login');
+    navigate("/login");
   };
-  
+
   return (
     <header>
       <div className="container">
-          <Link to={loggedIn ? '/posts' : '/login'}>
-            <h1 data-cy='h1'>Acebook</h1>
-         </Link>
-        <nav data-cy='nav'>
+        <Link to={loggedIn ? "/posts" : "/login"}>
+          <h1 data-cy="h1">Acebook</h1>
+        </Link>
+        <nav data-cy="nav">
           {loggedIn && (
             <div>
-              <button data-cy="logout" onClick={handleLogout}>Log out</button>
+              <Link to={`/users/${loggedIn}`}>Profile</Link>
+              <button data-cy="logout" onClick={handleLogout}>
+                Log out
+              </button>
             </div>
           )}
           {!loggedIn && (
