ba97a9678db0b6d4b071c946ade47fd54f1fc73f
Josh Carvell <j_carvell@hotmail.co.uk>
2023-01-18 18:44:15 +0000

    add upload widget component

---
+++ frontend/src/components/CreatePost/UploadWidget.js
@@ -0,0 +1,27 @@
+import { useEffect, useRef} from 'react'
+
+const UploadWidget = ({handleImageUpload} ) => {
+
+  const cloudinaryRef = useRef();
+  const widgetRef = useRef();
+  useEffect(() => {
+    cloudinaryRef.current = window.cloudinary
+    // Create an upload widget
+    widgetRef.current = cloudinaryRef.current.createUploadWidget({
+      cloudName: "dtdnuazpk",
+      uploadPreset: "ml_default"
+    }, function(error, result) {
+      if (!error && result && result.event === "success")  {
+        handleImageUpload(result);
+      }  
+    })
+  }, [])
+
+  return (
+    <button className="upload-image" onClick={() => widgetRef.current.open()}>
+      Upload
+    </button>
+  )
+}
+ 
+export default UploadWidget;
