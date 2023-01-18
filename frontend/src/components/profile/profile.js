4eaee65bcea2bcda7a3fde67bfa2d5870f4db9d0
williamjones <willj836@gmail.com>
2023-01-18 16:37:25 +0000

    styled profile more like fb - also added buttons to show either posts or about section

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react";
-import Post from "../post/Post";
 import { useParams } from "react-router";
 import "./profile.css";
 import Feed from "../feed/Feed";
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
-  const [newName, setNewName] = useState("");
-  const [updatedName, setUpdatedName] = useState(false);
+  const [selectedTab, setSelectedTab] = useState("posts");
   const token = window.localStorage.getItem("token");
   const { user_id } = useParams();
 
@@ -22,49 +20,63 @@ const Profile = () => {
       const data = await response.json();
       if (response.status === 200) {
         setProfile(data);
-        setUpdatedName(false);
       }
     };
     fetchProfile();
-  }, [updatedName]);
-
-  const handleUpdate = async (e) => {
-    e.preventDefault();
-
-    let response = await fetch(`/users/${user_id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({ name: newName }),
-    });
-
-    if (response.status === 200) {
-      setUpdatedName(true);
-    }
-  };
+  }, []);
 
   return (
     <>
-      <div class="profile-container">
-        {profile && <div>Name: {profile.user.name}</div>}
-        <div>Bio: Stuff here</div>
-        <div>Birthday: 11/11/11</div>
-        <div>Other stuff idk</div>
-        <form onSubmit={handleUpdate}>
-          <label>
-            Update Name:
-            <input
-              type="text"
-              value={newName}
-              onChange={(e) => setNewName(e.target.value)}
-            />
-          </label>
-          <button className="update-button">Update</button>
-        </form>
-      </div>
-      <Feed filter={user_id} />
+      {profile && (
+        <>
+          <div className="profile-header">
+            <div
+              className="cover-photo"
+              style={{
+                backgroundImage: `url(${"https://images.unsplash.com/photo-1608501078713-8e445a709b39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2FsbHBhcGVyJTIwNGt8ZW58MHx8MHx8&w=1000&q=80"})`,
+              }}
+            >
+              <div className="profile-picture-container">
+                <img
+                  className="profile-picture"
+                  src="https://wallpapersmug.com/download/3840x2400/43b4da/dwayne-johnson-face-jumanji-welcome-to-the-jungle-8k.jpg"
+                  alt="profile"
+                />
+              </div>
+              <div className="profile-name">{profile.user.name}</div>
+            </div>
+          </div>
+          <div className="tabs-container">
+            <button
+              className={`tab-button ${
+                selectedTab === "about" ? "active" : ""
+              }`}
+              onClick={() => setSelectedTab("about")}
+            >
+              About
+            </button>
+            <button
+              className={`tab-button ${
+                selectedTab === "posts" ? "active" : ""
+              }`}
+              onClick={() => setSelectedTab("posts")}
+            >
+              Posts
+            </button>
+          </div>
+          <div className="profile-container">
+            {selectedTab === "about" ? (
+              <div className="about-section">
+                <div>Bio: Stuff here</div>
+                <div>Birthday: 11/11/11</div>
+                <div>Other stuff idk</div>
+              </div>
+            ) : (
+              <Feed filter={user_id} />
+            )}
+          </div>
+        </>
+      )}
     </>
   );
 };
