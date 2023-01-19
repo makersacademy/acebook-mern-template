2c3c310c151c2a700414579065cfb4781d675b92
Andy <ajscottlewis@gmail.com>
2023-01-19 18:03:20 +0000

    Change stylesheet import, add className to h2

--- frontend/src/components/FriendsList/FriendsList.js
+++ frontend/src/components/FriendsList/FriendsList.js
@@ -1,12 +1,12 @@
 import Friend from "../Friend/Friend";
-import "./FriendsList.css";
+import "../FriendsPage/FriendsPage.css";
 
 const FriendsList = ({ friends, setFriendsUpdated }) => {
   return (
     <div className="friends-list-container">
       { friends && (
         <>
-          <h2>Friends</h2>
+          <h2 className="heading">Friends</h2>
           {/* Map through user's friends using Friend component, pass each friend info as a prop */}
           {friends.map((friend) =>
             <Friend friend={friend} key={friend._id} setFriendsUpdated={setFriendsUpdated} />
