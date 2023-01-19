45abf3e8333dec9d5e265f53db00b5572bc15826
henrychow <113641610+henrow5@users.noreply.github.com>
2023-01-19 11:02:26 +0000

    add profile feed own posts

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import Post from '../post/Post';
 import Create from '../createPost/CreatePost';
-import Card from '../Helpers/Card'
+import Card from '../Helpers/Card';
 import './Feed.css';
 
 const Feed = ({ navigate }) => {
   const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem('token'));
   const [postAdded, setPostAdded] = useState(false);
+  const userId = window.localStorage.getItem('user_id');
 
   useEffect(() => {
     if (token) {
@@ -22,27 +23,42 @@ const Feed = ({ navigate }) => {
           setToken(window.localStorage.getItem('token'));
           setPosts(data.posts.reverse());
           setPostAdded(false);
+          console.log(data.posts);
         });
     }
     // eslint-disable-next-line
   }, [postAdded]);
 
-  if (token) {
+  if (token && window.location.pathname === `/users/${userId}`) {
     return (
-      
-      <div className='feed'>
+      <div className="feed">
+        <h2>Posts</h2>
+
+        <Card>
+          <div id="feed" role="feed">
+            {posts
+              .filter((post) => post.user_id._id === userId)
+              .map((post) => (
+                <Post post={post} key={post._id} setPostAdded={setPostAdded} />
+              ))}
+          </div>
+        </Card>
+      </div>
+    );
+  } else if (token) {
+    return (
+      <div className="feed">
         <h2>Posts</h2>
         <Create setPostAdded={setPostAdded} />
         <Card>
-        <div id="feed" role="feed">
-          {posts.map((post) => (
-            <Post post={post} key={post._id} setPostAdded={setPostAdded} />
-          ))}
-        </div>
+          <div id="feed" role="feed">
+            {posts.map((post) => (
+              <Post post={post} key={post._id} setPostAdded={setPostAdded} />
+            ))}
+          </div>
         </Card>
       </div>
-      
-    )
+    );
   } else {
     navigate('/signin');
   }
