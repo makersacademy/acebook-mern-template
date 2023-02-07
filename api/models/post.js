23cb1094763588d1d0306ef01a5b2bcb607a74f6
Joe Burgess <josephburgess@gmail.com>
2023-02-07 12:14:50 +0000

    test driving timestamps in postschema

--- api/models/post.js
+++ api/models/post.js
@@ -5,9 +5,12 @@ const mongoose = require('mongoose');
  * it contains a list of the fields that you wish to have in the data
  * e.g. message: {type: String, required: true}
  */
-const PostSchema = new mongoose.Schema({
-  message: String,
-});
+const PostSchema = new mongoose.Schema(
+  {
+    message: String,
+  },
+  { timestamps: true }
+);
 
 const Post = mongoose.model('Post', PostSchema);
 
--- api/models/token_generator.js
+++ api/models/token_generator.js
@@ -6,15 +6,18 @@ const secret = process.env.JWT_SECRET;
 // Class with static method to generate (sign) JWT when passed user_id as argument
 
 class TokenGenerator {
+  // Generate JWT token with the user_id provided and an expiration date 10 minutes from now
   static jsonwebtoken(user_id) {
     return JWT.sign(
       {
+        // Payload with the user_id
         user_id: user_id,
+        // Timestamp of when the JWT was created (issued at)
         iat: Math.floor(Date.now() / 1000),
-
-        // Set the JWT token to expire in 10 minutes
+        // Timestamp of when the JWT token will expire
         exp: Math.floor(Date.now() / 1000) + 10 * 60,
       },
+      // The secret used to sign the JWT
       secret
     );
   }
