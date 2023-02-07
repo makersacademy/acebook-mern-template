713cb90959851ed65cd6b7c98b9bc03f02d28f86
Tom Rolfe <tomrolfe20@gmail.com>
2023-02-07 12:39:58 +0000

    creating post first test passed

---
+++ frontend/src/components/createPostForm/createPostForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const CreatePostForm = () => {
+  const [message, setMessage] = useState();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('hello');
+    alert('Post submitted');
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        {message}
+        <input
+          id='post-input'
+          placeholder='Whats on your mind?'
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+          required
+        ></input>
+        <button id='submit' type='submit'>
+          Submit post
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default CreatePostForm;
