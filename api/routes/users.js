99463eed0c02e9a3d95c8f0a514c6194f8b3bc2a
Joe Burgess <josephburgess@gmail.com>
2023-02-08 20:31:09 +0000

    one more redundant require

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
 const upload = require('./middleware');
 const UsersController = require('../controllers/users');
 
