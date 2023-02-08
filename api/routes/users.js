c65f690ac2ae02f02c1d342c668fb1a67ce00d57
Joe Burgess <josephburgess@gmail.com>
2023-02-08 20:05:39 +0000

    removing unneccessary line of code

--- api/routes/users.js
+++ api/routes/users.js
@@ -4,8 +4,6 @@ const multer = require('multer');
 const upload = require('./middleware');
 const UsersController = require('../controllers/users');
 
-router.post('/', upload.single('image'), async (req, res) => {
-  const user = await UsersController.Create(req, res);
-});
+router.post('/', upload.single('image'), UsersController.Create);
 
 module.exports = router;
