6f510e41015f050b6324ac793bf94dd555459c1d
Tom Rolfe <tomrolfe20@gmail.com>
2023-02-08 12:24:17 +0000

    improved test names for better readability in e2e tests

--- frontend/cypress/e2e/making_a_post.cy.js
+++ frontend/cypress/e2e/making_a_post.cy.js
@@ -3,7 +3,7 @@ describe('Creating a post', () => {
     cy.signup('user@email.com', '12345678');
   });
 
-  it('log in and then create a new post', () => {
+  it('log in and then create a new post should return submitted post window', () => {
     cy.visit('/login');
     cy.get('#email').type('someone@example.com');
     cy.get('#password').type('password');
@@ -15,7 +15,7 @@ describe('Creating a post', () => {
       expect(txt).to.contains('Post submitted');
     });
   });
-  it('log in and then create a new post', () => {
+  it('log in and then create a new post should return a feed containing new post', () => {
     cy.visit('/login');
     cy.get('#email').type('someone@example.com');
     cy.get('#password').type('password');
@@ -26,7 +26,7 @@ describe('Creating a post', () => {
     cy.on('window:alert', (txt) => {
       expect(txt).to.contains('Post submitted');
     });
-    cy.visit('/posts')
+    cy.visit('/posts');
     cy.contains('Hello world');
   });
 });
