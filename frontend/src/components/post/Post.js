2b7c73dbca72e14be19fa34c11ca3f5f35d5748c
santosluizfelipe <luizfelipe0598@icloud.com>
2023-02-09 15:55:36 +0000

    first attempt to implement the delete method

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -2,12 +2,27 @@ import React from 'react';
 import './Post.css';
 
 const Post = ({post}) => {
+  const token = localStorage.getItem('token');
 
+  const handleClick = async () => {
+    try {
+      await fetch(`/posts/${post._id}`, {
+        method: 'delete',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+  
   return(
     <div className="post">
       <article data-cy="post" key={ post._id }>{ post.message }</article>
+      <button onClick={handleClick}>delete post</button>
     </div>
   )
-}
-export default Post;
+};
 
+export default Post;
