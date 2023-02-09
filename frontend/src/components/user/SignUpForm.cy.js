4bb2a0eebf1ff88e11087bd7da6544d7759847d6
Joe Burgess <josephburgess@gmail.com>
2023-02-09 12:06:36 +0000

    adding front end displayname field for sign-up

--- frontend/src/components/user/SignUpForm.cy.js
+++ frontend/src/components/user/SignUpForm.cy.js
@@ -1,17 +1,30 @@
-import SignUpForm from './SignUpForm'
-const navigate = () => {}
+import SignUpForm from './SignUpForm';
+const navigate = () => {};
 
-describe("Signing up", () => {
-  it("calls the /users endpoint", () => {
-    cy.mount(<SignUpForm navigate={navigate}/>)
+describe('Signing up', () => {
+  it('calls the /users endpoint', () => {
+    cy.mount(<SignUpForm navigate={navigate} />);
 
-    cy.intercept('POST', '/users', { message: "OK" }).as("signUpRequest")
+    cy.intercept('POST', '/users', { message: 'OK' }).as('signUpRequest');
 
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
-    cy.wait('@signUpRequest').then( interception => {
-      expect(interception.response.body.message).to.eq("OK")
-    })
-  })
-})
+    cy.get('#email').type('someone@example.com');
+    cy.get('#password').type('password');
+    cy.get('#submit').click();
+    cy.wait('@signUpRequest').then((interception) => {
+      expect(interception.response.body.message).to.eq('OK');
+    });
+  });
+  it('allows the user to include a displayname', () => {
+    cy.mount(<SignUpForm navigate={navigate} />);
+
+    cy.intercept('POST', '/users', { message: 'OK' }).as('signUpRequest');
+
+    cy.get('#email').type('someone@example.com');
+    cy.get('#password').type('password');
+    cy.get('#display_name').type('John Smith');
+    cy.get('#submit').click();
+    cy.wait('@signUpRequest').then((interception) => {
+      expect(interception.response.body.message).to.eq('OK');
+    });
+  });
+});
--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -1,45 +1,70 @@
 import React, { useState } from 'react';
 
 const SignUpForm = ({ navigate }) => {
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    fetch( '/users', {
+    fetch('/users', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ email: email, password: password })
-    })
-      .then(response => {
-        if(response.status === 201) {
-          navigate('/login')
-        } else {
-          navigate('/signup')
-        }
-      })
-  }
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        display_name: displayName,
+      }),
+    }).then((response) => {
+      if (response.status === 201) {
+        navigate('/login');
+      } else {
+        navigate('/signup');
+      }
+    });
+  };
 
   const handleEmailChange = (event) => {
-    setEmail(event.target.value)
-  }
+    setEmail(event.target.value);
+  };
 
   const handlePasswordChange = (event) => {
-    setPassword(event.target.value)
-  }
+    setPassword(event.target.value);
+  };
 
+  const handleDisplayNameChange = (event) => {
+    setDisplayName(event.target.value);
+  };
 
-    return (
-      <form onSubmit={handleSubmit}>
-          <input placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
-          <input placeholder="Password" id="password" type='password' value={ password } onChange={handlePasswordChange} />
-        <input id='submit' type="submit" value="Submit" />
-      </form>
-    );
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder='Email'
+        id='email'
+        type='text'
+        value={email}
+        onChange={handleEmailChange}
+      />
+      <input
+        placeholder='Password'
+        id='password'
+        type='password'
+        value={password}
+        onChange={handlePasswordChange}
+      />
+      />
+      <input
+        placeholder='Display Name'
+        id='display_name'
+        type='text'
+        value={displayName}
+        onChange={handleDisplayNameChange}
+      />
+      <input id='submit' type='submit' value='Submit' />
+    </form>
+  );
+};
 
 export default SignUpForm;
