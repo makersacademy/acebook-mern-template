8a2c72a157252d334d0fbdfcce3d7b16903ad3df
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-02-09 12:52:40 +0000

    Passing second test - clicks Acebook logo

--- frontend/src/components/navbar/Navbar.cy.js
+++ frontend/src/components/navbar/Navbar.cy.js
@@ -8,25 +8,11 @@ describe("Navbar", () => {
     cy.get('div')
     .should('be.visible')
   });
+
+  it("clicks Acebook logo", () => {
+    cy.mount(<MemoryRouter><Navbar/></MemoryRouter>)
+    
+    cy.get('a')
+    .eq(0).click();
+  });
 });
-
-
-// Cypress Recorder:
-// cy.visit('http://localhost:3000/posts');
-// cy.get('#post-input').click();
-// cy.get('#post-input').type('{backspace}');
-// cy.get('#post-input').type('Hello world!');
-// cy.get('#submit').click();
-// cy.get('form').submit();
-// cy.get('#email').click();
-// cy.get('#email').type('ju@email.com');
-// cy.get('#password').type('Makers22');
-// cy.get('#submit').click();
-// cy.get('form').submit();
-// cy.get('#post-input').click();
-// cy.get('#post-input').type('Hello world!');
-// cy.get('#submit').click();
-// cy.get('form').submit();
-// cy.get('li:nth-child(1) > a').click();
-// cy.get('li:nth-child(2) > a').click();
-// cy.get('.Navbar_profile__PZm7V img').click();
