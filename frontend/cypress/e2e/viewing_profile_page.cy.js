1da77a3e73809862669add2cf6d62f089a253438
Thomas McCoy <finnmccoy99@gmail.com>
2023-02-09 11:55:25 +0000

    Finished first e2e test for profile page

--- frontend/cypress/e2e/viewing_profile_page.cy.js
+++ frontend/cypress/e2e/viewing_profile_page.cy.js
@@ -1,10 +1,11 @@
 describe("Visiting account page", () => {
   before(() => {
     cy.signup("user@email.com", "12345678");
-    cy.login(("user@email.com", "12345678"));
+    cy.login("user@email.com", "12345678");
   });
 
-  test("Can visit account page", () => {
+  it("Can visit account page", () => {
     cy.visit("/profile");
+    cy.get("h2").contains("Profile");
   });
 });
--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -1,11 +1,11 @@
 import Feed from "../feed/Feed";
-import React, { useNavigate } from "react";
+import React from "react";
 
 function Profile({ navigate }) {
   return (
     <>
       <h2>Profile</h2>
-      <Feed navigate={useNavigate} path={"/account"}></Feed>
+      <Feed navigate={navigate} path={"/account"}></Feed>
     </>
   );
 }
