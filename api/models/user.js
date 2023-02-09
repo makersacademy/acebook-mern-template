561b9ba1ab8cf3310f96bbb0bbf32125aba2379e
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-02-09 10:54:24 +0000

    Test passed for new user schema

--- api/models/user.js
+++ api/models/user.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  surname: { type: String, required: true },
+  friendsList: [{type: Number}]
 });
 
 const User = mongoose.model("User", UserSchema);
