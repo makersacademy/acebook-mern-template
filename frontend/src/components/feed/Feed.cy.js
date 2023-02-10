9f6b1e953426fc791c1f23b6b53267cac9df8be1
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-02-10 14:19:02 +0000

    added testing for reverse chronological ordering of posts

--- frontend/src/components/feed/Feed.cy.js
+++ frontend/src/components/feed/Feed.cy.js
@@ -2,7 +2,7 @@ import Feed from './Feed'
 const navigate = () => {}
 
 describe("Feed", () => {
-  it("Calls the /posts endpoint and lists all the posts", () => {
+  it("Calls the /posts endpoint and lists all the posts in reverse chronological order", () => {
     window.localStorage.setItem("token", "fakeToken")
     
     cy.intercept('GET', '/posts', (req) => {
@@ -10,7 +10,9 @@ describe("Feed", () => {
           statusCode: 200,
           body: { posts: [
             {_id: 1, message: "Hello, world"},
-            {_id: 2, message: "Hello again, world"}
+            {_id: 2, message: "Hello again, world"},
+            {_id: 3, message: "Hello thrice, world"}
+
           ] }
         })
       }
@@ -20,8 +22,10 @@ describe("Feed", () => {
     
     cy.wait("@getPosts").then(() =>{
       cy.get('[data-cy="post"]')
-      .should('contain.text', "Hello, world")
-      .and('contain.text', "Hello again, world")
+      .first().should('contain.text', "Hello thrice, world")
+
+      cy.get('[data-cy="post"]')
+      .last().should('contain.text', "Hello, world")
     })
   })
 })
