865410178cb671a36f00d702638743db9f53c221
alexHoggett <alexmh99@gmail.com>
2023-02-10 16:07:52 +0000

    removed logout

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -51,12 +51,7 @@ const Feed = ({ navigate }) => {
 
         <>
           <h2 className='h2'>Posts</h2>
-            <button className='deleteButton' onClick={() => setForm(!form) }>Add Post</button>
-                      {form && <PostForm />}
-            <button className='deleteButton' onClick={logout}>
 
-              Logout
-            </button>
           <div id='feed' role="feed">
               {posts.map(
                 (post) => ( <Post setPosts={ setPosts } post={ post } key={ post._id } /> )
