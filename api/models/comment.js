c66e9e8477761f99fbd5d43d57a44849d10d108e
Oana <oana.demian@gmail.com>
2023-02-13 15:19:35 +0000

    changed the CommentSchema to include likes and likers

--- api/models/comment.js
+++ api/models/comment.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const CommentSchema = new mongoose.Schema({
   content: { type: String, required: true },
+  likes:   {type: Number, default: 0, required: true},
+  likers:[{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
   user_id: { type: String, required: true },
   post_id: { type: String, required: true },
   }
