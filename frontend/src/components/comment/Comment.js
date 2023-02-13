b01b9bcd08de2e52b0c3260ca04b1cb3d0c9a25d
Jessica Lee <jessica.zy.lee@gmail.com>
2023-02-13 16:34:13 +0000

    Created comment.js component

---
+++ frontend/src/components/comment/Comment.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Comment = ({comment}) => {
+    return(
+        <article data-cy="comment" key={ comment._id }>{ comment.message }</article>
+    )
+}
+
+export default Comment;
