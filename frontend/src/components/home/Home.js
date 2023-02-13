f0680555553479aaba4dfdd5c23a5c7384e5e92d
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-02-13 16:58:18 +0000

    Add Home with links to login and signup tests passing

---
+++ frontend/src/components/home/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Home.css";
+
+const Home = ({ navigate }) => {
+  return (
+    <div>
+      <h2>Welcome to Acebook</h2>
+        <p>Acebook is a social media platform that allows users to connect with friends, family, organizations and public figures. As part of the community you can interact to other users and share pictures and posts. </p>
+        <p></p>
+        <ul className={styles.links}>
+          <Link to="/login"><button>Login</button></Link>
+        <p></p>
+          <Link to="/signup"><button>Sign Up</button></Link>
+        <p></p>
+        </ul>
+      <div>
+        <img src="/images/social-media-connecting-people-vector.avif" alt="home" />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
