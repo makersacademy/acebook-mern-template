6c7d9b498fc2c29ff925a1d46b36d21c47918dab
Alicia Moses <aliciamoses.dev@gmail.com>
2023-02-13 23:27:11 +0000

    Working post creation with and without photo

--- frontend/src/components/createPost/CreatePost.js
+++ frontend/src/components/createPost/CreatePost.js
@@ -1,54 +1,93 @@
 import React, { useState } from "react";
 
-const CreatePost = ({ token }) => {
-  const [post, setPost] = useState("");
-  const handlePostChange = (event) => {
-    setPost(event.target.value);
-  };
+
+const CreatePost = ({ navigate }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [token, setToken] = useState(window.localStorage.getItem("token"));
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    let response = await fetch("/newPost", {
+    let response = await fetch("/posts", {
       method: "post",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       },
-      body: JSON.stringify({ post: post }),
+      body: JSON.stringify({ title: title,  content: content, photo: photo }),
     });
 
     if (response.status !== 201) {
-      console.log("new post created");
-      //   navigate("/posts");
+      console.log("new post not created");
+      navigate("/posts");
     } else {
-      console.log("oops");
-      //   let data = await response.json();
-      //   window.localStorage.setItem("token", data.token);
-      //   navigate("/posts");
+      console.log("new post created");
+      let data = await response.json();
+      window.localStorage.setItem("token", data.token);
+      setToken(window.localStorage.getItem("token"));
+     
     }
   };
 
-  if (token) {
-    return (
-      <>
-        <form onSubmit={handleSubmit}>
-          <textarea
-            placeholder="what are you thinking?"
-            id="newPost"
-            type="text"
-            rows="5"
-            cols="50"
-            value={post}
-            onChange={handlePostChange}
-          ></textarea>
+   const handleTitleChange = (event) => {
+     setTitle(event.target.value);
+   };
 
-          <input id="submit" type="submit" value="Submit" />
-        </form>
-      </>
-    );
-  } else {
-    return <></>;
-  }
+  const handlePostChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handlePhotoChange = (event) => {
+    setPhoto(event.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title</label>
+        <br></br>
+        <input
+          placeholder="Title"
+          id="title"
+          type="text"
+          value={title}
+          onChange={handleTitleChange}
+        ></input>
+        <br></br>
+
+        <label htmlFor="content">Content:</label>
+        <br></br>
+        <textarea
+          placeholder="Content"
+          id="content"
+          name="content"
+          type="text"
+          rows="5"
+          cols="50"
+          value={content}
+          onChange={handlePostChange}
+        ></textarea>
+        <br></br>
+
+        <label htmlFor="photo">Photo:</label>
+        <br></br>
+        <input
+          type="file"
+          id="photo"
+          name="photo"
+          value={photo}
+          onChange={handlePhotoChange}
+        ></input>
+        <br></br>
+        <br></br>
+
+        <button id="submit" type="submit">
+          Create post
+        </button>
+      </form>
+    </div>
+  );
 };
 export default CreatePost;
