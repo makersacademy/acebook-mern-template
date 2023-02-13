4452846eea781a70581666fddf73f27f89ad4fbe
Murat <murat.sertay@icloud.com>
2023-02-13 16:28:52 +0000

    In app.js we moved the functions tokenChecker, errorHandler and catch404 to a separate file at controllers/controllerUtils.js for a cleaner page.
    we also added the comments router here

--- api/app.js
+++ api/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: './.env.development.local' })
+require("./utils")
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
@@ -10,35 +10,16 @@ const tokensRouter = require("./routes/tokens");
 const usersRouter = require("./routes/users");
 const commentsRouter = require("./routes/comments");
 
-const app = express();
+const {tokenChecker, errorHandler, catch404} = require("./controllers/controllerUtils");
 
+const app = express();
 // setup for receiving JSON
 app.use(express.json())
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // middleware function to check for valid tokens
-const tokenChecker = (req, res, next) => {
-
-  let token;
-  const authHeader = req.get("Authorization")
-
-  if(authHeader) {
-    token = authHeader.slice(7)
-  }
-
-  JWT.verify(token, process.env.JWT_SECRET, (err, payload) => {
-    if(err) {
-      console.log(err)
-      res.status(401).json({message: "auth error"});
-    } else {
-      req.user_id = payload.user_id;
-      next();
-    }
-  });
-};
 
 // route setup
 app.use("/posts", tokenChecker, postsRouter);
@@ -47,18 +28,9 @@ app.use("/users", usersRouter);
 app.use("/comments", tokenChecker, commentsRouter);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
+app.use(errorHandler);
 
 // error handler
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // respond with details of the error
-  res.status(err.status || 500).json({message: 'server error'})
-});
+app.use(catch404);
 
 module.exports = app;
