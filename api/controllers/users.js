d1ecaba8319f893436cb4ddf50668e529cf21c1f
Alicia Moses <aliciamoses.dev@gmail.com>
2023-02-13 21:19:00 +0000

    Updated users controller and route with Find

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -5,11 +5,20 @@ const UsersController = {
     const user = new User(req.body);
     user.save((err) => {
       if (err) {
-        res.status(400).json({ message: "Bad request" })
+        res.status(400).json({ message: "Bad request" });
       } else {
         res.status(201).json({ message: "OK" });
       }
     });
+  },
+ 
+  Find: async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (err) {
+      throw err;
+    }
+    const token = await TokenGenerator.jsonwebtoken(req.user_id);
+    res.status(200).json({ user: user, token: token });
   },
 };
 
