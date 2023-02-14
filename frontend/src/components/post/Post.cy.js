6397fec7a0df45651cd497ce7eb19ee4bf012878
ShaunFlood <shaunflood@hotmail.co.uk>
2023-02-14 16:54:34 +0000

    Testing for comment messages showing in post

--- frontend/src/components/post/Post.cy.js
+++ frontend/src/components/post/Post.cy.js
@@ -16,6 +16,26 @@ describe('Post', () => {
     cy.get('[data-cy="post"]').should('contain.text', 'Hello, world');
   });
 
+  it('If passed comments as a prop, will display comments', () => {
+    cy.mount(
+      <Post
+        post={{
+          _id: 1,
+          message: 'Hello world again',
+          likes: [],
+          comments: [
+            { message: 'another message', user_id: 1, post_id: 1 },
+            { message: 'another hello world', user_id: 2, post_id: 1 },
+          ],
+          createdAt: '2023-02-14T11:44:40.970Z',
+        }}
+      />
+    );
+    cy.get('[data-cy="comment"]')
+      .should('contain.text', 'another message')
+      .and('contain.text', 'another hello world');
+  });
+
   describe('like button', () => {
     it('Calls the /like endpoin and toggles likes on and off', () => {
       window.localStorage.setItem('token', 'fakeToken');
