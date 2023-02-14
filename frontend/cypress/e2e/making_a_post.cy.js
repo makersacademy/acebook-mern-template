554c688508b1b3b537823c94b320454006d14e83
abodian <ajnbodian@gmail.com>
2023-02-14 15:40:04 +0000

    making a post e2e cypress test fixed

--- frontend/cypress/e2e/making_a_post.cy.js
+++ frontend/cypress/e2e/making_a_post.cy.js
@@ -1,35 +1,21 @@
-describe.skip("post function", () => {
+describe("post function", () => {
   before(() => {
     cy.signup("someone@example.com", "password", "John", "Smith");
   });
 
   it("tests to see if the post button and box works properly", () => {
     // need to login in from the login page
-    cy.visit("/login");
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
-    cy.get("#message").type("cypress test 2");
-    cy.get("#submit").click();
-    cy.url().should("include", "/posts");
-    cy.get("#feed").should("contain", "cypress test 2");
+    cy.visit('http://localhost:3000/login');
+    cy.get('#email').click();
+    cy.get('#email').type('someone@example.com');
+    cy.get('#password').click();
+    cy.get('#password').type('password');
+    cy.get('#submit').click();
+    cy.get('#message').click();
+    cy.get('#message').type('This is a test post');
+    cy.get('#submitPost').click();
+    cy.get('form:nth-child(2)').submit();
+    cy.url().should('contains', 'http://localhost:3000/posts');
+    cy.get("#feed").should("contain", "This is a test post");
   });
 });
-describe("post function", () => {
-
-  before(() => {
-    cy.signup("someone@example.com", "password", "John", "Smith")
-  })
-
-it("tests to see if the post button and box works properly", () => {
-    // need to login in from the login page
-    cy.visit('/login');
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
-    cy.get('#message').type('cypress test 2');
-    cy.get('#submit').click();
-    cy.url().should('include', '/posts');    
-    cy.get('#feed').should('contain', 'cypress test 2')
-  })
-})
