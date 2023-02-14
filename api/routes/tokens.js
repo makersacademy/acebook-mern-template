35ca9fda0c1a2851865566b961b9f6212169ffdd
Ehijie <111647260+ehijie98@users.noreply.github.com>
2023-02-14 11:16:34 +0000

    Update tokens.js
    Updated tokens route back to default

--- api/routes/tokens.js
+++ api/routes/tokens.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const SessionsController = require("../controllers/tokens");
-// const TokensController = require("../controllers/tokens"); - Was this previously
+const TokensController = require("../controllers/tokens");
 
-router.post("/", SessionsController.Create);
-// router.post("/", TokensController.Create); - Was this previously
+// router.post("/", TokensController.Create);
 
 module.exports = router;
