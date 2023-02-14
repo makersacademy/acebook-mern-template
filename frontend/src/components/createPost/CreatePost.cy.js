8072f9a4ab899fbcd4930d8b8450ded59d02df0f
Ehijie <eaghedo1@gmail.com>
2023-02-14 14:08:16 +0000

    updated createpost cypress test

--- frontend/src/components/createPost/CreatePost.cy.js
+++ frontend/src/components/createPost/CreatePost.cy.js
@@ -3,12 +3,13 @@ import CreatePost from './CreatePost'
 const token = "12345"
 
 describe("create new post", () => {
-  it("calls the /newPost endpoint", () => {
+  it("calls the /posts endpoint", () => {
     cy.mount(<CreatePost token={token}/>)
 
     cy.intercept('POST', '/posts', { token: "fakeToken" }).as("postRequest")
 
-    cy.get("#newPost").type("I'm making pancakes!");
+    cy.get("#title").type("What I'm cooking..");
+    cy.get("#content").type("I'm making pancakes!");
     cy.get("#submit").click();
     cy.wait('@postRequest').then( interception => {
       expect(interception.response.body.token).to.eq("fakeToken")
