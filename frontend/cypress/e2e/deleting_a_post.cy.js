022e87ffbfb8a6f68099e5737eec78394c12d75a
Alex Bodian <113264125+abodian@users.noreply.github.com>
2023-02-14 13:46:51 +0000

    Revert "06/likes button 330pm"

--- frontend/cypress/e2e/deleting_a_post.cy.js
+++ frontend/cypress/e2e/deleting_a_post.cy.js
@@ -3,7 +3,6 @@ describe("post function", () => {
     cy.signup("someone@example.com", "password", "John", "Smith");
   });
 
-  // this test is not working following the implementation of the like button. Not sure why.
   it("tests to see if a posts can be deleted", () => {
     cy.visit("/login");
     cy.get("#email").type("someone@example.com");
@@ -11,8 +10,9 @@ describe("post function", () => {
     cy.get("#submit").click();
     cy.get("#message").type("cypress test 45");
     cy.get("#submit").click();
-    cy.get('[data-cy="delete"]').last().click();  
+
+    cy.get('[data-cy="delete"]').last().click();
+
     cy.get('[data-cy="post"]').contains("cypress test 45").should("not.exist");
-    // cy.get('.left-aligned').contains("cypress test 45").should("not.exist"); This line doesn't work either.
   });
 });
