8ffb513fadc0de31acb97234515b6af93630c1aa
Alicia Moses <aliciamoses.dev@gmail.com>
2023-02-15 17:09:01 +0000

    Completed components of navbar

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -3,36 +3,43 @@ import React, { useState } from "react";
 const Navbar = () => {
   return (
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <ul>
-        <div className="logoDiv">
-          <li>
-            <a href="/home">
-              <img src="blank.jpg" alt=""></img>
-            </a>
+      <div className="container-fluid">
+        <ul class="nav justify-content-center">
+          <div className="logoDiv">
+            <li class="nav-item">
+              <a href="/home">
+                <img
+                  src="https://media.geeksforgeeks.org/auth/profile/8ceu3jpotjla4m5wpbm7"
+                  alt=""
+                  width="30"
+                  height="30"
+                ></img>
+              </a>
+            </li>
+          </div>
+          <li class="nav-item">
+            <a href="/user/:id">Profile</a>
           </li>
-        </div>
-        <li class="nav-item">
-          <a href="/user/:id">Profile</a>
-        </li>
-        <li class="nav-item">
-          <a href="/posts">Feed</a>
-        </li>
-        <li>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="text"
-              placeholder="Search..."
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-        </li>
-        <li>
-          <a href="/signout">Sign out</a>
-        </li>
-      </ul>
+          <li class="nav-item">
+            <a href="/posts">Feed</a>
+          </li>
+          <li class="nav-item">
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="text"
+                placeholder="Search..."
+              />
+              <button class="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </li>
+          <li class="nav-item">
+            <a href="/signout">Sign out</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
