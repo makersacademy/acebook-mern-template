abd265323afcc0fe1f6efda870964c5e326c3c71
Murat <murat.sertay@icloud.com>
2023-02-15 09:08:37 +0000

    ap/models/comment.js model updated and is it now being called within posts, It also has timestamps as well.
    It has 100% code coverage through being tested on the post model
    
    Previous comment files were not needed so have been removed

--- api/routes/comments.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const CommentsController = require("../controllers/comments");
-
-router.get("/comments", CommentsController.Find_By_Post_Id);
-router.post("/", CommentsController.Create);
-module.exports = router;
