e144763527aa04e23b804eccf501fab0fad63825
Alicia Moses <aliciamoses.dev@gmail.com>
2023-02-15 22:03:56 +0000

    Completed styling for central navbar components

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,27 +1,37 @@
 import React, { useState } from "react";
-
+import './Navbar.css';
+// need to add padding to nav.css file and choose colours 
+//add padding top to logo
+// make signout button float to other side of page
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className=" bg-body-tertiary">
       <div className="container-fluid">
-        <ul class="nav justify-content-center">
-          <div className="logoDiv">
-            <li class="nav-item">
-              <a href="/home">
-                <img
-                  src="https://media.geeksforgeeks.org/auth/profile/8ceu3jpotjla4m5wpbm7"
-                  alt=""
-                  width="30"
-                  height="30"
-                ></img>
-              </a>
-            </li>
-          </div>
+        <div className="logoDiv">
+          <a className="navbar-brand" href="/home">
+            <img
+              src="https://media.geeksforgeeks.org/auth/profile/8ceu3jpotjla4m5wpbm7"
+              alt=""
+              width="30"
+              height="30"
+            ></img>
+          </a>
+        </div>
+        <ul className="nav justify-content-center">
+          <a href="/user/:id">
+            <button class="btn btn-outline-primary" type="submit"> 
+              <i class="bi bi-person-square"></i>
+              <span> Profile</span>
+            </button>
+          </a>
+
           <li class="nav-item">
-            <a href="/user/:id">Profile</a>
-          </li>
-          <li class="nav-item">
-            <a href="/posts">Feed</a>
+            <a href="/posts">
+              <button class="btn btn-outline-primary" type="submit">
+                <i class="bi bi-chat-square-text"></i>
+                <span> Feed</span>
+              </button>
+            </a>
           </li>
           <li class="nav-item">
             <form className="d-flex" role="search">
@@ -30,13 +40,18 @@ const Navbar = () => {
                 type="text"
                 placeholder="Search..."
               />
-              <button class="btn btn-outline-success" type="submit">
-                Search
+
+              <button class="btn btn-primary" type="submit">
+                <span>Search</span>
               </button>
             </form>
           </li>
           <li class="nav-item">
-            <a href="/signout">Sign out</a>
+            <a href="/signout">
+              <button class="btn btn-outline-secondary" type="submit">
+                Sign out
+              </button>
+            </a>
           </li>
         </ul>
       </div>
