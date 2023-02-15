e716172e854d4b79b76b5f5feecbaf7ce9959863
Jessica Lee <jessica.zy.lee@gmail.com>
2023-02-15 16:17:03 +0000

    Pushing to swap in pairs

--- frontend/src/components/commentFeed/CommentFeed.js
+++ frontend/src/components/commentFeed/CommentFeed.js
@@ -4,49 +4,48 @@ import Post from '../post/Post'
 import Comment from '../comment/Comment'
 
 const CommentFeed = ({ navigate }) => {
-    const [post, setPost] = useState([]);
-    const [comments, setComments] = useState([]);
-    const [token, setToken] = useState(window.localStorage.getItem("token"));
-    const [isUpdated, setIsUpdated] = useState(false);
+  const [post, setPost] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [isUpdated, setIsUpdated] = useState(false);
 
-    useEffect(() => {
-        if(token && (isUpdated || post.length === 0)) {
-          fetch(window.location.pathname, {
-            headers: {
-              'Authorization': `Bearer ${token}`
-            }
+  useEffect(() => {
+      if(token && (isUpdated || post.length === 0)) {
+        fetch(window.location.pathname, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        })
+          .then(response => response.json())
+          .then(async data => {
+            console.log(data)
+            window.localStorage.setItem("token", data.token)
+            setToken(window.localStorage.getItem("token"))
+            setPost(data.post);
+            setIsUpdated(false);
           })
-            .then(response => response.json())
-            .then(async data => {
-              console.log(data)
-              window.localStorage.setItem("token", data.token)
-              setToken(window.localStorage.getItem("token"))
-              setPost(data.post);
-              setIsUpdated(false);
-              console.log(data.post)
-            })
-        }
-    }, [token, post, isUpdated]);
+      }
+  }, [token, post, isUpdated]);
 
-    if(token) {
-      return(
-        <>
-        <h1>Post test</h1>
-        <Post post={ post } key={ post._id } />
-            {/* <CreateCommentForm callback={(value) => {
-              setIsUpdated(value); 
-            }}/> */}
-          <div id='feed' role="feed">
-              {post.comments.map(
-                (comment) => ( 
-                <Comment comment={ comment } key={ comment._id } />
-                )
-              )}
-          </div>
-          <p>End of test</p>
-        </>
-      )
-    }
+  if(token) {
+    return(
+      <>
+        <h1>Test</h1>
+        {console.log(post)}
+        <h2>{post.message}</h2>
+        {/* <h2><Post post={ post } key={ post._id } /></h2> */}
+
+        <div id='feed' role="feed">
+          {post.comments.map(
+            (comment) => ( 
+              <p>{comment.content}</p>
+              // <Comment comment={ comment } key={ comment._id } />
+            )
+          )}
+        </div>
+      </>
+    )
+  }
 }
 
 export default CommentFeed;
--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -3,8 +3,13 @@ import React from 'react';
 const Post = ({post}) => {
   return(
     <div className='post-div'>
-      <article data-cy="post" key={ post._id }>{ post.message }</article>
-      <a href={`/post/${post._id}`}><p data-cy='comments-link'>Comments: {post.comments.length}</p></a>
+      <article data-cy="post" key={ post._id }>
+        { post.message }
+      </article>
+
+      <a href={`/posts/${post._id}`}>
+        <p data-cy='comments-link'>Comments: {post.comments.length}</p>
+      </a>
     </div>
   )
 }
