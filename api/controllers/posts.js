8920a41c82c55c80cfe8e79b6041eaf953314300
Murat <murat.sertay@icloud.com>
2023-02-15 11:47:05 +0000

    Add comments to postcontroller

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -13,15 +13,16 @@ const PostsController = {
         });
     },
     FindPostById: (req, res) => {
-        //extract param post_id
         // TODO: Find by post_id needs to be tested
+        // Extract post_id from params (Taken from :post_id in the url)
         let post_id = req.params.post_id
+        // Use post_id to find particular post
         Post.findById({_id:post_id}, async (err, post) => {
             if (err) {
                 throw err;
             }
-            console.log(post)
             const token = await TokenGenerator.jsonwebtoken(req.user_id)
+            // returns a body containing the post object and token string
             res.status(200).json({post, token});
         });
     },
@@ -39,11 +40,14 @@ const PostsController = {
     CreateComment: (req, res) => {
         // TODO: CreateComment needs to have tests added
         let {params: {post_id}, body: {user_id,content}} = req
+        // user_id and content are the properties of a comment object and must always be sent.
         Post.findById({_id: post_id}, async (err, post) => {
             if (err) {
                 throw err;
             }
+            // This pushes the new comment object into the comments array in the post object
             post.comments.push({content,user_id})
+            // saves the change to the comments property in post object
             await post.save()
             const token = await TokenGenerator.jsonwebtoken(user_id)
             res.status(201).json({message: 'OK', token});
