82450dd2cdd6e568cca01623db8e85206ab83f9c
Jessica Lee <jessica.zy.lee@gmail.com>
2023-02-15 15:35:01 +0000

    Comments displayed feature implemented

--- frontend/src/components/comment/Comment.js
+++ frontend/src/components/comment/Comment.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Comment = ({comment}) => {
     return(
-        <article data-cy="comment" key={ comment._id }>{ comment.message }</article>
+        <article data-cy="comment" key={ comment._id }>
+            { comment.content }
+        </article>
     )
 }
 
--- frontend/src/components/commentFeed/CommentFeed.js
+++ frontend/src/components/commentFeed/CommentFeed.js
@@ -4,42 +4,46 @@ import Post from '../post/Post'
 import Comment from '../comment/Comment'
 
 const CommentFeed = ({ navigate }) => {
-    console.log("comment feed is executed")
-    const [posts, setPosts] = useState([]);
+    const [post, setPost] = useState([]);
     const [comments, setComments] = useState([]);
     const [token, setToken] = useState(window.localStorage.getItem("token"));
     const [isUpdated, setIsUpdated] = useState(false);
 
     useEffect(() => {
-        if(token && (isUpdated || comments.length === 0)) {
-          fetch("/posts/:post_id/", {
+        if(token && (isUpdated || post.length === 0)) {
+          fetch(window.location.pathname, {
             headers: {
               'Authorization': `Bearer ${token}`
             }
           })
             .then(response => response.json())
             .then(async data => {
+              console.log(data)
               window.localStorage.setItem("token", data.token)
               setToken(window.localStorage.getItem("token"))
-              setPosts(data.posts.reverse());
+              setPost(data.post);
               setIsUpdated(false);
+              console.log(data.post)
             })
         }
-    }, [token, comments, isUpdated]);
+    }, [token, post, isUpdated]);
 
     if(token) {
       return(
         <>
-        <h2>Post test test</h2>
-        {/* <Post post={ "test post" } key={ "post._id" } /> */}
+        <h1>Post test</h1>
+        <Post post={ post } key={ post._id } />
             {/* <CreateCommentForm callback={(value) => {
               setIsUpdated(value); 
             }}/> */}
-          {/* <div id='feed' role="feed">
-              {posts.map(
-                (post) => ( <Post post={ post } key={ post._id } /> )
+          <div id='feed' role="feed">
+              {post.comments.map(
+                (comment) => ( 
+                <Comment comment={ comment } key={ comment._id } />
+                )
               )}
-          </div> */}
+          </div>
+          <p>End of test</p>
         </>
       )
     }
