8c439206fdd980d58933b95e7c4bd50f96162364
charlie lambert <char.lam@ntlworld.com>
2023-02-15 12:56:48 +0000

    Changed href for the link to actually be dynamic with the right post._id

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -4,7 +4,7 @@ const Post = ({post}) => {
   return(
     <div className='post-div'>
       <article data-cy="post" key={ post._id }>{ post.message }</article>
-      <a href='/post/post._id'><p data-cy='comments-link'>Comments: {post.comments.length}</p></a>
+      <a href={`/post/${post._id}`}><p data-cy='comments-link'>Comments: {post.comments.length}</p></a>
     </div>
   )
 }
