e3aef554b183e98de22342795dc125360ca9ea6f
Orhan Khanbayov <orhan.khanbayov@hotmail.co.uk>
2023-02-15 14:28:17 +0000

    cors

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const JWT = require('jsonwebtoken');
+const cors = require('cors');
 
 const postsRouter = require('./routes/posts');
 const tokensRouter = require('./routes/tokens');
@@ -13,7 +14,11 @@ const commentRouter = require('./routes/comments');
 const app = express();
 
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
