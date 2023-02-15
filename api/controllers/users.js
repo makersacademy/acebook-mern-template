607b9f38346ca3540ab5be1a58435d2027e88805
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-02-15 16:11:22 +0000

    added email and password validation and tests

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,23 +1,23 @@
 const User = require("../models/user");
 
 const UsersController = {
-	Create: async (req, res) => {
-		const existingUser = await User.exists({ email: req.body.email });
-		if (existingUser) {
-			return res.status(400).json({ message: "Email already in use" });
-		}
+  Create: async (req, res) => {
+    const existingUser = await User.exists({ email: req.body.email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
 
-		const newUser = new User(req.body);
-		newUser.save((err) => {
-			if (err) {
-				res.status(400).json({ message: err.message });
-			} else {
-				res.status(201).json({
-					message: "Thanks! your account has been successfully created.",
-				});
-			}
-		});
-	},
+    const newUser = new User(req.body);
+    newUser.save((err) => {
+      if (err) {
+        res.status(400).json({ message: err.message });
+      } else {
+        res.status(201).json({
+          message: "Thanks! your account has been successfully created.",
+        });
+      }
+    });
+  },
 };
 
 module.exports = UsersController;
