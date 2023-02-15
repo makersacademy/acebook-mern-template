4dbea00ae82275d48a06c370c969567cf300ba39
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-02-15 15:40:20 +0000

    added code for email and password validations

--- api/models/user.js
+++ api/models/user.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
-mongoose.set('strictQuery', true);
-const UserSchema = new mongoose.Schema({
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-});
+mongoose.set("strictQuery", true);
+const validateEmail = (email) => {
+	const emailToCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	return emailToCheck.test(email);
+};
 
-UserSchema.plugin(uniqueValidator);
+const UserSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+		validate: [validateEmail, "Please use a valid email address"],
+	},
+	password: { type: String, required: true, minlength: 4, maxlength: 10 },
+});
 
 const User = mongoose.model("User", UserSchema);
 
