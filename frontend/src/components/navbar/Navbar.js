c043897efa928651528daa01fa65942e2364a955
Alicia Moses <aliciamoses.dev@gmail.com>
2023-02-15 23:22:43 +0000

    Navbar spacing, buttons and brand added

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import './Navbar.css';
 
-// make signout button float to other side of page
-const Navbar = () => {// need to add padding to nav.css file and choose colours 
-//add padding top to 
+
+const Navbar = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
+
+
 
   const handleSearchSubmit = (event) => {
     event.preventDefault();
@@ -24,63 +25,58 @@ const Navbar = () => {// need to add padding to nav.css file and choose colours
       });
     }
   return (
-    <nav className=" bg-body-tertiary">
-      <div className="container-fluid">
-        <div className="logoDiv">
-          <a className="navbar-brand" href="/home">
-            <img
-              src="https://media.geeksforgeeks.org/auth/profile/8ceu3jpotjla4m5wpbm7"
-              alt=""
-              width="30"
-              height="30"
-            ></img>
+    <nav class="navbar navbar-expand-lg bg-light rounded">
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse d-lg-flex">
+          <a class="navbar-brand col-lg-3 me-0" href="/">
+            Acebookers
           </a>
+          <ul class="navbar-nav col-lg-6 justify-content-lg-center">
+            <li class="nav-item">
+              <a href="/user/:id">
+                <button className="btn btn-outline-primary" type="submit">
+                  <i className="bi bi-person-square"></i>
+                  <span> Profile</span>
+                </button>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/posts">
+                <button className="btn btn-outline-primary" type="submit">
+                  <i className="bi bi-chat-square-text"></i>
+                  <span> Feed</span>
+                </button>
+              </a>
+            </li>
+            <li className="nav-item">
+              <form
+                onSubmit={handleSearchSubmit}
+                className="d-flex"
+                role="search"
+              >
+                <input
+                  className="form-control me-2"
+                  type="text"
+                  placeholder="Search..."
+                  value={searchTerm}
+                  onChange={handleSearchChange}
+                />
+
+                <button className="btn btn-primary" type="submit">
+                  <span>Search</span>
+                </button>
+              </form>
+            </li>
+          </ul>
+          <div class="d-lg-flex col-lg-3 justify-content-lg-end">
+            <a href="/logout">
+              <button class="btn btn-outline-dark">
+                <i class="bi bi-box-arrow-right"></i>
+                <span>Sign Out</span>
+              </button>
+            </a>
+          </div>
         </div>
-        <ul className="nav justify-content-center">
-          <a href="/user/:id">
-            <button className="btn btn-outline-primary" type="submit">
-              <i className="bi bi-person-square"></i>
-              <span> Profile</span>
-            </button>
-          </a>
-
-          <li className="nav-item">
-            <a href="/posts">
-              <button className="btn btn-outline-primary" type="submit">
-                <i className="bi bi-chat-square-text"></i>
-                <span> Feed</span>
-              </button>
-            </a>
-          </li>
-          <li className="nav-item">
-            <form
-              onSubmit={handleSearchSubmit}
-              className="d-flex"
-              role="search"
-            >
-              <input
-                className="form-control me-2"
-                type="text"
-                placeholder="Search..."
-                value={searchTerm}
-                onChange={handleSearchChange}
-              />
-
-              <button className="btn btn-primary" type="submit">
-                <span>Search</span>
-              </button>
-            </form>
-          </li>
-        </ul>
-        <ul className="nav justify-content-end">
-          <li className="nav-item">
-            <a href="/signout">
-              <button className="btn btn-outline-secondary" type="submit">
-                Sign out
-              </button>
-            </a>
-          </li>
-        </ul>
         {searchResults.length > 0 && (
           <ul>
             {searchResults.map((result) => (
