da6f3a5d2e4a12ad624e1f42a1ecb6508c788d13
abodian <ajnbodian@gmail.com>
2023-02-16 15:59:53 +0000

    added ability to change profile picture

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const upload = require("../config/multer");
 
 const UsersController = require("../controllers/users");
 
 router.post("/", UsersController.Create);
 router.get("/", UsersController.Find);
 router.post("/bio/:id", UsersController.UpdateBio);
+router.post("/profile-picture/:id", upload.single("image"), UsersController.UploadProfilePicture);
+router.put("/profile-picture/:id", UsersController.UpdateProfilePictureURL);
 
 module.exports = router;
