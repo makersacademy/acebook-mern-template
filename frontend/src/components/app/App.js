f9c0171ac16038297c3fdd0ba3442b2e57bf700c
mantaslv <mantasvolkas@gmail.com>
2023-02-16 13:02:42 +0000

    removed comments and console logs

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -5,24 +5,23 @@ import NavBar from '../navBar/navBar';
 import Feed from '../feed/Feed'
 import CommentFeed from '../commentFeed/CommentFeed';
 import {
-  useNavigate,
-  Routes,
-  Route,
+    useNavigate,
+    Routes,
+    Route,
 } from "react-router-dom";
 
 const App = () => {
     return (
-      <>
-        <NavBar/>
-        <Routes>
-          <Route path='/posts' element={<Feed navigate={useNavigate()} />} />
-          <Route path='/posts/:post_id' element={<CommentFeed navigate={useNavigate()} />} />
-          <Route path='/login' element={<LoginForm navigate={useNavigate()} />} />
-          <Route path='/signup' element={<SignUpForm navigate={useNavigate()} />} />
-        </Routes> 
-      </>
+        <>
+            <NavBar/>
+            <Routes>
+                <Route path='/posts' element={<Feed navigate={useNavigate()} />} />
+                <Route path='/posts/:post_id' element={<CommentFeed navigate={useNavigate()} />} />
+                <Route path='/login' element={<LoginForm navigate={useNavigate()} />} />
+                <Route path='/signup' element={<SignUpForm navigate={useNavigate()} />} />
+            </Routes> 
+        </>
     );
 }
-
 
 export default App;
--- frontend/src/components/commentFeed/CommentFeed.js
+++ frontend/src/components/commentFeed/CommentFeed.js
@@ -3,57 +3,48 @@ import CreateCommentForm from '../createCommentForm/createCommentForm';
 import Comment from '../comment/Comment'
 
 const CommentFeed = ({ navigate }) => {
-  const [post, setPost] = useState([]);
-  const [comments, setComments] = useState([]);
-  const [token, setToken] = useState(window.localStorage.getItem("token"));
-  const [isUpdated, setIsUpdated] = useState(false);
+    const [post, setPost] = useState([]);
+    const [comments, setComments] = useState([]);
+    const [token, setToken] = useState(window.localStorage.getItem("token"));
+    const [isUpdated, setIsUpdated] = useState(false);
 
+    useEffect(() => {
+        if(token && (isUpdated || comments.length === 0)) {
+            fetch(window.location.pathname, {
+                headers: {
+                  'Authorization': `Bearer ${token}`
+                }
+            })
+            .then(response => response.json())
+            .then(async data => {
+                window.localStorage.setItem("token", data.token)
+                setToken(window.localStorage.getItem("token"))
+                setPost(data.post.message);
+                setComments(data.post.comments);
+                setIsUpdated(false);
+            })
+        }
+    }, [token, comments, isUpdated]);
 
-  useEffect(() => {
-      if(token && (isUpdated || comments.length === 0)) {
-        console.log(token);
-        fetch(window.location.pathname, {
-          headers: {
-            'Authorization': `Bearer ${token}`
-          }
-        })
-          .then(response => response.json())
-          .then(async data => {
-            console.log(data.post.message);
-            window.localStorage.setItem("token", data.token)
-            setToken(window.localStorage.getItem("token"))
-            setPost(data.post.message);
-            setComments(data.post.comments);
-            setIsUpdated(false);
-          })
-      }
-  }, [token, comments, isUpdated]);
+    const logout = () => {
+      window.localStorage.removeItem("token")
+      navigate('/login')
+    }
 
-  const logout = () => {
-    window.localStorage.removeItem("token")
-    navigate('/login')
-  }
-
-  if(token) {
-    return(
-      <>
-        <h2>{post}</h2>
-        <CreateCommentForm callback={(value) => {
-              setIsUpdated(value); 
-        }}/>
-        <div id='feed' role="feed">
-          {comments.map(
-            (comment) => ( <Comment comment={ comment } key={ comment._id } /> )
-          )}
-        </div>
-        <button onClick={logout}>
-              Logout
-        </button>
-      </>
-    )
-  } else {
-    navigate('/login')
-  }
+    if(token) {
+        return(
+            <>
+                <h2>{post}</h2>
+                <CreateCommentForm callback={(value) => setIsUpdated(value)}/>
+                <div id='feed' role="feed">
+                    {comments.map((comment) => ( <Comment comment={ comment } key={ comment._id } /> ))}
+                </div>
+                <button onClick={logout}>Logout</button>
+            </>
+        )
+    } else {
+        navigate('/login')
+    }
 }
 
 export default CommentFeed;
