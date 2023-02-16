7f0357a3e4c54b09288e74089d6e46882dcff2c7
Tom Rolfe <tomrolfe20@gmail.com>
2023-02-16 16:13:07 +0000

    nearly finished profile pic changing

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,39 +1,38 @@
-import "./App.css";
-import LoginForm from "../auth/LoginForm";
-import SignUpForm from "../user/SignUpForm";
-import React, { useState } from "react";
-import Feed from "../feed/Feed";
-import Home from "../home/Home";
-import Navbar from "../navbar/Navbar";
-import { useNavigate, Routes, Route } from "react-router-dom";
-import Profile from "../profile/Profile";
+import './App.css';
+import LoginForm from '../auth/LoginForm';
+import SignUpForm from '../user/SignUpForm';
+import React, { useState, useEffect } from 'react';
+import Feed from '../feed/Feed';
+import Home from '../home/Home';
+import Navbar from '../navbar/Navbar';
+import { useNavigate, Routes, Route } from 'react-router-dom';
+import Profile from '../profile/Profile';
+import Account from '../account/account';
+import TimeAgo from 'javascript-time-ago';
+import Posts from '../posts/Posts';
 
-import Account from "../account/account";
-import TimeAgo from "javascript-time-ago";
-import Posts from "../posts/Posts";
-
-import en from "javascript-time-ago/locale/en.json";
+import en from 'javascript-time-ago/locale/en.json';
 
 TimeAgo.addDefaultLocale(en);
 const App = () => {
-  const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [token, setToken] = useState(window.localStorage.getItem('token'));
 
   return (
-    <div className="app-container">
-      <Navbar token={token} setToken={setToken} />
+    <div className='app-container'>
+      <Navbar navigate={useNavigate} token={token} setToken={setToken} />
       <Routes>
-        <Route path="/" element={<Home navigate={useNavigate()} />} />
-        <Route path="/account" element={<Account navigate={useNavigate()} />} />
-        <Route path="/posts" element={<Posts navigate={useNavigate()} />} />
+        <Route path='/' element={<Home navigate={useNavigate()} />} />
+        <Route path='/account' element={<Account navigate={useNavigate()} />} />
+        <Route path='/posts' element={<Posts navigate={useNavigate()} />} />
         <Route
-          path="/login"
+          path='/login'
           element={<LoginForm navigate={useNavigate()} token={setToken} />}
         />
         <Route
-          path="/signup"
+          path='/signup'
           element={<SignUpForm navigate={useNavigate()} />}
         />
-        <Route path="/profile" element={<Profile navigate={useNavigate()} />} />
+        <Route path='/profile' element={<Profile navigate={useNavigate()} />} />
       </Routes>
     </div>
   );
