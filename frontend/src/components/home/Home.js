37a3559aaf7e239a7ca04dbd13e6a317a880cf2b
Tom Rolfe <tomrolfe20@gmail.com>
2023-02-17 13:26:12 +0000

    changed homepage alt desc

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styles from "./Home.module.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Home.module.css';
 
 const Home = ({ navigate }) => {
   return (
@@ -9,21 +9,24 @@ const Home = ({ navigate }) => {
       <p>
         Acebook is a social media platform that allows users to connect with
         friends, family, organizations and public figures. As part of the
-        community you can interact to other users and share pictures and posts.{" "}
+        community you can interact to other users and share pictures and posts.{' '}
       </p>
       <p></p>
       <ul className={styles.links}>
-        <Link to="/login">
+        <Link to='/login'>
           <button>Login</button>
         </Link>
         <p></p>
-        <Link to="/signup">
+        <Link to='/signup'>
           <button>Sign Up</button>
         </Link>
         <p></p>
       </ul>
       <div>
-        <img src="/images/noback-community-acebook.svg" alt="home" />
+        <img
+          src='/images/noback-community-acebook.svg'
+          alt='People from around the world connecting on social media'
+        />
       </div>
     </div>
   );
