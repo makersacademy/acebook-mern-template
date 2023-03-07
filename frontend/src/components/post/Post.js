0035da0cae5125e0a6dd06ff0be88dd4d035e1aa
Adam Evans <addauk@gmail.com>
2023-03-07 14:34:48 +0000

    created relation between posts and users, included user id in posts request, store userid in local and clear when logged out

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
-const Post = ({post}) => {
-  return(
-    <article data-cy="post" key={ post._id }>{ post.message }</article>
-  )
-}
+const Post = ({ post }) => {
+  return (
+    <article data-cy="post" key={post._id}>
+      Post from {post.user}:{post.message}
+    </article>
+  );
+};
 
 export default Post;
