d451cd9eb51f04171c1a9c2c795ca6048fa756e7
Luke Lai <lukelai.dev@gmail.com>
2023-03-08 16:20:55 +0000

    Fix test and code for make a new post feature

--- frontend/src/components/feed/Feed.cy.js
+++ frontend/src/components/feed/Feed.cy.js
@@ -1,85 +1,44 @@
-import Feed from './Feed'
-import { mount } from 'cypress/react'
-import React from 'react'
-const navigate = () => {}
+import Feed from "./Feed";
+const navigate = () => {};
 
 describe("Feed", () => {
   it("Calls the /posts endpoint and lists all the posts", () => {
-    window.localStorage.setItem("token", "fakeToken")
-    
-    cy.intercept('GET', '/posts', (req) => {
-        req.reply({
-          statusCode: 200,
-          body: { posts: [
-            {_id: 1, message: "Hello, world"},
-            {_id: 2, message: "Hello again, world"}
-          ] }
-        })
-      }
-    ).as("getPosts")
+    window.localStorage.setItem("token", "fakeToken");
 
-    cy.mount(<Feed navigate={navigate}/>)
-    
-    cy.wait("@getPosts").then(() =>{
-      cy.get('[data-cy="post"]')
-      .should('contain.text', "Hello, world")
-      .and('contain.text', "Hello again, world")
-    })
-  })
-
-  it("should create a new post", () => {
-    window.localStorage.setItem("token", "fakeToken")
-    cy.intercept('POST', '/posts', (req) => {
+    cy.intercept("GET", "/posts", (req) => {
       req.reply({
         statusCode: 200,
         body: {
-          post: {
-            _id: 1,
-            message: "This is a test post"
-          },
-        }
+          posts: [
+            { _id: 1, message: "Hello, world" },
+            { _id: 2, message: "Hello again, world" },
+          ],
+        },
       });
-    }).as('newPostCreation');
+    }).as("getPosts");
 
-    cy.mount(<Feed navigate={navigate}/>)
+    cy.mount(<Feed navigate={navigate} />);
 
-    cy.get('form').within(() => {
-      cy.get('input[type="text"]').type("This is a test post");
-      cy.get('button[type="submit"]').click();
-    })
-
-    cy.wait('@newPostCreation').then((interception) => {
-      expect(interception.request.body).to.deep.equal({
-        message: 'This is a test post',
-      });
+    cy.wait("@getPosts").then(() => {
+      cy.get('[data-cy="post"]')
+        .should("contain.text", "Hello, world")
+        .and("contain.text", "Hello again, world");
     });
-
-    cy.get('#feed').should('contain', 'This is a test post');
   });
 
-  // This test currently fails with the following error: 
-  // CypressError: `cy.wait()` could not find a registered 
-  // alias for: `@newPostCreation`. You have not aliased anything yet.
+  it("sends a post request to /posts and response a OK message", () => {
+    window.localStorage.setItem("token", "fakeToken");
 
-  xit("should create a new post", () => {
-    cy.mount(<Feed navigate={navigate}/>)
-    window.localStorage.setItem("token", "fakeToken")
+    cy.intercept("POST", "/posts", { message: "OK" }).as("newPostRequest");
+    cy.mount(<Feed navigate={navigate} />);
 
-    cy.intercept('POST', 'http://localhost:3000/posts', (req) => {
-      req.reply((res) => {
-        res.send({post: {
-          _id: 1,
-          message: "This is a test post"
-        },
-        })
-      })
-    })
-    cy.get('input[type="text"]').type("This is a test post");
-    cy.get('button[type="submit"]').click();
-    cy.get('[data-cy="post"]').should('contain.text', "This is a test post")
-})
+    cy.get('input[data-cy="post-input"]').should("exist");
+    cy.get('input[data-cy="post-input"]').type("This is a new post");
+    cy.get('button[data-cy="form-submit"]').should("exist");
+    cy.get('button[data-cy="form-submit"]').click();
+    cy.wait(1000);
+    cy.wait("@newPostRequest")
+      .its("response.body.message")
+      .should("equal", "OK");
+  });
 });
-
-// The above test was my alternative to the other 'should create
-// a new post' that Sameera wrote; I have skipped it because running
-// it causes both tests to fail.
--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,72 +1,76 @@
-import React, { useEffect, useState } from 'react';
-import Post from '../post/Post'
+import React, { useEffect, useState } from "react";
+import Post from "../post/Post";
 
 const Feed = ({ navigate }) => {
   const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token"));
-  const [newPost, setNewPost] = useState('')
+  const [newPost, setNewPost] = useState("");
 
   useEffect(() => {
-    if(token) {
+    if (token) {
       fetch("/posts", {
         headers: {
-          'Authorization': `Bearer ${token}`
-        }
+          Authorization: `Bearer ${token}`,
+        },
       })
-        .then(response => response.json())
-        .then(async data => {
-          window.localStorage.setItem("token", data.token)
-          setToken(window.localStorage.getItem("token"))
+        .then((response) => response.json())
+        .then(async (data) => {
+          window.localStorage.setItem("token", data.token);
+          setToken(window.localStorage.getItem("token"));
           setPosts(data.posts);
-        })
+        });
     }
-  }, [])
-    
+  }, []);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     await fetch("/posts", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        'Authorization': `Bearer ${token}`
+        Authorization: `Bearer ${token}`,
       },
-      body: JSON.stringify({ "message": newPost })
+      body: JSON.stringify({ message: newPost }),
     });
     setPosts([...posts, newPost]);
-    setNewPost('');
-    window.location.reload(true)
-  }
-
+    setNewPost("");
+    navigate(0);
+  };
 
   const logout = () => {
-    window.localStorage.removeItem("token")
-    navigate('/login')
-  }
-  
-    if(token) {
-      return(
-        <>
-          <h2>Posts</h2>
-            <button onClick={logout}>
-              Logout
-            </button>
-            <form onSubmit={handleSubmit}>
-              <label>
-                Add a new post:
-                <input type="text" value={newPost} onChange={(e) => setNewPost(e.target.value)} />
-              </label>
-              <button type="submit">Post</button>
-            </form>
-          <div id='feed' role="feed">
-              {[...posts].reverse().map(
-                (post) => ( <Post post={ post } key={ post._id } /> )
-              )}
-          </div>
-        </>
-      )
-    } else {
-      navigate('/signin')
-    }
-}
+    window.localStorage.removeItem("token");
+    navigate("/login");
+  };
 
-export default Feed; 
+  if (token) {
+    return (
+      <>
+        <h2>Posts</h2>
+        <button onClick={logout}>Logout</button>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Add a new post:
+            <input
+              type="text"
+              value={newPost}
+              onChange={(e) => setNewPost(e.target.value)}
+              data-cy="post-input"
+            />
+          </label>
+          <button type="submit" data-cy="form-submit">
+            Post
+          </button>
+        </form>
+        <div id="feed" role="feed">
+          {[...posts].reverse().map((post) => (
+            <Post post={post} key={post._id} />
+          ))}
+        </div>
+      </>
+    );
+  } else {
+    navigate("/signin");
+  }
+};
+
+export default Feed;
