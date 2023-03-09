806ae4250ef5248b30afb6f67200ad14aa8cfe91
marie taylor <marietaylortailor@hotmail.com>
2023-03-09 14:56:13 +0000

    save to merge updated files from main

--- api/models/post.js
+++ api/models/post.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
   message: { type: String, required: true },
-  date: { type: Date, required: true },
-  //time: { type: Date, default: Date.now },
+  //date: { type: Date, required: true },
+  time: { type: Date, default: Date.now },
   //timestamp
-  time: { type: Date, default: new ISODate() }, // timestamp
+  //time: { type: Date, default: new ISODate() }, // timestamp
   //time: { type: Date, default: new Date() }, //date Object
 });
 
