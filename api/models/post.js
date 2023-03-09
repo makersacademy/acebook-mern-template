e3843d10f239b6bf96c26467e39aee39ac9fb968
marie taylor <marietaylortailor@hotmail.com>
2023-03-09 12:36:01 +0000

    post_date_ displayed and updating to current time on refresh instead of date created

--- api/models/post.js
+++ api/models/post.js
@@ -4,6 +4,7 @@ const PostSchema = new mongoose.Schema({
   message: { type: String, required: true },
   date: { type: Date, required: true },
   time: { type: Date, default: Date.now }, //timestamp
+  //time: { type: Date, default: new ISODate()  }, // timestamp
 });
 
 const Post = mongoose.model("Post", PostSchema);
--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,18 +1,19 @@
 import React from "react";
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
-const newPost = {
-  time: Date.now(),
-  ...post,
-};
+// const newPost = {
+//   time: Date.now(),
+//   ...post,
+// };
 const Post = ({ post }) => {
-  const formattedDate = formatDistanceToNow(new Date(), {
-    addSuffix: true,
-  });
+  // const formattedDate = formatDistanceToNow(new Date(), {
+  //   addSuffix: true,
+  // });
+
   return (
     <article data-cy="post" key={post._id}>
       {post.message}
-      Published {formattedDate}
+      Published {post.time}
     </article>
   );
 };
