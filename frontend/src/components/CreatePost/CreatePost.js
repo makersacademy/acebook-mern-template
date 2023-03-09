97a10749618d89462ea729388dff96ff533de8d2
Max Allan-Smith <Maxibon_1195@hotmail.com>
2023-03-09 16:25:20 +0000

    Completed the final BASIC design for feed page.

--- frontend/src/components/CreatePost/CreatePost.js
+++ frontend/src/components/CreatePost/CreatePost.js
@@ -1,5 +1,5 @@
-
 import React, { useState } from "react";
+import './CreatePost.css'
 
 function CreatePost() {
   const [postMessage, setPostMessage] = useState("");
@@ -9,7 +9,7 @@ function CreatePost() {
     //const token = localStorage.getItem("jwt");
     const token = window.localStorage.getItem("token");
 
-    const formattedMessge = { "message":  postMessage }
+    const formattedMessge = { "message": postMessage }
 
     fetch('/posts', {
       method: 'POST',
@@ -19,33 +19,24 @@ function CreatePost() {
         'Authorization': `Bearer ${token}`
       },
     })
-    .then(response => {
-      if (response.ok) {
-        console.log('Success.')
-      } else {
-        throw new Error('Something went wrong.');
-      }
-    })
-    .catch(error => {
-      console.log(error)
-    })
+      .then(response => {
+        if (response.ok) {
+          console.log('Success.')
+        } else {
+          throw new Error('Something went wrong.');
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
 
     setPostMessage("");
   };
 
   return (
-    <form onSubmit={handleSubmit} className='post-form'>
-      <label htmlFor="post-text" className="post-label">What's on your mind?</label>
-      <textarea
-        className="post-input"
-        id="post-input"
-        value={postMessage}
-        onChange={(event) => {
-          setPostMessage(event.target.value)
-          //console.log(event.target.value)
-        }}
-      />
-      <button id="submit" className="submit-button">Post</button>
+    <form id='formCreatePost' onSubmit={handleSubmit} className='post-form'>
+      <textarea id="textCreatePost" className="post-input" placeholder="What's on your mind?" value={postMessage} onChange={(event) => { setPostMessage(event.target.value) }}></textarea>
+      <button id="submitButton" className="submit-button">Post</button>
     </form>
   );
 }
