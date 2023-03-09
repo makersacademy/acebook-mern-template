4377431e733ff8b3345c61de79763d2e29c7284a
Luke Lai <lukelai.dev@gmail.com>
2023-03-09 11:14:32 +0000

    Refactoring

--- frontend/cypress/e2e/making_a_post.cy.js
+++ frontend/cypress/e2e/making_a_post.cy.js
@@ -1,9 +1,9 @@
 describe("Make a new post", () => {
   before(() => {
     cy.signup("name", "surname", "someone@example.com", "password");
+    cy.wait(1000);
   });
   it("sign up, login and make a new post", () => {
-    cy.wait(1000);
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("password");
     cy.get("#submit").click();
