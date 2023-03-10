e72fdfee3f6b0fe1d9f1afe4e49598a34be1783b
Max Allan-Smith <Maxibon_1195@hotmail.com>
2023-03-10 17:46:02 +0000

    Added some basic CSS for the homepage. Functionality still needs to be implemeneted.

--- frontend/src/components/homepage/Homepage.js
+++ frontend/src/components/homepage/Homepage.js
@@ -1,13 +1,45 @@
 import Feed from "../feed/Feed";
 import { useContext } from 'react'
 import { UserContext } from '../../context/UserContext';
+import '../homepage/homepage.css'
 
 const Homepage = (props) => {
-    const {userInfo} = useContext(UserContext);
+    const { userInfo } = useContext(UserContext);
 
     return (
         <>
-            {userInfo ? <Feed navigate={props.navigate}/> : <div>You need an account to view and create posts.</div>}
+            {userInfo ?
+                <Feed navigate={props.navigate} />
+                :
+                <>
+                    <div className="recentLogins">
+                        <div className="logo">
+                            <h1>Recent Logins</h1>
+                        </div>
+                        <div className="instructions">Click your picture or add an account.</div>
+                        <div className="recentLogins_Profiles">
+                            <div className="addNewProfile">
+                                <a className="newProfile" href="/login">
+                                    <div className="addAccountLabel">Add Account</div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="homepage">
+                        <div className="loginForm">
+                            <form>
+                                <input type="email" id="email" name="email" class="form-input" placeholder="Email Address" required></input>
+                                <input type="password" id="password" name="password" class="form-input" placeholder="Password" required></input>
+                                <button type="submit" className="buttonLogin">Login</button>
+                                <a className="linkForgottenPassword" href="">Forgotten password?</a>
+                                <div class="lineBreak"></div>
+                                <button type="button" className="buttonSignUp" href="/signup/">Sign Up</button>
+                            </form>
+                        </div>
+                    </div>
+                </>
+            }
         </>
     )
 }
