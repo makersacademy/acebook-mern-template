6ac428dafad393e93ef5848e3682883d694f7329
Moeid Haider <moeidhaider9@gmail.com>
2023-03-10 11:08:29 +0000

    Added tests for time stamps

--- api/spec/models/post.spec.js
+++ api/spec/models/post.spec.js
@@ -15,6 +15,27 @@ describe("Post model", () => {
     expect(post.message).toEqual("some message");
   });
 
+  it("has a timestamp for when created ", () => {
+    const post = {
+      _id: "1",
+      message: "Hello World",
+      user: "John",
+      createdAt: new Date(),
+    };
+    const now = new Date();
+    // const options = {
+    //   hour12: false,
+    //   hour: '2-digit',
+    //   minute: '2-digit',
+    // };
+    // const formattedTime = now.toLocaleString('en-US', options);
+
+    expect(post.message).toEqual("Hello World");
+    expect(post.createdAt).toEqual(now);
+    
+  });
+
+
   it("can list all posts", (done) => {
     Post.find((err, posts) => {
       expect(err).toBeNull();
