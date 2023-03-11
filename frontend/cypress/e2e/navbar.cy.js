e658192759f3713c0258c089b1878a8a6c718fb1
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-03-11 15:17:40 +0000

    fix current frontend e2e tests

--- frontend/cypress/e2e/navbar.cy.js
+++ frontend/cypress/e2e/navbar.cy.js
@@ -24,14 +24,14 @@ describe("Signing in", () => {
         cy.get("#email").type("someone@example.com");
         cy.get("#password").type("password");
         cy.get("#submit").click();
-        cy.get("button:contains(Logout)").should("be.visible");
+        cy.get("button:contains(Log Out)").should("be.visible");
       });
 
           
       it("Displays the login / signup before logging in", () => {
         cy.visit("/");
         cy.get("a:contains(Login)").should("be.visible");
-        cy.get("a:contains(Signup)").should("be.visible");
+        cy.get("a:contains(Sign Up)").should("be.visible");
       });
     
     });
