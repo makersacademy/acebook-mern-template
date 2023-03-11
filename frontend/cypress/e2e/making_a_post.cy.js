b953786ac22311842890b537a41bcf5b16a70260
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-03-11 17:00:14 +0000

    add date & time to posts and update post.cy.js component test

--- frontend/cypress/e2e/making_a_post.cy.js
+++ frontend/cypress/e2e/making_a_post.cy.js
@@ -1,11 +1,11 @@
 describe("Make new post", () => {
 
     before(() => {
-      cy.signup("user@email.com", "12345678");
-      cy.visit("/login");
-      cy.get("#email").type("someone@example.com");
-      cy.get("#password").type("password");
-      cy.get("#submit").click();
+        cy.signup("user@email.com", "12345678");
+        cy.visit("/login");
+        cy.get("#email").type("someone@example.com");
+        cy.get("#password").type("password");
+        cy.get("#submit").click();
     })
 
     it("Posts a new message", () => {
@@ -15,5 +15,5 @@ describe("Make new post", () => {
         cy.wait(2000);
         cy.reload();
         cy.get("div:contains(hello world)").should("be.visible");
-    });
+        });
 });
--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import './Post.css'
 
+
 const Post = ({post}) => {
+
+  const postedAt = post.createdAt;
+  const date = new Date(postedAt);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  const postDate = (day + "-" + month + "-" + year)
+  const postTime = (hours + ":" + minutes)
+
+
   return(
     <>
       <article 
@@ -9,7 +23,9 @@ const Post = ({post}) => {
       data-cy="post" 
       key={ post._id }
       className="row-span-1 m-3 py-8 bg-white rounded-xl shadow-lg space-y-2"
-      >{ post.message }</article>
+      >{ post.message }<br/>
+      {postDate} posted at: {postTime}
+      </article>
     </>
   )
 }
