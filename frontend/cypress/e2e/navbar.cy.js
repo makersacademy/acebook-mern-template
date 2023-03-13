88c3dc7b288d980e716028fd2c0a552d2654b32b
annamaggg <annamagnusson99@gmail.com>
2023-03-13 10:49:28 +0000

    navbar e2e test refactored

--- frontend/cypress/e2e/navbar.cy.js
+++ frontend/cypress/e2e/navbar.cy.js
@@ -1,68 +1,44 @@
 //cy.signup("name", "surname", "someone@example.com", "password");
 
+beforeEach( () => {
+  cy.visit("/signup")
+  cy.get('nav').should('not.exist')
+  cy.signup("name", "surname", "someone@example.com", "password");
+  cy.visit("/login");
+  cy.get('nav').should('not.exist')
+  cy.get("#email").type("someone@example.com");
+  cy.get("#password").type("password");
+  cy.get("#submit").click();
+  cy.wait(1000)
+ })
 
 describe('Navbar', () => {
   it('shows navbar once user has signed in', () => {
-    cy.visit("/signup")
-    cy.get('nav').should('not.exist')
-    cy.signup("name", "surname", "someone@example.com", "password");
 
-    cy.visit("/login");
-    cy.get('nav').should('not.exist')
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
-    cy.wait(1000)
     cy.get('nav').should('exist').and('contain','Home')
+
   })
 
   it('redirects to home page when home button clicked', () => {
-    cy.visit("/signup")
-    cy.get('nav').should('not.exist')
-    cy.signup("name", "surname", "someone@example.com", "password");
 
-    cy.visit("/login");
-    cy.get('nav').should('not.exist')
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
-    cy.wait(1000)
     cy.get('nav').should('exist').and('contain','Home')
-
     cy.get('[data-cy="homeButton"]').click();
     cy.url().should('include', '/posts')
+
   })
 
   it('redirects to login page when logout button clicked', () => {
-    cy.visit("/signup")
-    cy.get('nav').should('not.exist')
-    cy.signup("name", "surname", "someone@example.com", "password");
 
-    cy.visit("/login");
-    cy.get('nav').should('not.exist')
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
-    cy.wait(1000)
     cy.get('nav').should('exist').and('contain','Home')
-
     cy.get('[data-cy="logoutButton"]').click();
     cy.url().should('include', '/login')
+
   })
 
   it('shows first name in nav bar', () => {
-    cy.visit("/signup")
-    cy.get('nav').should('not.exist')
-    cy.signup("name", "surname", "someone@example.com", "password");
 
-    cy.visit("/login");
-    cy.get('nav').should('not.exist')
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
-    cy.wait(1000)
     cy.get('nav').should('exist').and('contain','Home')
-
     cy.get('[data-cy="user-first-name"]').should('contain.text', 'name')
+    
   })
 })
