588ffaabafabab8e22a12826924e6befd78acc7f
Chris Leech <chrisleech@monzo.com>
2023-03-13 17:00:03 +0000

    Added new routes for post comments

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -2,9 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getAllPosts, createPost } = require("../controllers/posts");
+const {
+  getAllPosts,
+  createPost,
+  createComment,
+  getPostComments,
+} = require("../controllers/posts");
 
 router.get("/", getAllPosts);
 router.post("/", createPost);
+router.post("/comment", createComment);
+router.get("/comment", getPostComments);
 
 module.exports = router;
