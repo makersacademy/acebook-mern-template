a3199b8071d9e5dac423e1527651980dfcaed8e4
Adam Evans <addauk@gmail.com>
2023-03-13 15:51:56 +0000

    frontend support for reactive state

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -3,6 +3,7 @@ import { formatDistanceToNow } from "date-fns";
 
 const Post = ({ post }) => {
   const [likes, setLikes] = useState(post.likes.length || 0);
+  const [isLiked, setLiked] = useState(post.liked);
   const createdAt = new Date(post.createdAt);
   const result = formatDistanceToNow(createdAt, { addSuffix: true });
 
@@ -17,12 +18,13 @@ const Post = ({ post }) => {
     response.json().then(async (data) => {
       window.localStorage.setItem("token", data.token);
       setLikes(data.likes);
+      setLiked(data.liked);
     });
   };
   return (
     <article data-cy="post" key={post._id}>
       Post from {post.user.name}: {post.message} ({result})
-      <button onClick={toggleLike}>Toggle Like</button>
+      <button onClick={toggleLike}>{isLiked ? "unlike" : "like"}</button>
       Like count: {likes}
     </article>
   );
