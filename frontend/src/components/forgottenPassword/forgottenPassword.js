909dd025992a6c3dd0fbd0553f2d52d028ced3ee
Max Allan-Smith <Maxibon_1195@hotmail.com>
2023-03-13 14:49:34 +0000

    Added a forgotten-password page

---
+++ frontend/src/components/forgottenPassword/forgottenPassword.js
@@ -0,0 +1,13 @@
+import '../forgottenPassword/forgottenPassword.css'
+import React, { Component } from 'react';
+
+class ForgottenPassword extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <h1>Hello</h1>
+    );
+  }
+}
+ 
+export default ForgottenPassword;
