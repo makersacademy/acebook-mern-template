b0b1c79800ecdd196265542625c09941ad774a29
Chris Leech <chrisleech@monzo.com>
2023-03-13 17:00:50 +0000

    Created new controller methods to handle the creation and display of comments

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -1,4 +1,5 @@
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 const generateToken = require("../models/token_generator");
 
 const getAllPosts = async (req, res) => {
@@ -25,4 +26,60 @@ const createPost = async (req, res) => {
   }
 };
 
-module.exports = { getAllPosts, createPost };
+const createComment = async (req, res) => {
+  try {
+    const { userId, postId } = req;
+
+    const comment = new Comment(req.body);
+    comment.author = userId;
+    await comment.save();
+
+    const post = await Post.find({ _id: postId });
+
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    const updatedPost = await Post.findOneAndUpdate(
+      { _id: req.body.postId },
+      { $push: { comments: comment.id } },
+      { new: true }
+    );
+
+    const token = await generateToken(req.userId);
+
+    return res.status(201).json({ updatedPost: updatedPost || post[0], token });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const getPostComments = async (req, res) => {
+  try {
+    const { postId } = req.query;
+
+    const comments = await Comment.find({ postId }).populate(
+      "author",
+      "username"
+    );
+
+    if (!comments) {
+      return res.status(404).json({ error: "No comments found for this post" });
+    }
+
+    // return res.status(200).json({ comments });
+    const postComments = comments.map((comment) => {
+      return {
+        id: comment.id,
+        message: comment.message,
+        author: comment.author.username,
+        createdAt: comment.createdAt,
+      };
+    });
+    return res.status(200).json({ postComments });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+module.exports = { getAllPosts, createPost, createComment, getPostComments };
