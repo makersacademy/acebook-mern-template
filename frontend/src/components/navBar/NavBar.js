81f457b03121452da5e7684e79ce246544c909a2
Joel Powell <joelhpowell@icloud.com>
2023-03-15 12:19:36 +0000

    uncomment navbar button

--- frontend/src/components/navBar/NavBar.js
+++ frontend/src/components/navBar/NavBar.js
@@ -87,19 +87,19 @@ const NavBar = () => {
                   data-cy="menu_items"
                   className="absolute right-0 z-10 mt-2 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                 >
-                  {/* <Menu.Item> */}
-                  {/*  {({ active }) => ( */}
-                  {/*    <a */}
-                  {/*      href="#" */}
-                  {/*      className={classNames( */}
-                  {/*        active ? "bg-gray-100" : "", */}
-                  {/*        "block px-4 py-2 text-sm text-gray-700" */}
-                  {/*      )} */}
-                  {/*    > */}
-                  {/*      Your Profile */}
-                  {/*    </a> */}
-                  {/*  )} */}
-                  {/* </Menu.Item> */}
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#"
+                        className={classNames(
+                          active ? "bg-gray-100" : "",
+                          "block px-4 py-2 text-sm text-gray-700"
+                        )}
+                      >
+                        Your Profile
+                      </a>
+                    )}
+                  </Menu.Item>
                   <Menu.Item>
                     {({ active }) => (
                       <button
