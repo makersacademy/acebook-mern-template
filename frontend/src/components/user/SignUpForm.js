cea72e8a5ca814dfee39b13d4020873a4b4cf4bb
Konrad <kdun.work@gmail.com>
2023-04-04 15:41:12 +0100

    Added name field to the schema.

--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 const SignUpForm = ({ navigate }) => {
 
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -13,7 +14,7 @@ const SignUpForm = ({ navigate }) => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ email: email, password: password })
+      body: JSON.stringify({ name: name, email: email, password: password })
     })
       .then(response => {
         if(response.status === 201) {
@@ -22,6 +23,10 @@ const SignUpForm = ({ navigate }) => {
           navigate('/signup')
         }
       })
+  }
+
+  const handleNameChange = (event) => {
+    setName(event.target.value)
   }
 
   const handleEmailChange = (event) => {
@@ -35,6 +40,7 @@ const SignUpForm = ({ navigate }) => {
 
     return (
       <form onSubmit={handleSubmit}>
+          <input placeholder="Name" id="name" type='text' value={ name } onChange={handleNameChange} />
           <input placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
           <input placeholder="Password" id="password" type='password' value={ password } onChange={handlePasswordChange} />
         <input id='submit' type="submit" value="Submit" />
