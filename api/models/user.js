ffa478252fc6510b3ad30cebd056b970e8995e8e
Konrad <kdun.work@gmail.com>
2023-04-04 16:57:29 +0100

    Added an avatarUrl to the schema, with the default value linking to a generic image.

--- api/models/user.js
+++ api/models/user.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  avatarUrl: { 
+    type: String, 
+    default: 'https://localhost:8080/images/defaultAvatar.png',
+    required: true 
+  }
 });
 
 const User = mongoose.model("User", UserSchema);
