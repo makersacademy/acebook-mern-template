b4de33ca7bf13c59ba2f6108a66032744f6d8755
samuelbadru <samuelbadru@outlook.com>
2023-04-06 12:11:02 +0100

    Does not accept signing up with an email already in use

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -2,15 +2,22 @@ const User = require("../models/user");
 
 const UsersController = {
   Create: (req, res) => {
-    const user = new User(req.body);
-    user.save((err) => {
-      if (err) {
-        res.status(400).json({message: 'Bad request'})
+    User.findOne({ email: req.body.email }, (err, user) => {
+      if (user) {
+        console.log( `User is: ${user}`)
+        res.status(409).json({message: 'Email already exists'});
       } else {
-        res.status(201).json({ message: 'OK' });
+        const user = new User(req.body);
+        user.save((err) => {
+          if (err) {
+            res.status(400).json({message: 'Bad request'})
+          } else {
+            res.status(201).json({ message: 'OK' });
+          }
+        });
       }
-    });
-  },
+    }) 
+  }
 };
 
 module.exports = UsersController;
