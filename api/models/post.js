56efcf0416b77635b6ce99949f4c1da0231d4efe
Sarah <sarahdavies186@gmail.com>
2023-04-06 14:34:50 +0100

    added date to post

--- api/models/post.js
+++ api/models/post.js
@@ -8,7 +8,7 @@ const PostSchema = new mongoose.Schema({
     // required: true,
   }
   
-});
+},{ timestamps: true });
 
 const Post = mongoose.model("Post", PostSchema);
 
--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -4,16 +4,21 @@ import { faThumbsUp } from "@fortawesome/free-solid-svg-icons/faThumbsUp";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import moment from "moment";
 
 const Post = ({ post }) => {
+  const date = moment(`${post.createdAt}`).format("MMMM Do YYYY, h:mm:ss a");
+  console.log(date)
   return (
     <div>
       <article data-cy="post" key={post._id}>
       <div> {console.log(post)}</div>
         <div> <img src={post.user.image} className="profileImage"></img> {post.user && post.user.name}</div>
         <div>{post.message}</div>
+        <div>{date}</div>
+        <FontAwesomeIcon icon={faThumbsUp} />
       </article>
-      <FontAwesomeIcon icon={faThumbsUp} />
+      
     </div>
   );
 };
