67b7859c3fcc54accf5b3bed646a575d97cf9041
samuelbadru <samuelbadru@outlook.com>
2023-04-06 14:43:39 +0100

    Signupform tweaks to reject duplicate email address

--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -4,6 +4,7 @@ const SignUpForm = ({ navigate }) => {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -18,7 +19,10 @@ const SignUpForm = ({ navigate }) => {
       .then(response => {
         if(response.status === 201) {
           navigate('/login')
-        } else {
+        } else if (response.status === 409) {
+          setErrorMessage('Email address is already in use');
+        } 
+        else {
           navigate('/signup')
         }
       })
@@ -40,6 +44,7 @@ const SignUpForm = ({ navigate }) => {
           <input placeholder="Password" id="password" type='password' value={ password } onChange={handlePasswordChange} />
           <input id='submit' type="submit" value="Submit" />
         </form>
+        {errorMessage && <p>{errorMessage}</p>}
         Have an account already? <a href="/login">Log in</a>
       </div>
       
