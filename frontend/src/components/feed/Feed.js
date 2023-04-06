26c584afdb0c5193d1b3e2fd90cf10a2e3b67bc2
Callum <Callum@hotmail.com>
2023-04-06 16:10:08 +0100

    added comment ability

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -4,6 +4,9 @@ import Post from '../post/Post'
 const Feed = ({ navigate }) => {
   const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [newPost, setNewPost] = useState("")
+  const [likes, setLikes] = useState(0) // to be used for count of like
+  const [comments, setComments] = useState(0) // to be used for count of comments
 
   useEffect(() => {
     if(token) {
@@ -26,6 +29,32 @@ const Feed = ({ navigate }) => {
     window.localStorage.removeItem("token")
     navigate('/login')
   }
+
+  const new_post = async (event) => {
+    event.preventDefault();
+
+    fetch( '/posts', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json', 
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify({message: newPost})
+    })
+    .then(response => {
+      if(response.status === 201) {
+        navigate('/posts');
+        window.location.reload();
+      } else {
+        throw new Error('Failed to create post');
+      }
+    })
+}
+
+  const handleNewPostChange = (event) => {
+    setNewPost(event.target.value);
+  }
+
   
     if(token) {
       return(
@@ -34,6 +63,10 @@ const Feed = ({ navigate }) => {
             <button onClick={logout}>
               Logout
             </button>
+            <form>
+              <input type='text' id='post' value={newPost} onChange={handleNewPostChange}></input>
+              <button onClick={new_post}>Post</button>
+            </form>
           <div id='feed' role="feed">
               {posts.map(
                 (post) => ( <Post post={ post } key={ post._id } /> )
