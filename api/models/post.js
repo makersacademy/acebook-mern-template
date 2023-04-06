31fc5672de69714aafe25648a6c3daa781a3241e
Dilan Patel <patel_dilan@hotmail.com>
2023-04-06 17:42:00 +0100

    Added 'likes' to PostSchema

--- api/models/post.js
+++ api/models/post.js
@@ -1,3 +1,4 @@
+// const { Decimal128 } = require("mongodb");
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
@@ -6,8 +7,11 @@ const PostSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     // required: true,
+  },
+  likes: {
+    type: Number,
+    default: 0
   }
-  
 });
 
 const Post = mongoose.model("Post", PostSchema);
