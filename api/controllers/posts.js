ab5f821db082c469fef53aa4daebc663409c00ed
ChangWynn <huynhchang.one@gmail.com>
2023-04-11 01:29:17 +0100

    one button to send them all

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -4,29 +4,20 @@ const Image = require('../models/image')
 const TokenGenerator = require("../models/token_generator");
 const { ObjectId } = require("mongodb");
 
-
 const PostsController = {
+  
   Index: async (req, res) => {
-    const user = await Users.findById(req.user_id)
+    const user = await findUser(req.user_id)  // method further down
+
     Post.find(async (err, posts) => {
       if (err) { throw err }
-
       const token = await TokenGenerator.jsonwebtoken(req.user_id)
       res.status(200).json({ posts: posts, user: user, token: token });
     }).sort({ createdAt: -1 });
   },
   
   Create: async (req, res) => {
-    const post = new Post(req.body);
-    const user = await Users.findById(req.user_id)
-    post.createdBy = user
-
-    if (req.file) { 
-      post.image = {
-        fileName: req.file.filename,
-        contentType: req.file.mimetype,
-      }
-    }
+    const post = await buildPostData(req) // method further down
 
     post.save(async (err) => {
       if (err) { throw err }
@@ -45,5 +36,29 @@ const PostsController = {
     })
   }
 };
+
+
+// HELPER METHODS -----------------
+
+const findUser = (userId) => {
+  return Users.findById(userId)
+}
+
+const retrieveImgData = (req) => {
+  return {
+    fileName: req.file.filename,
+    contentType: req.file.mimetype,
+  }
+}
+
+const buildPostData = async (req) => {
+  const post = new Post();
+  const user = await findUser(req.user_id)
+  
+  post.createdBy = user
+  post.message = req.body.post
+  if (req.file) { post.image = retrieveImgData(req) }
+  return post
+}
 
 module.exports = PostsController;
