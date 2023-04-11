34d7c5a0c3fc10ae7b03f99d9853a56947ec9293
samuelbadru <samuelbadru@outlook.com>
2023-04-11 15:24:11 +0100

    password hash put into database and when logging in this is checked

--- api/controllers/tokens.js
+++ api/controllers/tokens.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const User = require("../models/user");
 const TokenGenerator = require("../models/token_generator")
 
@@ -7,17 +8,28 @@ const SessionsController = {
     const email = req.body.email;
     const password = req.body.password;
 
-    User.findOne({ email: email }).then(async (user) => {
+    User.findOne({ email: email }).then((user) => {
       if (!user) {
         res.status(401).json({ message: "auth error" });
-      } else if (user.password !== password) {
-        res.status(402).json({ message: "auth error" });
       } else {
-        const token = await TokenGenerator.jsonwebtoken(user.id)
-        res.status(201).json({ token: token, message: "OK" });
+          bcrypt.compare(password, user.password, async (err, result) => {
+          if (err) {
+            res.status(401).json({message: 'Password encryption error'})
+          }
+          else if (result === false) {
+            res.status(402).json({message: 'Incorrect password'})
+          }
+          else {
+            const token = await TokenGenerator.jsonwebtoken(user.id)
+            res.status(201).json({ token: token, message: "OK" });
+          }
+        })
       }
     });
   }
 };
 
 module.exports = SessionsController;
+
+//else if (user.password !== password) {
+ // res.status(402).json({ message: "auth error" });
--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,20 +1,30 @@
+const bcrypt = require('bcrypt')
 const User = require("../models/user");
 
 const UsersController = {
   Create: (req, res) => {
     User.findOne({ email: req.body.email }, (err, user) => {
       if (user) {
-        console.log( `User is: ${user}`)
         res.status(409).json({message: 'Email already exists'});
       } else {
-        const user = new User(req.body);
-        user.save((err) => {
+        const saltRounds = 10;
+        bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
           if (err) {
-            res.status(400).json({message: 'Bad request'})
-          } else {
-            res.status(201).json({ message: 'OK' });
+            res.status(401).json({message: 'Password encryption error'})
           }
-        });
+          else {
+            req.body.password = hash;
+            const user = new User(req.body);
+            user.save((err) => {
+            if (err) {
+              res.status(400).json({message: 'Bad request'})
+            } else {
+              res.status(201).json({ message: 'OK' });
+            }
+            });
+          }
+        })
+        
       }
     }) 
   }
