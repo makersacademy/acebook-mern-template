fc0e461d90d99d54571ce8857118c653e19098b0
Kera <106094452+Keremesh@users.noreply.github.com>
2023-04-11 16:12:51 +0100

    did not allow me to switch branches without saving the navbar file

---
+++ frontend/src/components/navigation/NavBar.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+// import Button from "react-bootstrap/Button";
+// import Form from "react-bootstrap/Form";
+// import Container from "react-bootstrap/Container";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import {
+  faHouse,
+  faPlus,
+} from '@fortawesome/free-solid-svg-icons';
+
+
+
+const NavBar = () => {
+  // const [user, setUser] = useState({ name: 'John Doe', picture: 'https://placekitten.com/50/50' });
+
+  return (
+    <>
+      <br />
+      <Navbar bg="light" variant="light">
+
+        <Navbar.Brand href="#">
+          <h5>acebook.</h5>
+        </Navbar.Brand>
+
+        <Nav className="me-auto">
+          <Nav.Link href="#feed">
+            <FontAwesomeIcon icon={faHouse} size="xl" />
+            </Nav.Link>
+          <Nav.Link href="#create_post">
+            <FontAwesomeIcon icon={faPlus} size="2xl" />
+          </Nav.Link>
+          
+          {user && (
+            <Nav.Link href="#profile">
+              <img src={user.image} alt={user.name} />
+              <span>{user.name}</span>
+            </Nav.Link>
+          )};
+        </Nav>
+      </Navbar>
+    </>
+  );
+};
+
+
+export default NavBar;
+// add
+//   < Navbar fixed = "top" />
