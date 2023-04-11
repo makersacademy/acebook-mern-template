5d31e0f2cec4f54eaf0af0c4083b7ac285a4e2d9
kamahack <kamila.felger@gmail.com>
2023-04-11 15:46:32 +0200

    commit for pull

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Post = ({post}) => {
+
+  const [message, setMessage] = useState("")
+
+  const handleSubmit = async (event) => {
+
+  }
+
+   
   return(
+    
     <article data-cy="post" key={ post._id }>{ post.message }</article>
   )
 }
