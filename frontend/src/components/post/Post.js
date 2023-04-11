da8ca1854dc4a55dff1c89c27a8c6685bd85ff36
Josh Neuwford <josh.c.gosling@gmail.com>
2023-04-11 12:38:41 +0100

    refactor

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Post = ({post}) => {
+
   return(
-    <article data-cy="post" key={ post._id }>{ post.message }</article>
+    <>
+      <article data-cy="post" key={ post._id }>{ post.message }</article>
+    </>
+
   )
 }
 
