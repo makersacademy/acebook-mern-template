76a182fe188191695c48a0aaebeb9328c16135ac
Dilan Patel <patel_dilan@hotmail.com>
2023-04-11 07:42:13 +0100

    Added 'handleLikeClick' to post component that increments like counter on display and in database

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react";
+import LikeButton from "../likes/LikeButton"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp } from "@fortawesome/free-solid-svg-icons/faThumbsUp";
 import Container from "react-bootstrap/Container";
@@ -6,14 +7,33 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 const Post = ({ post }) => {
+
+  const [likes, setLikes] = useState(post.likes);
+
+  const handleLikeClick = async () => {
+    try {
+      await fetch(`/posts/${post._id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify({ likes: likes + 1 }),
+      });
+      console.log("Liked post with ID: ", post._id);
+      setLikes(likes + 1);
+    } catch (error) {
+      console.error("Error updating likes:", error);
+    }
+  };
+
   return (
     <div>
       <article data-cy="post" key={post._id}>
-        {" "}
-        <div>{post.user.name}</div>
-        <div>{post.message}</div>{" "}
+        <div>{post.user && post.user.name}</div>
+        <div>{post.message}</div>{" "} 
+        <LikeButton likes={post.likes} onClick={handleLikeClick} />
       </article>
-      <FontAwesomeIcon icon={faThumbsUp} />
     </div>
   );
 };
