a71f0086cc82d85a4d3eb2b0961cc64ae6f58a82
jpates1 <jamesandrewpates@gmail.com>
2023-04-11 09:13:05 +0100

    change to timestamp

--- api/models/post.js
+++ api/models/post.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-  message: String
+  message: String,
+  dateCreated: { type: Number, default: () => Math.floor(Date.now() / 1000) }
 });
 
 const Post = mongoose.model("Post", PostSchema);
