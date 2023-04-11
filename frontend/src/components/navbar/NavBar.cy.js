a50a61821bbd685b737380c7ac8cac6ed9aa9575
lizaebadi <liza-1@hotmail.co.uk>
2023-04-11 22:59:26 +0100

    created tests to check log out/feed/create a post are displayed

--- frontend/src/components/navbar/NavBar.cy.js
+++ frontend/src/components/navbar/NavBar.cy.js
@@ -1,9 +1,27 @@
 const navigate = () => {};
 
 describe("NavBar", () => {
-  it("displays the Acebook logo"), () => {
+  it("displays the Acebook logo", () => {
     cy.mount(<NavBar navigate={useNavigate()}/>);
     cy.get('[data-cy="logo-container"]')
     .find("img").should('be.visible');
-  };
+  });
+
+  it("displays link to log out", () => {
+    cy.mount(<NavBar navigate={useNavigate()}/>);
+    cy.get('[data-cy="menu-container"]')
+    .should('contain.text', "Log out");
+  });
+
+  it("displays link to feed", () => {
+    cy.mount(<NavBar navigate={useNavigate()}/>);
+    cy.get('[data-cy="menu-container"]')
+    .should('contain.text', "Feed");
+  });
+
+  it("displays link to create a post", () => {
+    cy.mount(<NavBar navigate={useNavigate()}/>);
+    cy.get('[data-cy="menu-container"]')
+    .should('contain.text', "Create a post");
+  });
 });
