5a40a242edcbc07a74e1021c42be15547895d1a1
jpates1 <jamesandrewpates@gmail.com>
2023-04-11 21:28:32 +0100

    changed to be a better date format

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+const formatDate = (timestamp) => {
+  const date = new Date(timestamp);
+  const formattedDate = date.toLocaleDateString();
+  const formattedTime = date.toLocaleTimeString();
+  return `${formattedDate} - ${formattedTime}`
+};
+
 const Post = ({post}) => {
+  const formattedDate = formatDate(post.dateCreated)
   return(
-    <article data-cy="post" key={ post._id }>{ post.message } { post.dateCreated }</article>
+    <article data-cy="post" key={ post._id }>
+      <p id="message">{ post.message } </p>
+      <p id="date">{ formattedDate }</p>
+    </article>
   )
 }
 
