fb07c1f4d80bc6a684e3f07ee2151143e138c9c6
jpates1 <jamesandrewpates@gmail.com>
2023-04-12 09:57:39 +0100

    styling updates

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import './Post.css';
+
 
 const formatDate = (timestamp) => {
   const date = new Date(timestamp);
   const formattedDate = date.toLocaleDateString();
-  const formattedTime = date.toLocaleTimeString();
+  const formattedTime = date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
   return `${formattedDate} - ${formattedTime}`
 };
 
 const Post = ({post}) => {
   const formattedDate = formatDate(post.dateCreated)
   return(
-    <article data-cy="post" key={ post._id }>
+    <article className="post-box" data-cy="post" key={ post._id }>
       <p id="message">{ post.message } </p>
       <p id="date">{ formattedDate }</p>
     </article>
