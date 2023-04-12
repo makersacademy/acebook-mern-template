8f36b485d93d277552a3925b5d7712ac80b05493
Dilan Patel <patel_dilan@hotmail.com>
2023-04-12 16:59:07 +0100

    Amended Post component click function to send a patch request to update likes and use the response to update the local state of 'likes'

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -47,20 +47,26 @@ const Post = ({ post }) => {
 
   const handleLikeClick = async () => {
     try {
-      await fetch(`/posts/${post._id}`, {
+      const response = await fetch(`/posts/${post._id}`, {
         method: "PATCH",
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
-        body: JSON.stringify({ likes: likes + 1 }),
       });
-      console.log("Liked post with ID: ", post._id);
-      setLikes(likes + 1);
+  
+      if (response.ok) {
+        const updatedPost = await response.json();
+        setLikes(updatedPost.likes);
+        console.log("Updated likes for post with ID:", post._id);
+      } else {
+        console.error("Error updating likes:", response.statusText);
+      }
     } catch (error) {
       console.error("Error updating likes:", error);
     }
   };
+  
 
   return (
     <article data-cy="post" key={post._id}>
