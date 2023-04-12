8b438e02f29c8024a109520a06284ed1686d260a
Sarah <sarahdavies186@gmail.com>
2023-04-12 13:25:07 +0100

    middleware for express static for image to show on page

--- api/app.js
+++ api/app.js
@@ -17,6 +17,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // middleware function to check for valid tokens
 const tokenChecker = (req, res, next) => {
 
