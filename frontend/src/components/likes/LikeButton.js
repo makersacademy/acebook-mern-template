48c1c91d4dde64d0219df5059d9079afaa54d585
Dilan Patel <patel_dilan@hotmail.com>
2023-04-14 07:35:51 +0100

    Updated LikeButton to switch between empty and fill depending on whether the post has been liked by the user logged in

--- frontend/src/components/likes/LikeButton.js
+++ frontend/src/components/likes/LikeButton.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp } from "@fortawesome/free-solid-svg-icons/faThumbsUp";
+import { faThumbsUp as farThumbsUp } from "@fortawesome/free-regular-svg-icons"; 
 
-const LikeButton = ({ likes, onClick }) => {
+const LikeButton = ({ likes, onClick, isLiked }) => {
   return (
     <button className="likeButton" onClick={onClick}>
-      Like <FontAwesomeIcon icon={faThumbsUp} /> {likes}
+      <FontAwesomeIcon
+        icon={isLiked ? faThumbsUp : farThumbsUp} 
+      />{" "}
+      {likes}
     </button>
   );
 };
