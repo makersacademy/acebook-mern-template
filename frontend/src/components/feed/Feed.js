71c21f2475b7a40572dfe0e1e68328554d8904db
sonia khan <soniamarei.khan@gmail.com>
2023-04-19 15:01:32 +0100

    theoretical-working-frontend

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -36,7 +36,7 @@ const Feed = ({ navigate }) => {
             </button>
           <div id='feed' role="feed">
               {posts.map(
-                (post) => ( <Post post={ post } key={ post._id } /> )
+                (post) => ( <Post post={ post } key={ post._id } token={token} setToken={setToken}/> )
               )}
           </div>
         </>
--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const Post = ({post}) => {
-  return(
-    <article data-cy="post" key={ post._id }>{ post.message }</article>
-  )
-}
+const Post = (props) => {
+  const { post, token, setToken } = props;
+  const [deletedPost, setDeletedPost] = useState(false);
+
+  const Delete = (token, setToken, post) => {
+    if (token) {
+      fetch("/posts", {
+        method: "delete",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: {_id: post._id}
+      })
+        .then((response) => response.json())
+        .then(async (data) => {
+          window.localStorage.setItem("token", data.token);
+          setToken(window.localStorage.getItem("token"));
+          setDeletedPost(true);
+          console.log("message", "Message has been deleted");
+        });
+    }
+  };
+
+  const displayPost = (deletedPost) => {
+    if (deletedPost === false)
+      return (
+        <article data-cy="post" key={post._id}>
+          {post.message}
+          <button onClick={Delete(token, setToken)}>Delete</button>
+        </article>
+      );
+    else
+      return (
+        <div>
+          <p>This message has been deleted.</p>
+        </div>
+      );
+  };
+
+  return (
+    // <article data-cy="post" key={post._id}>
+    //   {post.message}
+    //   <button onClick={Delete(token, setToken)}>Delete</button>
+    // </article>
+
+    displayPost(deletedPost)
+  );
+};
 
 export default Post;
