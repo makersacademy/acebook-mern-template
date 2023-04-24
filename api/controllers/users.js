c2516b251e46ac9ea0416c18b0abb06d50b518bd
GCloughley <georgecloughley.jm@gmail.com>
2023-04-24 14:24:45 +0100

    bug fix

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -11,6 +11,16 @@ const UsersController = {
       }
     });
   },
+  Update: (req, res) => {
+    const user = new User(req.body);
+    user.save((err) => {
+      if (err) {
+        res.status(400).json({message: 'Bad request'})
+      } else {
+        res.status(201).json({ message: 'OK' });
+      }
+    });
+  },
 };
 
 module.exports = UsersController;
