5a3c5a5064e746851e7400bf5427ad50abf4e743
Jack Thacker <jackthacker89@gmail.com>
2023-04-24 20:50:31 +0100

    Fix LandingPage tests

--- frontend/src/components/landing/LandingPage.cy.js
+++ frontend/src/components/landing/LandingPage.cy.js
@@ -3,11 +3,11 @@ import LandingPage from "./LandingPage";
 describe("LandingPage", () => {
   it("renders a LandingPage with HTML", () => {
     cy.mount(<LandingPage />);
-    cy.get(".header").should("contain.text", "LandingPage");
+    cy.get(".header").should("contain.text", "ACEBOOK");
     cy.get(".text")
       .should("be.visible")
       .should("contain.text", "Miaow then turn around");
-    cy.get(".user-btn")
+    cy.get(".button")
       .should("contain.text", "Sign up")
       .should("be.visible")
       .should("be.enabled");
