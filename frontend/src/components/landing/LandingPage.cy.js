f8bbec9c9d9ab74db74f9ec812c31b3b3ca38e40
Jack Thacker <jackthacker89@gmail.com>
2023-04-25 09:22:57 +0100

    Edit LandingPage class names + style guide

--- frontend/src/components/landing/LandingPage.cy.js
+++ frontend/src/components/landing/LandingPage.cy.js
@@ -3,8 +3,8 @@ import LandingPage from "./LandingPage";
 describe("LandingPage", () => {
   it("renders a LandingPage with HTML", () => {
     cy.mount(<LandingPage />);
-    cy.get(".header").should("contain.text", "ACEBOOK");
-    cy.get(".text")
+    cy.get(".landing-header").should("contain.text", "ACEBOOK");
+    cy.get(".landing-text")
       .should("be.visible")
       .should("contain.text", "Miaow then turn around");
     cy.get(".button")
--- frontend/src/components/landing/LandingPage.jsx
+++ frontend/src/components/landing/LandingPage.jsx
@@ -5,8 +5,8 @@ import styles from "./LandingPage.css";
 const LandingPage = ({ navigate }) => {
   return (
     <section className="landing">
-      <div className="header">ACEBOOK</div>
-      <p className="text">
+      <div className="landing-header">ACEBOOK</div>
+      <p className="landing-text">
         Miaow then turn around and show you my bum have a lot of grump in
         yourself because you can't forget to be grumpy and not be like king
         grumpy cat, take a deep sniff of sock then walk around with mouth half
