c64a10a8f6f130ea0b04230f9d701f93dc5de968
Jack Thacker <jackthacker89@gmail.com>
2023-04-25 11:15:51 +0100

    Write post a post button test for Feed
    Co-authored-by: Matt Parkinson <MGP1992@users.noreply.github.com>

--- frontend/src/components/feed/Feed.cy.js
+++ frontend/src/components/feed/Feed.cy.js
@@ -1,27 +1,37 @@
-import Feed from './Feed'
-const navigate = () => {}
+import Feed from "./Feed";
+const navigate = () => {};
 
 describe("Feed", () => {
-  it("Calls the /posts endpoint and lists all the posts", () => {
-    window.localStorage.setItem("token", "fakeToken")
-    
-    cy.intercept('GET', '/api/posts', (req) => {
-        req.reply({
-          statusCode: 200,
-          body: { posts: [
-            {_id: 1, message: "Hello, world"},
-            {_id: 2, message: "Hello again, world"}
-          ] }
-        })
-      }
-    ).as("getPosts")
+  beforeEach(() => {
+    window.localStorage.setItem("token", "fakeToken");
+  });
 
-    cy.mount(<Feed navigate={navigate}/>)
-    
-    cy.wait("@getPosts").then(() =>{
+  it("Calls the /posts endpoint and lists all the posts", () => {
+    cy.intercept("GET", "/api/posts", (req) => {
+      req.reply({
+        statusCode: 200,
+        body: {
+          posts: [
+            { _id: 1, message: "Hello, world" },
+            { _id: 2, message: "Hello again, world" },
+          ],
+        },
+      });
+    }).as("getPosts");
+
+    cy.mount(<Feed navigate={navigate} />);
+
+    cy.wait("@getPosts").then(() => {
       cy.get('[data-cy="post"]')
-      .should('contain.text', "Hello, world")
-      .and('contain.text', "Hello again, world")
-    })
+        .should("contain.text", "Hello, world")
+        .and("contain.text", "Hello again, world");
+    });
+  });
+  it('should have a clickable \'create post\' button', () => {
+    cy.get(".button")
+      .should("contain.text", "Create Post")
+      .should("be.visible")
+      .should("be.enabled");
+
   })
-})
+});
