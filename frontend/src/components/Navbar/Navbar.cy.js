1f15478b007a2c693db0689541f02e9b6b7f32ec
sonia khan <soniamarei.khan@gmail.com>
2023-04-26 14:58:24 +0100

    tests all working and navbar refactored

--- frontend/src/components/Navbar/Navbar.cy.js
+++ frontend/src/components/Navbar/Navbar.cy.js
@@ -2,62 +2,117 @@ import Navbar from './Navbar';
 import { BrowserRouter as Router} from "react-router-dom";
 
 
-
-
-
-// TEST WORKS (1)
 const location = () => {}
 describe("Navbar", () => {
-  it("renders the Acebook title and a link to the login page", () => {
+  it("renders the Acebook title and a link to the login page on the landing page", () => {
     cy.mount(
                 <Router>
-                    <Navbar/>
+                    <Navbar location="/"/>
                 </Router>
             );
     cy.clearLocalStorage("token")
     cy.get('[data-cy="logo"]')
-    .should('contain', "Acebook")
-    cy.get('[data-cy="signup"]')
-    .should('contain', "Sign Up")
+    cy.get('[data-cy="login"]')
+    .should('contain', "Log In")
+    cy.screenshot()
     })
 
-
-it("changes from signup to login when the user is on the sign in page", () => {
+    it("renders the Acebook title and a link to the signup page on the login page when the user is not logged in", () => {
         cy.mount(
                     <Router>
-                        <Navbar location={location}/>
+                        <Navbar location="/login"/>
                     </Router>
                 );
         cy.clearLocalStorage("token")
         cy.get('[data-cy="logo"]')
-        .should('contain', "Acebook")
-        cy.get('[data-cy="login"]')
-        .should('contain', "Log In")
-    })
+        cy.get('[data-cy="signup"]')
+        .should('contain', "Sign Up")
+        cy.screenshot()
+        })
 
-
-    it("allows the user to access the feed page when the user is logged in and clicks the Acebook logo", () => {
+    it("renders the Acebook logo and the profile and signout links when the user is logged in and on feed", () => {
         cy.mount(
                     <Router>
-                        <Navbar/>
+                        <Navbar location="/posts"/>
+                    </Router>
+                );
+        window.localStorage.setItem("token", "fakeToken")
+        cy.get('[data-cy="logo"]')
+        cy.get('[data-cy="search-friend"]')
+        .should('contain', "Search Friend")
+        cy.get('[data-cy="profile"]')
+        .should('contain', "Profile")
+        cy.get('[data-cy="logout"]')
+        .should('contain', "Sign Out")
+    })
+
+    it("renders the Acebook logo and main nav bar when the user is logged in and on a page that is not feed", () => {
+        cy.mount(
+                    <Router>
+                        <Navbar location="/profile"/>
+                    </Router>
+                );
+        window.localStorage.setItem("token", "fakeToken")
+        cy.get('[data-cy="logo"]')
+        cy.get('[data-cy="search-friend"]')
+        .should('contain', "Search Friend")
+        cy.get('[data-cy="feed"]')
+        .should('contain', "Feed")
+        cy.get('[data-cy="profile"]')
+        .should('contain', "Profile")
+        cy.get('[data-cy="logout"]')
+        .should('contain', "Sign Out")
+    })
+
+    it("renders the Acebook logo and the feed, profile and signout links when the user is logged in and on the search friends page", () => {
+        cy.mount(
+                    <Router>
+                        <Navbar location="/search-friend"/>
+                    </Router>
+                );
+        window.localStorage.setItem("token", "fakeToken")
+        cy.get('[data-cy="logo"]')
+        cy.get('[data-cy="feed"]')
+        .should('contain', "Feed")
+        cy.get('[data-cy="profile"]')
+        .should('contain', "Profile")
+        cy.get('[data-cy="logout"]')
+        .should('contain', "Sign Out")
+    })
+
+    it("navigates the user to the feed page when they are logged in and click the Acebook logo", () => {
+        cy.mount(
+                    <Router>
+                        <Navbar location="/search-friend"/>
                     </Router>
                 );
         window.localStorage.setItem("token", "fakeToken")
         cy.get('[data-cy="logo"]').click()
-        cy.url().should('include', '/feed') 
-     })
+        cy.url().should('contain', '/posts')  
+    })
 
-     it("renders the Acebook title and the profile and signout links when the user is logged in and on a feed or posts page", () => {
+    it("navigates the user to the landing page when they are not logged in and click the Acebook logo", () => {
         cy.mount(
                     <Router>
-                        <Navbar location={location}/>
+                        <Navbar location="/login"/>
+                    </Router>
+                );
+        cy.clearLocalStorage("token")
+        cy.get('[data-cy="logo"]').click()
+        cy.url().should('contain', '/')  
+    })
+
+    it("removes the token when the user clicks the signout button", () => {
+        cy.mount(
+                    <Router>
+                        <Navbar location="/posts"/>
                     </Router>
                 );
         window.localStorage.setItem("token", "fakeToken")
-        
-        cy.get('[data-cy="logo"]')
-        cy.get('[data-cy="user"]')
-        cy.get('[data-cy="signout"]')
-        .should('contain', "Signout")        
-     })
+        cy.get('[data-cy="logout"]').click()
+        cy.wait(10000).should(() => {
+            expect(localStorage.getItem("token", "fakeToken")).to.be.null;
+        })
+    
+    })
 })
