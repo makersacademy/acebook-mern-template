ea81f206ff6386f3c34e09c469cc46b81766aca9
Jack Thacker <jackthacker89@gmail.com>
2023-04-27 10:00:41 +0100

    Updates nav folders

--- frontend/src/components/app/App.jsx
+++ frontend/src/components/app/App.jsx
@@ -2,16 +2,15 @@ import "./App.css";
 import LandingPage from "../landing/LandingPage";
 import LoginForm from "../auth/LoginForm";
 import SignUpForm from "../user/SignUpForm";
-import UserProfile from "../UserProfile/UserProfile";
-import FriendProfile from "../FriendProfile/FriendProfile";
-import FriendList from "./FriendList/FriendList";
-import SearchFriend from "./SearchFriend/SearchFriend";
-import Signout from "./Signout/Signout";
+import UserProfile from "../userProfile/UserProfile";
+import FriendProfile from "../friendProfile/FriendProfile";
+import FriendList from "../friendList/FriendList";
+import Signout from "../signout/Signout";
 import React, { useState } from "react";
 import Feed from "../feed/Feed";
 import NewPost from "../newPost/NewPost";
 import { useNavigate, Routes, Route, useLocation } from "react-router-dom";
-import Navbar from "../Navbar/Navbar";
+import Navbar from "../navbar/Navbar";
 
 const App = () => {
   const location = useLocation().pathname;
