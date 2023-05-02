89244cf849d6a444a0e50cb29e0d8bef68efdbc9
Jack Thacker <jackthacker89@gmail.com>
2023-05-02 16:59:32 +0100

    Delete images in upload folder after added to S3

--- api/controllers/images.js
+++ api/controllers/images.js
@@ -1,10 +1,14 @@
 const { uploadFile } = require("../s3");
+const fs = require("fs");
+const util = require("util");
+const unlinkFile = util.promisify(fs.unlink);
 
 const ImagesController = {
   Create: async (req, res) => {
     const file = req.file;
     console.log(req.file);
     const result = await uploadFile(file);
+    await unlinkFile(file.path);
     console.log(result.Location);
     res.status(200).json({ url: result.Location, message: "OK" });
   },
