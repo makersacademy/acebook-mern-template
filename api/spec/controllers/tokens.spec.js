58737623ff7ea7b1dbab85608d5dd6fd002dad39
Madelane <madelanedaz@gmail.com>
2023-05-10 16:27:05 +0100

    displays posts from newest and oldest

--- api/spec/controllers/tokens.spec.js
+++ api/spec/controllers/tokens.spec.js
@@ -1,34 +1,33 @@
 const app = require("../../app");
 const request = require("supertest");
 require("../mongodb_helper");
-const User = require('../../models/user');
+const User = require("../../models/user");
 
 describe("/tokens", () => {
-  beforeAll( () => {
-    const user = new User({ email: "test@test.com", password: "12345678" })
-    user.save()
+  beforeAll(async () => {
+    const user = new User({ email: "test@test.com", password: "12345678" });
+    await user.save();
   });
 
-  afterAll( async () => {
-    await User.deleteMany({})
-  })
+  afterAll(async () => {
+    await User.deleteMany({});
+  });
 
   test("a token is returned when creds are valid", async () => {
     let response = await request(app)
       .post("/tokens")
-      .send({email: "test@test.com", password: "12345678"})
-    expect(response.status).toEqual(201)
-    expect(response.body.token).not.toEqual(undefined)
-    expect(response.body.message).toEqual("OK")
-  })
-
+      .send({ email: "test@test.com", password: "12345678" });
+    expect(response.status).toEqual(201);
+    expect(response.body.token).not.toEqual(undefined);
+    expect(response.body.message).toEqual("OK");
+  });
 
   test("a token is not returned when creds are invalid", async () => {
     let response = await request(app)
       .post("/tokens")
-      .send({email: "test@test.com", password: "1234"})
-    expect(response.status).toEqual(401)
-    expect(response.body.token).toEqual(undefined)
-    expect(response.body.message).toEqual("auth error")
-  })
-})
+      .send({ email: "test@test.com", password: "1234" });
+    expect(response.status).toEqual(401);
+    expect(response.body.token).toEqual(undefined);
+    expect(response.body.message).toEqual("auth error");
+  });
+});
