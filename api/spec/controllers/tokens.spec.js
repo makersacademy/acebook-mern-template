2c6f268e8a69f2d1d8420ba5b301f47565ae7e88
toppy007 <33899845+toppy007@users.noreply.github.com>
2023-05-10 16:18:58 +0100

    update

--- api/spec/controllers/tokens.spec.js
+++ api/spec/controllers/tokens.spec.js
@@ -17,7 +17,7 @@ describe("/tokens", () => {
   test("a token is returned when creds are valid", async () => {
     let response = await request(app)
       .post("/tokens")
-      .send({email: "test@test.com", password: "12345678" })
+      .send({email: "test@test.com", password: "12345678", username: "username" })
     expect(response.status).toEqual(201)
     expect(response.body.token).not.toEqual(undefined)
     expect(response.body.message).toEqual("OK")
