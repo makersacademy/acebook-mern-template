77739aa6231a5f64962b4da5a2fae869df1c358f
Sam White <sam.white745@gmail.com>
2023-05-10 11:24:12 +0100

    added createPostForm component with testing

---
+++ frontend/src/components/createPost/CreatePostForm.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const CreatePostForm = ({ navigate }) => {
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    let response = await fetch( '/posts', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message: message })
+    })
+
+    if(response.status !== 201) {
+      //bad reponse
+      console.log("bad response - != 201")
+      //refresh posts
+      navigate('/posts')
+    } else {
+      //good response
+      console.log("Good response - 201")
+      //refresh posts
+      navigate('/posts');
+    }
+  }
+
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value)
+  }
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <input placeholder='Write your post here' id="message" type='text' value={ message } onChange={handleMessageChange} />
+        <input role='submit-button' id='submit' type="submit" value="Submit" />
+      </form>
+    );
+}
+
+export default CreatePostForm;
