d3845baea5c682fa5cbb3dcf6454256b50db8c6b
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2023-05-10 19:22:22 +0100

    Homepage with baby css and passing tests

---
+++ frontend/src/components/home/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Acebook</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Home;
