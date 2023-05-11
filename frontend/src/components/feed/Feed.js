2014af5739319ecf849441cdf34a711ad2701399
natashabuckham <natashaisobel@gmail.com>
2023-05-11 17:17:03 +0100

    implemented add post on frontend - not tested

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -14,7 +14,7 @@ const Feed = ({ navigate }) => {
         }
       })
         .then(response => response.json())
-        .then(async data => {
+        .then(async data => { // try to remove async and see if it works
           window.localStorage.setItem("token", data.token)
           setToken(window.localStorage.getItem("token"))
           setPosts(data.posts);
@@ -29,10 +29,26 @@ const Feed = ({ navigate }) => {
   }
 
   const submitPost = (event) => {
-    event.preventDefault() // don't do the default functionality of the form - which would submit and reload the page
-    console.log(newPost) // connect to database, update data
-    setNewPost(""); // clear the input field - set as empty string so type doesn't change
+  if(token) {
+    fetch("/posts", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        "Authorization": `Bearer ${token}`
+      },
+      body: JSON.stringify({"message": newPost})
+    })
+    .then(response => response.json())
+    .then(async data => {
+      window.localStorage.setItem("token", data.token)
+      setToken(window.localStorage.getItem("token"))
+      console.log(data.message)
+      event.preventDefault() // don't do the default functionality of the form - which would submit and reload the page
+      console.log(newPost) // connect to database, update data
+      setNewPost(""); // clear the input field - set as empty string so type doesn't change
+    })
   }
+}
 
   const onChangePostInput = (event) => {
     setNewPost(event.target.value)
@@ -45,13 +61,15 @@ const Feed = ({ navigate }) => {
           <button onClick={logout}>
             Logout
           </button>
+        {/* can extract into its own component - separation of concerns */}
         <form onSubmit={submitPost}>
           <input onChange={onChangePostInput} type="text" value={newPost} />
           <button type="submit">Submit</button>
         </form>
+
         <div id='feed' role="feed">
             {posts.map(
-              (post) => ( <Post post={ post } key={ post._id } /> )
+              (post) => ( <Post post={ post } key={ post._id }/> )
             )}
         </div>
       </>
