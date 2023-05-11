ddc6a662dcf60b5510a094cfd6919c36573ea3c4
Pablo <pablito@me.com>
2023-05-11 18:21:25 +0100

    post being created but posts not refreshing

--- frontend/src/components/AddPost/AddPost.js
+++ frontend/src/components/AddPost/AddPost.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 
-const AddPost = ({ navigate }) => {
+const AddPost = () => { // was passinf in navigate but that is not passed into this component in Feed
   const [message, setMessage] = useState('');
   const [token, setToken] = useState(window.localStorage.getItem("token"));
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
       if (token) {
 
-        console.log(`{message}`) // VISIBILITY
-        fetch('/posts', {
+        console.log(`${message}`) // VISIBILITY
+        const response = await fetch('/posts', {
           method: 'POST',
           headers: {
             'Authorization': `Bearer ${token}`,
@@ -18,25 +18,36 @@ const AddPost = ({ navigate }) => {
           },
           body: JSON.stringify({ message: message })
         })
-          .then(response => response.json())
-          .then(async data => {
-            window.localStorage.setItem("token", data.token);
-            setToken(window.localStorage.getItem("token"));
-            console.log(data) // VISIBILITY
+        if (response.status !== 201) {
+          console.log("oops")
+          // navigate('/login')
+        } else {
+          console.log("yay!")
+          let data = await response.json()
+          console.log(data) // VISIBILITY
+          window.localStorage.setItem("token", data.token)
+          setToken(window.localStorage.getItem("token"));
+          // navigate('/posts'); // navigate is not present in this component
+        }
+          
+          
+          
+          
+          
+          
+          // .then(response => response.json())
+          // .then(async data => {
+          //   window.localStorage.setItem("token", data.token);
+          //   setToken(window.localStorage.getItem("token"));
+            // console.log(data) // VISIBILITY
             // navigate('./login')
             // Should trigger a reload of all posts
-          })
+          // })
           
       }
       
   }
-  // let response = await fetch('/tokens', {
-  //   method: 'post',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify({ email: email, password: password })
-  // })
+  
 
 
 
