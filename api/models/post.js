9f7ad1a50a43195facab6ba1683d4032442220bb
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2023-05-11 14:58:46 +0100

    Updated post

--- api/models/post.js
+++ api/models/post.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema({
   message: { type: String },
   dateCreated: { type: Date, default: Date.now },
+  likeCount: {type: Number, default: 0 }
 });
 
 const Post = mongoose.model("Post", PostSchema);
--- frontend/src/components/like/Like.js
+++ frontend/src/components/like/Like.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState} from "react";
+import { useState } from "react";
 
 const Like = ( { postId } ) => {
   let like = 0;
