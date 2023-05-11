2be9bbdf4f7bf6089ca7f556c03c8c923a88ddd4
Pablo <pablito@me.com>
2023-05-11 17:29:48 +0100

    new posts creating and all tests passing

--- frontend/src/components/AddPost/AddPost.js
+++ frontend/src/components/AddPost/AddPost.js
@@ -1,29 +1,97 @@
 import React, { useState } from 'react';
 
-const AddPost = ({ newPost, navigate }) => {
+const AddPost = ({ navigate }) => {
   const [message, setMessage] = useState('');
+  const [token, setToken] = useState(window.localStorage.getItem("token"));
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
-    let response = await fetch('/posts', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ message: message })
-    })
+    // useEffect(() => {
+      if (token) {
+        
+        // fetch("/posts", {
+        //   headers: {
+        //     'Authorization': `Bearer ${token}`
+        //   }
+        // })
+        //   .then(response => response.json())
+        //   .then(async data => {
+        //     window.localStorage.setItem("token", data.token)
+        //     setToken(window.localStorage.getItem("token"))
+        //     setPosts(data.posts);
+        //   })
 
-    if (response.status !== 201) {
-      console.log("yay")
-      navigate('/posts')
-    } else {
-      console.log("oops")
-      // let data = await response.json()
-      // window.localStorage.setItem("token", data.token)
-      navigate('/posts');
-    }
+        fetch('/posts', {
+          method: 'POST',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ message: message })
+        })
+          .then(response => response.json())
+          .then(async data => {
+            window.localStorage.setItem("token", data.token);
+            setToken(window.localStorage.getItem("token"));
+            console.log(data)
+            // setPosts(data.posts);
+          })
+    
+        // if (response.status !== 201) {
+        //   console.log(response)
+        //   navigate('/posts')
+        // } else {
+        //   console.log("Yay!")
+        //   let data = response.json()
+        //   window.localStorage.setItem("token", data.token)
+        //   navigate('/posts');
+        // }
+
+      }
+    // }, [])
   }
+
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+
+  //   useEffect(() => {
+  //     if (token) {
+        
+  //       // fetch("/posts", {
+  //       //   headers: {
+  //       //     'Authorization': `Bearer ${token}`
+  //       //   }
+  //       // })
+  //       //   .then(response => response.json())
+  //       //   .then(async data => {
+  //       //     window.localStorage.setItem("token", data.token)
+  //       //     setToken(window.localStorage.getItem("token"))
+  //       //     setPosts(data.posts);
+  //       //   })
+
+  //       let response = await fetch('/posts', {
+  //         method: 'POST',
+  //         headers: {
+  //           'Authorization': `Bearer ${token}`,
+  //           'Content-Type': 'application/json',
+  //         },
+  //         body: JSON.stringify({ message: message })
+  //       })
+    
+  //       if (response.status !== 201) {
+  //         console.log(response)
+  //         navigate('/posts')
+  //       } else {
+  //         console.log("Yay!")
+  //         let data = await response.json()
+  //         window.localStorage.setItem("token", data.token)
+  //         navigate('/posts');
+  //       }
+
+  //     }
+  //   }, [])
+  // }
 
     const handleMessageChange = (event) => {
       setMessage(event.target.value)
--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import Post from '../post/Post'
+import Post from '../post/Post';
+import AddPost from '../AddPost/AddPost';
 
 const Feed = ({ navigate }) => {
   const [posts, setPosts] = useState([]);
@@ -33,7 +34,8 @@ const Feed = ({ navigate }) => {
           <h2>Posts</h2>
             <button onClick={logout}>
               Logout
-            </button>
+          </button>
+          <AddPost />
           <div id='feed' role="feed">
               {posts.map(
                 (post) => ( <Post post={ post } key={ post._id } /> )
