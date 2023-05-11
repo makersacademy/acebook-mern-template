709fdf5e88a407fa103957c444a7d575baf2f47a
Sam White <sam.white745@gmail.com>
2023-05-11 12:52:26 +0100

    Changed the input text field to a textarea

--- frontend/src/components/createPost/CreatePostForm.js
+++ frontend/src/components/createPost/CreatePostForm.js
@@ -24,10 +24,13 @@ const CreatePostForm = ({ onCreated }) => {
   }
 
     return (
-      <form onSubmit={handleSubmit}>
-        <input placeholder='Write your post here' id="message" type='text' value={ message } onChange={handleMessageChange} />
+      <>
+      <form onSubmit={handleSubmit} id="postForm">
+        <h1>Make a post</h1>
+        <textarea rows="4" cols="50" placeholder='Write your post here...' id="message" value={ message } onChange={handleMessageChange} form="postForm"/>
         <input role='submit-button' id='submit' type="submit" value="Submit" />
       </form>
+      </>
     );
 }
 
