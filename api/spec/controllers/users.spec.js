a5ba8f858d7217ad497cd2f4ea02c66f9485147c
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-05-11 15:52:29 +0100

    signup page first and last name code,tests,frontend complete

--- api/spec/controllers/users.spec.js
+++ api/spec/controllers/users.spec.js
@@ -8,18 +8,19 @@ describe("/users", () => {
     await User.deleteMany({});
   });
 
-  describe("POST, when email and password are provided", () => {
+  describe("POST, when email, password, first name and last name are provided", () => {
     test("the response code is 201", async () => {
       let response = await request(app)
         .post("/users")
-        .send({email: "poppy@email.com", password: "1234"})
+        .send({email: "poppy@email.com", password: "1234",  firstName: "Betty",
+        lastName: "Rubble"})
       expect(response.statusCode).toBe(201)
     })
 
     test("a user is created", async () => {
       await request(app)
         .post("/users")
-        .send({email: "scarlett@email.com", password: "1234"})
+        .send({email: "scarlett@email.com", password: "1234", firstName: "Scarlett", lastName: "Ohara"})
       let users = await User.find()
       let newUser = users[users.length - 1]
       expect(newUser.email).toEqual("scarlett@email.com")
@@ -30,7 +31,7 @@ describe("/users", () => {
     test("response code is 400", async () => {
       let response = await request(app)
         .post("/users")
-        .send({email: "skye@email.com"})
+        .send({email: "skye@email.com", firstName: "Scarlett", lastName: "Ohara"})
       expect(response.statusCode).toBe(400)
     });
 
@@ -47,7 +48,7 @@ describe("/users", () => {
     test("response code is 400", async () => {
       let response = await request(app)
         .post("/users")
-        .send({password: "1234"})
+        .send({password: "1234", firstName: "Scarlett", lastName: "Ohara"})
       expect(response.statusCode).toBe(400)
     });
 
