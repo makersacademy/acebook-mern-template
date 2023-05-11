6976e3d66a28b0e3f1438592ec83c0317b2e9de4
natashabuckham <natashaisobel@gmail.com>
2023-05-11 14:07:21 +0100

    initial exploration

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -4,6 +4,7 @@ import Post from '../post/Post'
 const Feed = ({ navigate }) => {
   const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [newPost, setNewPost] = useState("");
 
   useEffect(() => {
     if(token) {
@@ -20,30 +21,44 @@ const Feed = ({ navigate }) => {
         })
     }
   }, [])
-    
+
 
   const logout = () => {
     window.localStorage.removeItem("token")
     navigate('/login')
   }
-  
-    if(token) {
-      return(
-        <>
-          <h2>Posts</h2>
-            <button onClick={logout}>
-              Logout
-            </button>
-          <div id='feed' role="feed">
-              {posts.map(
-                (post) => ( <Post post={ post } key={ post._id } /> )
-              )}
-          </div>
-        </>
-      )
-    } else {
-      navigate('/signin')
-    }
+
+  const submitPost = (event) => {
+    event.preventDefault() // don't do the default functionality of the form - which would submit and reload the page
+    console.log(newPost) // connect to database, update data
+    setNewPost(""); // clear the input field - set as empty string so type doesn't change
+  }
+
+  const onChangePostInput = (event) => {
+    setNewPost(event.target.value)
+  }
+
+  if(token) {
+    return(
+      <>
+        <h2>Posts</h2>
+          <button onClick={logout}>
+            Logout
+          </button>
+        <form onSubmit={submitPost}>
+          <input onChange={onChangePostInput} type="text" value={newPost} />
+          <button type="submit">Submit</button>
+        </form>
+        <div id='feed' role="feed">
+            {posts.map(
+              (post) => ( <Post post={ post } key={ post._id } /> )
+            )}
+        </div>
+      </>
+    )
+  } else {
+    navigate('/signin')
+  }
 }
 
 export default Feed;
