7980a2c3162add8123b9808ec2f671652e18da73
Quddus Rahman <qrahman115@gmail.com>
2023-05-12 17:02:12 +0100

    cleanup commit

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,9 +1,39 @@
 import React from 'react';
+// import useState from 'react';
 
 const Post = ({post}) => {
+  // const [token, setToken] = useState(window.localStorage.getItem("token"));
+  // const handleLike = async () => {
+  //   try {
+  //     await fetch(`/posts/${ post._id }/likes` , {
+  //       method: 'POST',
+  //       headers: {
+  //         // 'Content-Type': 'application/json',
+  //         'Authorization': `Bearer ${token}`,
+  //       },
+        // body: JSON.stringify({})
+      // })
+      // .then(response => response.json())
+      //   .then(async data => {
+      //     window.localStorage.setItem("token", data.token)
+      //     setToken(window.localStorage.getItem("token"))
+      //   })
+      // ;
+
+      // console.log(token)
+      
+
+    //   const data = await response.json();
+    //   console.log(data);
+      
+    // } catch (error) {
+    //   console.error(error);
+    // }
+  // }
+
   return(
-    <article data-cy="post" key={ post._id }>{ post.message }</article>
+    <article data-cy="post" key={ post._id }>{ post.message }  <button>👍 | { post.like }</button></article>
   )
 }
-
 export default Post;
+
