c944222ace579fb23c1a564bcedb0db8ff06c27b
umut <umut_baykan@hotmail.com>
2023-05-12 18:08:39 +0100

    unlike functionality added

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -8,6 +8,9 @@ const PostsController = {
         throw err;
       }
       const token = await TokenGenerator.jsonwebtoken(req.user_id);
+      // This is mapping an additional field called didUserLikeThis inside posts
+      // which returns true if the user's id is already in the likedBy array.
+      // likedBy array is the list of users who already liked the post
       posts.forEach(post => {
         if (post.likedBy.includes(req.user_id)) {post._doc.didUserLikeThis = true;}})
       res.status(200).json({ posts: posts, token: token });
@@ -37,7 +40,18 @@ const PostsController = {
     })
   },
   Unlike: (req, res) => {
-
+    Post.findById(req.body.post_id, async (err, post) => {
+      if (err) {
+        throw err;
+      } else {
+        post.likes -= 1
+        const index = post.likedBy.indexOf(req.user_id);
+          if (index > -1) {post.likedBy.splice(index, 1);}
+        post.save()
+        const token = await TokenGenerator.jsonwebtoken(req.user_id)
+        res.status(200).json({ likes: (post.likes), token: token });
+      }
+    })
   },
 };
 
