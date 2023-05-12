11a26e2a57389380d7201f1da7544c9f772671dd
Jack <jwleggett@outlook.com>
2023-05-12 13:46:19 +0100

    working nav bar

--- frontend/src/components/nav_bar/nav_bar.js
+++ frontend/src/components/nav_bar/nav_bar.js
@@ -1,6 +1,7 @@
- import './nav_bar.css'
- import React, { useState } from 'react';
+import './nav_bar.css'
+import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import logo from './logo.png'
 
 
 
@@ -21,43 +22,55 @@ const Navigation = () => {
    navigate('/signup')
   }
 
+  const handleLogoClick = () => {
+    if (token) {
+      navigate('/posts')
+    }
+  }
+
   const [isOpen, setIsOpen] = useState(false);
   const handleDropdownClick = () => setIsOpen(!isOpen);
   const token = window.localStorage.getItem('token');
   const isLoginPage = location.pathname === '/login';
   const isSignupPage = location.pathname === '/signup';
 
-
   return ( 
-   <>
-   { token ? (
-   <div className="dropdown">
+  <>
+  <div className='navbar-container'>
+    <div className='logo-container'>
+      <div className="logo" onClick={handleLogoClick}>
+        <img src={logo} alt="logo"></img>
+      </div>
+    </div>
+  { token ? (
+    <div className="dropdown">
       <div className='dropdown-container'>
-     <button className="dropdown-button" onClick={handleDropdownClick}>
-       Dropdown
-     </button>
-     </div>
-     {isOpen && (
-       <div className="dropdown-content">
-         <button className="dropdown-item">Profile Settings</button>
-         <div>
-           <button onClick={logout}>Logout</button>
-         </div>
-       </div>
-     )}
-   </div>
- ) : (
-   <>
-   <div className='login-signup'>
-   {isLoginPage && <button className='signUpNav' onClick={handleSignupClick}>Sign up page</button>}
-   {isSignupPage && <button className='loginNav' onClick={handleLoginClick}>Login page</button>}
-   </div>
-   </>
- )
-   }
-   </>
+        <button className="dropdown-button" onClick={handleDropdownClick}>
+        Dropdown
+        </button>
+      </div>
+    {isOpen && (
+      <div className="dropdown-content">
+        <button className="dropdown-item">Profile Settings</button>
+      <div>
+        <button className='dropdown-logout' onClick={logout}>Logout</button>
+      </div>
+    </div>
+    )}
+  </div>
+  ) : (
+  <>
+    <div className='login-signup'>
+      {isLoginPage && <button className='signUpNav' onClick={handleSignupClick}>Sign up page</button>}
+      {isSignupPage && <button className='loginNav' onClick={handleLoginClick}>Login page</button>}
+    </div>
+  </>
   )
-};
+   }
+  </div>
+  </>
+  )
+  };
   export default Navigation;
 
  
