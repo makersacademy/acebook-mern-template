e4c1fbc68e9ef128b07a5eddf481632fdab09ca6
umut <umut_baykan@hotmail.com>
2023-05-12 15:19:36 +0100

    back end to handle likes in server

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -22,6 +22,19 @@ const PostsController = {
       res.status(201).json({ message: 'OK', token: token });
     });
   },
+  AddLike: (req, res) => {
+    Post.findById(req.body.post_id, async (err, post) => {
+      if (err) {
+        throw err;
+      } else {
+        post.likes += 1
+        post.likedBy.push(req.user_id)
+        post.save()
+        const token = await TokenGenerator.jsonwebtoken(req.user_id)
+        res.status(200).json({ likes: (post.likes), token: token });
+      }
+    })
+  }
 };
 
 module.exports = PostsController;
