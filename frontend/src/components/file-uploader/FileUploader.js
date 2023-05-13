9413ecdcdfc172c9d5651607e751f77e69d0e036
natashabuckham <natashaisobel@gmail.com>
2023-05-13 15:48:58 +0100

    added FileUploader component

---
+++ frontend/src/components/file-uploader/FileUploader.js
@@ -0,0 +1,21 @@
+import React, { useRef } from 'react'
+
+const FileUploader = ({onFileSelect}) => {
+  const fileInput = useRef(null)
+
+  const handleFileInput = (event) => {
+    const file = event.target.files[0]
+    if (file.size > 30720) // size of 30MB in binary KB
+      onFileSelectError({ error: "File size cannot exceed 30MB" });
+    else onFileSelectSuccess(file);
+  }
+
+  return (
+    <div className="file-uploader">
+      <input type="file" onChange={handleFileInput} />
+      <button onClick={event => fileInput.current && fileInput.current.click()} className="btn btn-primary" />
+    </div>
+  )
+}
+
+export default FileUploader;
