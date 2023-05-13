332981c82743c51dd37ad62b33687d68d0d76a50
Robin Vuyyuru <robinvuy@MASTUDENT-Robin-Vuyyuru-C17GMB1VQ6L4.local>
2023-05-13 19:21:44 +0100

    home page

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -5,13 +5,13 @@ import "./Home.css"
 
 const Home = () => {
   return (
-    <div>
+    <div className='container'>
       <h1>Acebook</h1>
       <br/>
       
         <li><Link to="/signup">Sign Up</Link></li>
         <li><Link to="/login">Log In</Link></li>
-      
+    
     </div>
   
   );
