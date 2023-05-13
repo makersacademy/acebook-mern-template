716b0822e1d462e9033f289990946a6290ce2a14
natashabuckham <natashaisobel@gmail.com>
2023-05-13 12:56:28 +0100

    component test passing, added login command to commands.js

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 's1s33c',
   component: {
     devServer: {
       framework: "create-react-app",
--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Post from '../post/Post'
+import NewPostForm from '../new-post/NewPostForm'
 
 const Feed = ({ navigate }) => {
   const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token"));
-  const [newPost, setNewPost] = useState("");
+
 
   useEffect(() => {
     if(token) {
@@ -20,38 +21,12 @@ const Feed = ({ navigate }) => {
           setPosts(data.posts);
         })
     }
-  }, [])
+  }, []) // don't retrigger on every re-render - just when submit button is clicked?
 
 
   const logout = () => {
     window.localStorage.removeItem("token")
     navigate('/login')
-  }
-
-  const submitPost = (event) => {
-  if(token) {
-    fetch("/posts", {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-        "Authorization": `Bearer ${token}`
-      },
-      body: JSON.stringify({"message": newPost})
-    })
-    .then(response => response.json())
-    .then(async data => {
-      window.localStorage.setItem("token", data.token)
-      setToken(window.localStorage.getItem("token"))
-      console.log(data.message)
-      event.preventDefault() // don't do the default functionality of the form - which would submit and reload the page
-      console.log(newPost) // connect to database, update data
-      setNewPost(""); // clear the input field - set as empty string so type doesn't change
-    })
-  }
-}
-
-  const onChangePostInput = (event) => {
-    setNewPost(event.target.value)
   }
 
   if(token) {
@@ -61,11 +36,11 @@ const Feed = ({ navigate }) => {
           <button onClick={logout}>
             Logout
           </button>
-        {/* can extract into its own component - separation of concerns */}
-        <form onSubmit={submitPost}>
-          <input onChange={onChangePostInput} type="text" value={newPost} />
-          <button type="submit">Submit</button>
-        </form>
+
+        <div className="new-post-form">
+          < NewPostForm/>
+        </div>
+
 
         <div id='feed' role="feed">
             {posts.map(
