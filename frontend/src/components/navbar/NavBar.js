0d254bf1f601c2f9389b95c854dbb724e83e4fa1
Manuela Iacobovici <miacobovici@yahoo.co.uk>
2023-05-15 16:15:20 +0100

    nav logout button to work, deleted feed logout button

--- frontend/src/components/navbar/NavBar.js
+++ frontend/src/components/navbar/NavBar.js
@@ -1,17 +1,59 @@
-    import React from 'react'
+    import React, { useEffect, useState } from 'react';
     // import { PrimaryNav, MenuLink, Menu, style} from ‘./NavbarElements’
     import './NavBar.css';
+    import Feed from '../feed/Feed';
+    import LogInForm from '../auth/LoginForm';
 
-    const Navbar = () => {
+    const Navbar = ({ navigate }) => {
 
-      return (
+      const [token, setToken] = useState(window.localStorage.getItem("token"));
+
+      useEffect(() => {
+        if(token !== window.localStorage.getItem("token")) {
+          setToken(window.localStorage.getItem("token"))
+        }
+      }, [window.localStorage.getItem("token")])
+
+      const logout = () => {
+        window.localStorage.removeItem("token")
+        // navigate('/login')
+      }
+
+    
+      if(token) {
+        return (
           <>
            <h2>Farcebook</h2>
-            <nav className='nav-bar'>
-              <div className='nav-item' ><a href='/login'>Login</a></div>
-              <div className='nav-item'><a href='/signup'>Sign up</a></div>
-            </nav>
+           <nav className='nav-bar'>
+             <div className='nav-item' onClick={logout}><a href='/logout'>Logout</a></div>
+           </nav>
           </>
-      )
+        )
+      } else {
+        return (
+          <>
+          <h2>Farcebook</h2>
+           <nav className='nav-bar'>
+             <div className='nav-item' ><a href='/login'>Login</a></div>
+             <div className='nav-item'><a href='/signup'>Sign up</a></div>
+           </nav>
+          </>
+        )
+      }
+
+      
+      // return (
+      //     <>
+      //      <h2>Farcebook</h2>
+      //       <nav className='nav-bar'>
+      //         <div className='nav-item' ><a href='/login'>Login</a></div>
+      //         <div className='nav-item'><a href='/signup'>Sign up</a></div>
+      //         <div className='nav-item'><a href='/logout'>Logout</a></div>
+      //       </nav>
+      //     </>
+      // )
     }
     export default Navbar
+
+
+  
