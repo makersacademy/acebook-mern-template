7325c0dd3f64aae0e8d4a2123813db18715ebdda
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-05-15 22:53:01 +0100

    does not create user account if email already in use

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,13 +1,21 @@
 const User = require("../models/user");
 
 const UsersController = {
-  Create: (req, res) => {
-    const user = new User(req.body);
-    user.save((err) => {
+  Create: async (req, res) => {  //defines a method called "Create" that is an asynchronous function and takes two parameters: "req" and "res", which represent the request and response objects.
+    const { email } = req.body; // extracts the email property from the request body
+    const existingUser = await User.findOne({ email }); //queries the database to see if a user with the provided email already exists.
+                                                        // It uses the User model to find a document in the database that matches the provided email.
+    
+    if (existingUser) {
+      return res.status(409).json({ message: 'Email address already in use' });
+    }  //condition checks if the user with the provided email already exists. If it does, it returns a response with a 409 status code and a message indicating that the email address is already in use.
+    
+    const user = new User(req.body); // creates a new instance of the user model and with the request body as data
+    user.save((err) => {             // saves the user
       if (err) {
-        res.status(400).json({message: 'Bad request'})
+        return res.status(400).json({ message: 'Bad request' });  // if there was an error whilst saving, it will return a 400 status with the message 'Bad request'
       } else {
-        res.status(201).json({ message: 'OK' });
+        return res.status(201).json({ message: 'OK' }); // else if the save is successfull, it will return a 201 status and an 'OK' message
       }
     });
   },
--- api/spec/controllers/users.spec.js
+++ api/spec/controllers/users.spec.js
@@ -48,16 +48,34 @@ describe("/users", () => {
     test("response code is 400", async () => {
       let response = await request(app)
         .post("/users")
-        .send({password: "1234", firstName: "Scarlett", lastName: "Ohara"})
-      expect(response.statusCode).toBe(400)
+        .send({ password: "1234", firstName: "Scarlett", lastName: "Ohara" });
+      expect(response.statusCode).toBe(400);
     });
-
+  
     test("does not create a user", async () => {
       await request(app)
         .post("/users")
-        .send({password: "1234"})
-      let users = await User.find()
-      expect(users.length).toEqual(0)
+        .send({ password: "1234" });
+      let users = await User.find();
+      expect(users.length).toEqual(0);
     });
+  
+    test("returns a 409 if email already exists in the database", async () => {
+      const user = {
+        firstName: "John",
+        lastName: "Doe",
+        email: "johndoe@example.com",
+        password: "password123",
+      };
+      const existingUser = new User(user);
+      await existingUser.save();
+      
+      let response = await request(app)
+        .post("/users")
+        .send(user);
+      expect(response.statusCode).toBe(409);
+      expect(response.body).toEqual({ message: "Email address already in use" });
+    });
+    
+  });
   })
-})
