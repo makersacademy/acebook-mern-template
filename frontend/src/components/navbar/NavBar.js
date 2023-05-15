17280a1eeda9f79b00d0b82a78c036b1d1b5cfe4
Manuela Iacobovici <miacobovici@yahoo.co.uk>
2023-05-15 19:15:06 +0100

    added username to navbar

--- frontend/src/components/navbar/NavBar.js
+++ frontend/src/components/navbar/NavBar.js
@@ -3,29 +3,36 @@
     import './NavBar.css';
     import Feed from '../feed/Feed';
     import LogInForm from '../auth/LoginForm';
+    import SignUpForm from '../user/SignUpForm'
 
     const Navbar = ({ navigate }) => {
-
+      
       const [token, setToken] = useState(window.localStorage.getItem("token"));
-
+      const [username, setUsername] = useState('');
+      
       useEffect(() => {
         if(token !== window.localStorage.getItem("token")) {
           setToken(window.localStorage.getItem("token"))
         }
+        setUsername(window.localStorage.getItem("username"))
       }, [window.localStorage.getItem("token")])
+
+      
+      
+      
 
       const logout = () => {
         window.localStorage.removeItem("token")
         // navigate('/login')
       }
 
-    
       if(token) {
         return (
           <>
            <h2>Farcebook</h2>
            <nav className='nav-bar'>
              <div className='nav-item' onClick={logout}><a href='/logout'>Logout</a></div>
+             <div className="username-format">{username}</div>
            </nav>
           </>
         )
@@ -40,18 +47,6 @@
           </>
         )
       }
-
-      
-      // return (
-      //     <>
-      //      <h2>Farcebook</h2>
-      //       <nav className='nav-bar'>
-      //         <div className='nav-item' ><a href='/login'>Login</a></div>
-      //         <div className='nav-item'><a href='/signup'>Sign up</a></div>
-      //         <div className='nav-item'><a href='/logout'>Logout</a></div>
-      //       </nav>
-      //     </>
-      // )
     }
     export default Navbar
 
