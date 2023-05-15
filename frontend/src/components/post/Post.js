b3a431a3f79b9fe2b844964db73c118ce1a00486
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-05-15 16:57:40 +0100

    Add formatting to display of username and timestamp for posts

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Post = ({post}) => {
   return(
-    <article data-cy="post" key={ post._id }>{ post.message }{ post.createdAt }{ post.author }</article>
-    // <article data-cy="post" key={ post._id }>{ post.message }</article>
+    <article data-cy="post" key={ post._id }>
+      <p>{ post.message }</p>
+      <p>Posted at { post.createdAt.slice(11, 16) } on { post.createdAt.slice(0, 10) }</p>
+      <p>By  { post.author }</p>
+    </article>
   )
 }
 
