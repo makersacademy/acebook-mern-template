1ae22bf5e093ee4496258a1e88d85a91e562dfc7
ana carolina <anacarolinarodriguesoares@gmail.com>
2023-05-16 21:10:26 +0100

    adding testing for likes in the models.

--- api/spec/models/post.spec.js
+++ api/spec/models/post.spec.js
@@ -9,10 +9,24 @@ describe("Post model", () => {
       done();
     });
   });
-
   it("has a message", () => {
     var post = new Post({ message: "some message" });
     expect(post.message).toEqual("some message");
+  });
+
+  it("finding a like in a existing post", async() => {
+    //to be able to test an object id, we need to create one, that's why we are using mongoose
+    var id = mongoose.Types.ObjectId();
+    var post = new Post({ message: "some message", authorUserID: "645fd378ceaefce663b8b497", likes: [id] });
+
+    post.save((err) => {
+      expect(err).toBeNull();
+       Post.find( (err, posts) => {
+        expect(err).toBeNull();
+        expect(posts[0].likes.length).toEqual(1)
+        expect(posts[0].likes[0]).toEqual(id)
+      });
+    });
   });
 
   it("can list all posts", (done) => {
