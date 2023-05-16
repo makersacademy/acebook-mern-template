02024f10bb1772ee1b4e05d3807c2c7875ae9069
umut <umut_baykan@hotmail.com>
2023-05-16 17:47:04 +0100

    renders user profile picture and resizes and centres it

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
-import Like from '../like/Like'
-import DataURLToJPGConverter from '../DataURLToJPGConverter/DataURLToJPGConverter';
+import React from 'react';
+import Like from '../like/Like';
+import './Post.css';
 
 const Post = ({post}) => {
   var date = new Date(post.createdDateTime);
   var formattedDate = date.toUTCString()
+  
   return(
     <>
     <h3>{ post.author.userName }</h3>
-    <DataURLToJPGConverter dataURL={post.author.photo}/>
+    <div className="user-profile-picture" >
+      <img src={post.author.photo} alt="user-profile-picture"/>
+    </div>
     <article data-cy="post" key={ post._id }>{ post.message } { formattedDate } </article>
     <Like likes={post.likes} post_id={ post._id } didUserLikeThis={post.didUserLikeThis}/>
     </>
