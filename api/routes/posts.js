8962785dfcaac86e623b6d262baeb75be8338cd3
Matt Jones <67526111+Matt-J-Jones@users.noreply.github.com>
2023-05-16 15:18:57 +0100

    Clicking on a liked post removes the like

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -6,6 +6,7 @@ const PostsController = require("../controllers/posts");
 router.get("/", PostsController.Index);
 router.post("/", PostsController.Create);
 router.post("/:id/likes", PostsController.AddLikes);
+router.delete("/:id/likes", PostsController.RemoveLikes);
 router.post("/:id/comments", PostsController.CreateComment);
 
 module.exports = router;
