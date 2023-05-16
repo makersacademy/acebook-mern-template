87ba3f9af5c0c5d3cd84a000886cd92d35f8b4e7
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-05-16 11:46:32 +0100

    Remove errnoeuous commented out code block in posts.js

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -15,30 +15,6 @@ const PostsController = {
       });
   },
 
-  // Create: (req, res) => {
-  //   console.log(req.user_id)
-  //   // check for valid token and extract userID from it
-  //   let token;
-  //   const authHeader = req.get("Authorization")
-  //   if(authHeader) { token = authHeader.slice(7) }
-  //   JWT.verify(token, process.env.JWT_SECRET, (err, payload) => {
-  //     if(err) {
-  //       console.log(err)
-  //       res.status(401).json({message: "auth error"});
-  //     } else {
-  //       console.log(payload.user_id)
-  //       const post = new Post({ ...req.body, authorUserID: payload.user_id });
-  //       post.save(async (err) => {
-  //         if (err) {
-  //           throw err;
-  //         }
-  //         const token = await TokenGenerator.jsonwebtoken(req.user_id)
-  //         res.status(201).json({ message: 'OK', token: token });
-  //       });
-  //     }
-  //   });
-  // }
-
   Create: (req, res) => {
     if (!req.user_id) { 
       return res.status(401).json({ message: 'Auth error' })
