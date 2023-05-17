270af910e98bcd0751131869e2ac712dbd03f0d3
Quddus Rahman <qrahman115@gmail.com>
2023-05-17 16:12:24 +0100

    login form updated with css

--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './LoginForm.css';
 
 const LogInForm = ({ navigate }) => {
   const [email, setEmail] = useState("");
@@ -19,7 +20,7 @@ const LogInForm = ({ navigate }) => {
       console.log("yay")
       navigate('/login')
     } else {
-      console.log("oop")
+      console.log("oops")
       let data = await response.json()
       window.localStorage.setItem("token", data.token)
       navigate('/posts');
@@ -35,13 +36,21 @@ const LogInForm = ({ navigate }) => {
   }
 
 
-    return (
-      <form onSubmit={handleSubmit}>
-        <input placeholder='Email' id="email" type='text' value={ email } onChange={handleEmailChange} />
-        <input placeholder='Password' id="password" type='password' value={ password } onChange={handlePasswordChange} />
-        <input role='submit-button' id='submit' type="submit" value="Submit" />
+  return (
+    <div className='login-container'>
+      <form className='login-form' onSubmit={handleSubmit}>
+        <div className='form-input'>
+          <input placeholder='Email' id="email" type='text' value={ email } onChange={handleEmailChange} />
+        </div>
+        <div className='form-input'>
+          <input placeholder='Password' id="password" type='password' value={ password } onChange={handlePasswordChange} />
+        </div>
+        <div className='login-button-container'>
+          <input className='login-button' role='submit-button' id='submit' type="submit" value="Submit" />
+        </div>
       </form>
-    );
+    </div>
+  );  
 }
 
 export default LogInForm;
