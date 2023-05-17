6e9390a2a43abb621946dcc6d2ac3ffa0aedf50b
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-05-17 11:46:02 +0100

    Fix failing tests for addComment and addPost

--- frontend/src/components/AddComment/AddComment.cy.js
+++ frontend/src/components/AddComment/AddComment.cy.js
@@ -1,12 +1,26 @@
 import AddComment from "./AddComment";
 
+const mockPost =
+  {
+    _id: 1, 
+    message: "Hello, world",
+    authorUserID: {
+                    username: "Cypress",
+                    _id: "mockHexUserObjectID",
+                    email: "cypress@hill.com",
+                  },
+    createdAt: "2023-05-11T14:09:31.064+00:00",
+    comments: [{comment: "fee", author: "fi"}, {comment: "fo", author: "fum"} ]
+  }
+
 describe("AddComment", () => {
   it("creates a new comment within a post", () => {
     window.localStorage.setItem("token", "fakeToken")
     
-    cy.mount(<AddComment post={ {_id: "mockHexPostID" }} />);
+    // cy.mount(<AddComment post={ {_id: "mockHexPostID" }} />);
+    cy.mount(<AddComment post = { _id: "mockHexPostID" } />);
 
-    cy.intercept('POST', '/posts/mockHexPostID', {
+    cy.intercept('PATCH', '/posts/1', {
       statusCode: 200,
       body: {
         comment: 'The comment under Hello FarceBook',
@@ -15,9 +29,7 @@ describe("AddComment", () => {
       },
     }).as('newCommentRequest'); 
 
-    // cy.get('#comment').type('text');
     cy.get('[data-cy="comment"]').type('text');
-    // cy.get('#submit-comment').click();
     cy.get('[data-cy="submit"]').click();
 
     cy.wait('@newCommentRequest').then((interception) => {
