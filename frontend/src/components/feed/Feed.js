97f75e9d21319bf9edbfe3df7fed25ceb0464f91
Pablo <pablito@me.com>
2023-05-18 20:27:37 +0100

    duplicate code fix

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -14,8 +14,6 @@ const Feed = ({ navigate }) => {
 
   const [showComments, setShowComments] = useState(false);    
 
-  const [showComments, setShowComments] = useState(false);  
-
   function orderByDate (posts) {
     return posts.sort((a,b) => new Date(a.createdAt) - new Date(b.createdAt)).reverse()
   }
