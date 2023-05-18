079af24a4c5504d2f049b1942869dc2bd86daa35
kjartanh <kheimission@gmail.com>
2023-05-18 16:15:29 +0100

    added changes to post and feed layout

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -9,20 +9,22 @@ const Post = ({post}) => {
   return(
     <>
       <div className="post">
-        <div className="user-info">
-          <div className="user-profile-picture" >
-            {post.author.photo ? (
-              <img src={post.author.photo} alt="user-profile"/>
-            ) : (
-              <img src="/happy-fox.jpeg" alt="default-profile" />
-            )}
+        <div className="post-container">
+          <div className="user-info">
+            <div className="user-profile-picture" >
+              {post.author.photo ? (
+                <img src={post.author.photo} alt="user-profile"/>
+              ) : (
+                <img src="/happy-fox.jpeg" alt="default-profile" />
+              )}
+            </div>
+            <h3 className="user-name">{ post.author.userName }</h3>
           </div>
-          <h3 className="user-name">{ post.author.userName }</h3>
-        </div>
-        <div className="post-content">
-          <article className="post-message" data-cy="post" key={ post._id }>{ post.message } </article>
-          <article className="post-date" data-cy="post" key ={ post.id }>{ formattedDate } </article>
-          <Like likes={post.likes} post_id={ post._id } didUserLikeThis={post.didUserLikeThis}/>
+          <div className="post-content">
+            <article className="post-message" data-cy="post" key={ post._id }>{ post.message } </article>
+            <article className="post-date" data-cy="post" key ={ post.id }>{ formattedDate } </article><br></br>
+            <Like likes={post.likes} post_id={ post._id } didUserLikeThis={post.didUserLikeThis}/>
+          </div>
         </div>
       </div>
     </>
