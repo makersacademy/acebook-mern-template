d46b7284ffb256ccfdead85afccea8aa23922a02
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-05-18 12:45:34 +0100

    delete confirmation pop functional

--- frontend/src/components/updates/UserForm.js
+++ frontend/src/components/updates/UserForm.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import './UserForm.css';
+import React, { useState, useEffect } from "react";
+import "./UserForm.css";
 
 const UserForm = ({ navigate }) => {
   const [email, setEmail] = useState("");
@@ -7,12 +7,13 @@ const UserForm = ({ navigate }) => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
-  const [responseStatus, setResponseStatus] = useState(null); 
+  const [responseStatus, setResponseStatus] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-  console.log(window.localStorage.getItem("token"));
+    console.log(window.localStorage.getItem("token"));
 
     fetch("/userUpdatesRoute", {
       method: "put",
@@ -26,22 +27,20 @@ const UserForm = ({ navigate }) => {
         lastName: lastName,
       }),
     }).then((response) => {
-        console.log(response.status);
+      console.log(response.status);
       if (response.status === 200) {
         setSuccessMessage("Your changes have been updated successfully.");
 
         // Clear the input fields
-      setEmail("");
-      setPassword("");
-      setFirstName("");
-      setLastName("");
-      
+        setEmail("");
+        setPassword("");
+        setFirstName("");
+        setLastName("");
       } else {
         setSuccessMessage("Changes failed, please try again.");
       }
     });
   };
-   
 
   const handleEmailUpdate = (event) => {
     setEmail(event.target.value);
@@ -60,7 +59,11 @@ const UserForm = ({ navigate }) => {
   };
 
   const handleDelete = () => {
+    setShowPopup(true);
+  };
 
+  const handlePopupOK = () => {
+    // Handle OK button click
     window.localStorage.removeItem("token");
 
     fetch("/userUpdatesRoute", {
@@ -69,91 +72,122 @@ const UserForm = ({ navigate }) => {
       .then((response) => {
         if (response.status === 200) {
           setSuccessMessage("Your account has been deleted successfully.");
-        //  setFirstName("Unknown");
-       //  setLastName("User");
           navigate("/goodbye");
         } else {
-          setSuccessMessage("Account deletion failed, please contact our amazing team for support!");
+          setSuccessMessage(
+            "Account deletion failed, please contact our amazing team for support!"
+          );
         }
       })
       .catch((error) => {
         setSuccessMessage("Account deletion failed, please try again.");
       });
+
+    setShowPopup(false);
   };
+
+  const handlePopupCancel = () => {
+    // Handle Cancel button click
+    setShowPopup(false);
+  };
+
+  useEffect(() => {
+    const popupElement = document.getElementById("popup");
+    if (popupElement) {
+      popupElement.style.display = showPopup ? "block" : "none";
+    }
+  }, [showPopup]);
 
   return (
     <form onSubmit={handleSubmit}>
-    <div class="form-header">
-      <p>Update your details here:</p>
-    </div>
-
-    <div class="form-group">
-      <div class="aligned-credentials">
-        <div class="email-box-space">
-          <input
-            placeholder="Email"
-            id="email"
-            type="text"
-            value={email}
-            onChange={handleEmailUpdate}
-          />
-        </div>
-         <div class="password-box-space">
-          <input
-            placeholder="Password"
-            id="password"
-            type="password"
-            value={password}
-            onChange={handlePasswordUpdate}
-          /> 
-        </div> 
+      <div className="form-header">
+        <p>Update your details here:</p>
       </div>
-    </div>
 
-    <div class="form-group">
-      <div class="aligned-names">
-        <div class="first-name-box-space">
-          <input
-            placeholder="First Name"
-            id="firstName"
-            type="text"
-            value={firstName}
-            onChange={handleFirstNameUpdate}
-          />
-        </div>
-        <div class="last-name-box-space">
-          <input
-            placeholder="Last Name"
-            id="lastName"
-            type="text"
-            value={lastName}
-            onChange={handleLastNameUpdate}
-          />
+      <div className="form-group">
+        <div className="aligned-credentials">
+          <div className="email-box-space">
+            <input
+              placeholder="Email"
+              id="email"
+              type="text"
+              value={email}
+              onChange={handleEmailUpdate}
+            />
+          </div>
+          <div className="password-box-space">
+            <input
+              placeholder="Password"
+              id="password"
+              type="password"
+              value={password}
+              onChange={handlePasswordUpdate}
+            />
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="form-group">
-      <input id="submit" type="submit" value="Submit" />
-    </div>
-    
+      <div className="form-group">
+        <div className="aligned-names">
+          <div className="first-name-box-space">
+            <input
+              placeholder="First Name"
+              id="firstName"
+              type="text"
+              value={firstName}
+              onChange={handleFirstNameUpdate}
+            />
+          </div>
+          <div className="last-name-box-space">
+            <input
+              placeholder="Last Name"
+              id="lastName"
+              type="text"
+              value={lastName}
+              onChange={handleLastNameUpdate}
+            />
+          </div>
+        </div>
+      </div>
 
-<div class="form-group">
-      <button type="button" onClick={handleDelete}>
-        Delete Account
-      </button>
-    </div>
+      <div className="form-group">
+        <input id="submit" type="submit" value="Submit" />
+      </div>
 
-    {successMessage && (
+      <div className="form-group">
+        <button type="button" onClick={handleDelete}>
+          Delete Account
+        </button>
+      </div>
+
+      {successMessage && (
         <div
-        className={`success-message ${successMessage.includes('failed') ? 'error' : 'ok'}`}
+          className={`success-message ${
+            successMessage.includes("failed") ? "error" : "ok"
+          }`}
         >
           {successMessage}
         </div>
-)}
-  </form>
+      )}
+
+      {showPopup && (
+        <div id="popup" className="popup">
+          <div className="popup-content">
+            <h2>Confirmation</h2>
+            <p>Are you sure you want to delete your account?</p>
+            <div className="popup-buttons">
+              <button id="popup-ok" onClick={handlePopupOK}>
+                OK
+              </button>
+              <button id="popup-cancel" onClick={handlePopupCancel}>
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </form>
   );
 };
 
 export default UserForm;
-
