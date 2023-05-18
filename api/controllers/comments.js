0e4465fbddb40bf7c1d1a7fc6da6d5cf82983bba
umut <umut_baykan@hotmail.com>
2023-05-18 15:12:11 +0100

    back end integration

--- api/controllers/comments.js
+++ api/controllers/comments.js
@@ -1,22 +1,18 @@
-const Comment = require("../models/comment");
+const Comment = require("../models/comment").Comment;
 const Post = require("../models/post");
 const TokenGenerator = require("../models/token_generator");
 
 const CommentsController = {
   Create: async (req, res) => {
-    console.log("here")
     const comment = new Comment();
-    comment.author = req.params.userId;
+    comment.author = req.user_id;
     comment.message = req.body.message;
-    const postId = req.params.postId;
-    console.log(postId)
-    const post = await Post.findById(postId);
-
+    const post = await Post.findById(req.body.post_id);
     post.comments.push(comment);
     await post.save();
-
     const token = await TokenGenerator.jsonwebtoken(req.user_id);
-    res.status(201).json({ message: 'Comment Added', token: token });
+    res.status(201).json({comment: comment, post: post, token: token})
+    // res.status(201).json({ message: 'Comment Added', token: token });
   },
 
   /*
--- api/models/comment.js
+++ api/models/comment.js
@@ -2,8 +2,12 @@ const mongoose = require("mongoose");
 
 const CommentSchema = new mongoose.Schema({
   message: String,
-  createdDateTime: Date,
-  user: String,
+  // createdDateTime: Date,
+  // user: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 const Comment = mongoose.model("Comment", CommentSchema);
