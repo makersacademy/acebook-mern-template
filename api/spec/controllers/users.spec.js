20d62a0ef4a7000b3c16f55d2f50a04924e72624
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-05-18 16:18:05 +0100

    Create passing tests for email validation is spec/controllers/users

--- api/spec/controllers/users.spec.js
+++ api/spec/controllers/users.spec.js
@@ -26,6 +26,36 @@ describe("/users", () => {
     })
   })
 
+  describe("POST, when password is invalid", () => {
+    test("the response code is 400 with less than 8 chars", async () => {
+      let response = await request(app)
+        .post("/users")
+        .send({email: "poppy@email.com", password: "Pa12", username: "poppy"})
+      expect(response.statusCode).toBe(400)
+    })
+    test("the response code is 400 with more than 8 chars", async () => {
+      let response = await request(app)
+        .post("/users")
+        .send({email: "poppy@email.com", password: "PPPAAAssswwwooorrrdd1234", username: "poppy"})
+      expect(response.statusCode).toBe(400)
+    })
+  })
+
+  describe("POST, when password is valid", () => {
+    test("the response code is 201", async () => {
+      let response = await request(app)
+        .post("/users")
+        .send({email: "poppy@email.com", password: "1234passworD", username: "poppy"})
+      expect(response.statusCode).toBe(201)
+    })
+    test("the response code is 201 with special chars", async () => {
+      let response = await request(app)
+        .post("/users")
+        .send({email: "poppy@email.com", password: "1234passworD!@?", username: "poppy"})
+      expect(response.statusCode).toBe(201)
+    })
+  })
+
   describe("POST, when password is missing", () => {
     test("response code is 400", async () => {
       let response = await request(app)
