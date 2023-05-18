461e97abdc7898b9c9bf7757f4aa3c0437752c9b
ana carolina <anacarolinarodriguesoares@gmail.com>
2023-05-18 21:01:25 +0100

    testing for validation when email exist

--- api/spec/controllers/users.spec.js
+++ api/spec/controllers/users.spec.js
@@ -89,4 +89,23 @@ describe("/users", () => {
       expect(users.length).toEqual(0)
     });
   })
+
+  describe("POST, when email already exist", () => {
+    test("the response code is 400", async () => {
+       let responseOK= await request(app)
+        .post("/users")
+        .send({email: "poppy@email.com", password: "1234passworD", username: "poppy"})
+
+        expect(responseOK.statusCode).toBe(201)
+
+        let responseNok = await request(app)
+        .post("/users")
+        .send({email: "poppy@email.com", password: "1234passworD", username: "poppy"})
+
+      expect(responseNok.statusCode).toBe(400)
+
+      
+    })
+    
+  })
 })
