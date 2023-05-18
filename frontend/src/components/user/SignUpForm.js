8d9425056c7276f24cc985e579c62a95f5966e7e
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-05-18 13:17:54 +0100

    password confirmation on signup

--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -1,15 +1,26 @@
 import React, { useState } from "react";
-import './SignUpForm.css';
+import "./SignUpForm.css";
 
 const SignUpForm = ({ navigate }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [firstName, setfirstName] = useState("");
-  const [lastName, setlastName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [passwordsMatchError, setPasswordsMatchError] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
+    if (password !== "") {
+      setShowConfirmation(true);
+    } else {
+      submitForm();
+    }
+  };
+
+  const submitForm = () => {
     fetch("/users", {
       method: "post",
       headers: {
@@ -30,6 +41,22 @@ const SignUpForm = ({ navigate }) => {
     });
   };
 
+  const handleConfirm = () => {
+    if (confirmPassword === password) {
+      setShowConfirmation(false);
+      setPasswordsMatchError(false); // Reset error state when password is correct
+      submitForm();
+    } else {
+      setPasswordsMatchError(true);
+      const inputBox = document.getElementById("confirmPassword");
+      inputBox.classList.add("error");
+    }
+  };
+
+  const handleCancel = () => {
+    setShowConfirmation(false);
+  };
+
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
   };
@@ -38,70 +65,98 @@ const SignUpForm = ({ navigate }) => {
     setPassword(event.target.value);
   };
 
-  const handlefirstNameChange = (event) => {
-    setfirstName(event.target.value);
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
   };
 
-  const handlelastNameChange = (event) => {
-    setlastName(event.target.value);
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-    <div class="form-header">
-      <p>Please complete the details below to create your account:</p>
-    </div>
+      <div className="form-header">
+        <p>Please complete the details below to create your account:</p>
+      </div>
 
-    <div class="form-group">
-      <div class="aligned-credentials">
-        <div class="email-box-space">
-          <input
-            placeholder="Email"
-            id="email"
-            type="text"
-            value={email}
-            onChange={handleEmailChange}
-          />
+      <div className="form-group">
+        <div className="aligned-credentials">
+          <div className="email-box-space">
+            <input
+              placeholder="Email"
+              id="email"
+              type="text"
+              value={email}
+              onChange={handleEmailChange}
+              required
+            />
+          </div>
+          <div className="password-box-space">
+            <input
+              placeholder="Password"
+              id="password"
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+            />
+          </div>
         </div>
-        <div class="password-box-space">
+      </div>
+
+      <div className="form-group">
+        <div className="aligned-names">
+          <div className="first-name-box-space">
+            <input
+              placeholder="First Name"
+              id="firstName"
+              type="text"
+              value={firstName}
+              onChange={handleFirstNameChange}
+              required
+            />
+          </div>
+          <div className="last-name-box-space">
+            <input
+              placeholder="Last Name"
+              id="lastName"
+              type="text"
+              value={lastName}
+              onChange={handleLastNameChange}
+              required
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="form-group">
+        <input id="submit" type="submit" value="Submit" />
+      </div>
+
+      {showConfirmation && (
+        <div className="confirmation-popup">
           <input
-            placeholder="Password"
-            id="password"
+            placeholder="Confirm Password"
+            id="confirmPassword"
             type="password"
-            value={password}
-            onChange={handlePasswordChange}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
           />
+          {passwordsMatchError && (
+            <p className="error-message">Passwords don't match</p>
+          )}
+          <div className="confirmation-buttons">
+            <button type="button" onClick={handleConfirm}>
+              Confirm
+            </button>
+            <button type="button" onClick={handleCancel}>
+              Cancel
+            </button>
+          </div>
         </div>
-      </div>
-    </div>
-
-    <div class="form-group">
-      <div class="aligned-names">
-        <div class="first-name-box-space">
-          <input
-            placeholder="First Name"
-            id="firstName"
-            type="text"
-            value={firstName}
-            onChange={handlefirstNameChange}
-          />
-        </div>
-        <div class="last-name-box-space">
-          <input
-            placeholder="Last Name"
-            id="lastName"
-            type="text"
-            value={lastName}
-            onChange={handlelastNameChange}
-          />
-        </div>
-      </div>
-    </div>
-
-    <div class="form-group">
-      <input id="submit" type="submit" value="Submit" />
-    </div>
-  </form>
+      )}
+    </form>
   );
 };
 
