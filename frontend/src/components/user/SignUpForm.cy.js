b1b42b5a404c4eb6f50da349b7f92bd1052ef182
marco belahouane <nfk18@hotmail.co.uk>
2023-06-07 16:16:02 +0100

    writing cypress test

--- frontend/src/components/user/SignUpForm.cy.js
+++ frontend/src/components/user/SignUpForm.cy.js
@@ -9,9 +9,26 @@ describe("Signing up", () => {
 
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("password");
+    cy.get("#firstName").type("test");
+    cy.get("#lastName").type("testson");
+    cy.get("#userName").type("testy");
     cy.get("#submit").click();
     cy.wait('@signUpRequest').then( interception => {
       expect(interception.response.body.message).to.eq("OK")
+    })
+  })
+  it("does not call the /users endpoint if a field is empty", () => {
+    cy.mount(<SignUpForm navigate={navigate}/>)
+
+    cy.intercept('POST', '/users', { message: "TEST" }).as("signUpRequest")
+
+    cy.get("#email").type("someone@example.com");
+    cy.get("#firstName").type("test");
+    cy.get("#lastName").type("testson");
+    cy.get("#userName").type("testy");
+    cy.get("#submit").click();
+    cy.wait('@signUpRequest').then( interception => {
+      expect(interception.response.body.message).to.eq("TEST")
     })
   })
 })
--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -4,6 +4,11 @@ const SignUpForm = ({ navigate }) => {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userName, setUserName] = useState("");
+
+
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -13,7 +18,13 @@ const SignUpForm = ({ navigate }) => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ email: email, password: password })
+      body: JSON.stringify({ 
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName,
+        userName: userName,
+      })
     })
       .then(response => {
         if(response.status === 201) {
@@ -31,12 +42,24 @@ const SignUpForm = ({ navigate }) => {
   const handlePasswordChange = (event) => {
     setPassword(event.target.value)
   }
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value)
+  }
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value)
+  }
+  const handleUserNameChange = (event) => {
+    setUserName(event.target.value)
+  }
 
 
     return (
       <form onSubmit={handleSubmit}>
           <input placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
           <input placeholder="Password" id="password" type='password' value={ password } onChange={handlePasswordChange} />
+          <input placeholder="First Name" id="firstName" type='text' value={ firstName } onChange={handleFirstNameChange} />
+          <input placeholder="Last Name" id="lastName" type='text' value={ lastName } onChange={handleLastNameChange} />
+          <input placeholder="User Name" id="userName" type='text' value={ userName } onChange={handleUserNameChange} />
         <input id='submit' type="submit" value="Submit" />
       </form>
     );
