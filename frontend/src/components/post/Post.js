74ec0b842057d76d18f89145f23e5bcf636a2351
hanguyen <lotusviet.uk@gmail.com>
2023-06-08 15:48:19 +0100

    change date to the right format

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 const Post = ({post}) => {
+  const formattedDate = new Date(post.time).toLocaleString('en-GB', {
+    day: 'numeric',
+    month: 'long',
+    hour: 'numeric',
+    minute: 'numeric',
+  });
+  
   return(
     <article data-cy="post" key={ post._id }> 
-      <p>{ post.time } </p>
+      <p>{ formattedDate } </p>
       <p>{ post.message }</p>
     </article>
   )
-}
+};
 
 export default Post;
