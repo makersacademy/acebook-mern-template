4fba244531b8fb869cddc7eac74da45add0f3190
Sharmine <sharmine.ss@gmail.com>
2023-06-08 14:48:44 +0100

    signing up with an existing email now returns a 409 error

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -5,9 +5,13 @@ const UsersController = {
     const user = new User(req.body);
     user.save((err) => {
       if (err) {
-        res.status(400).json({message: 'Bad request'})
+        if (err.code === 11000) {
+          res.status(409).json({ message: "Email already exists. Please choose a different email." });
+        } else {
+          res.status(400).json({ message: "Bad request" });
+        }
       } else {
-        res.status(201).json({ message: 'OK' });
+        res.status(201).json({ message: "OK" });
       }
     });
   },
