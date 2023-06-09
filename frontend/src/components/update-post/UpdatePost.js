3eefc2bf4f77af28fb75330a5cf0a35c8080816c
Barney Wilton <B.Wilton1993@gmail.com>
2023-06-09 14:08:57 +0100

    Figured out how to extract the post_id from the params in react

--- frontend/src/components/update-post/UpdatePost.js
+++ frontend/src/components/update-post/UpdatePost.js
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react"
+import { useParams } from "react-router";
 
 const UpdatePost = ({ navigate }) => {
-  const [token, setToken] = useState(window.localStorage.getItem("token"));
-  const post_id = '6483207441bded05b7bfe1fa'
-
+  // const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const post_id = useParams();
+  console.log(post_id.id);
+  
   return(
     <>
-      <h2>Post {`${post_id}`}</h2>
+      <h2>Post {`${post_id.id}`}</h2>
       <div id='feed' role="feed">
           <p>test</p>
       </div>
     </>
   )
-
 }
 
 export default UpdatePost;
