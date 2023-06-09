5fe20d0b2770fd94cfc9f8362f0b106dfbce7f2b
Ormeline <ormelineluyengi@outlook.com>
2023-06-09 11:49:33 +0100

    changed .json to .send -receive plain text rspnse

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -14,21 +14,21 @@ const UsersController = {
 
   //This gets the user's username based on their userId
   // Find the user by their userId and retrieve only the 'username' field
-  // If an error is found send a JSON response with a status of 400 (Bad Request)
-  // If a user is found, send a JSON response with a status of 200 (OK) and the retrieved username
-  // If a user is not found, send a JSON response with a status of 404 (Not Found)
+  // If an error is found sends a (plain text) response with a status of 400 (Bad Request)
+  // If a user is found, sends a response with a status of 200 (OK) and the retrieved username
+  // If a user is not found, sends a response with a status of 404 (Not Found)
   GetUsername: (req, res) => {
     const { user_Id } = req.params;
 
     User.findById(user_Id, 'username', (err, user) => {
       if (err) {
         
-        res.status(400).json({ message: 'Bad request' });
+        res.status(400).send({ message: 'Bad request' });
       } else {
         if (user) {
-          res.status(200).json({ username: user.username });
+          res.status(200).send({ username: user.username });  //can use .send instead of returning the json object
         } else {
-          res.status(404).json({ message: 'User not found' });
+          res.status(404).send({ message: 'User not found' });
         }
       }
     });
