da7b3ac2e7d7c48e0b14de612df03fa06278df97
Cameron <cameronjamesbishop@googlemail.com>
2023-06-09 14:56:40 +0100

    Started SPIKE on default avatar 2

--- api/models/user.js
+++ api/models/user.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true, default: "Name" },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  avatar: { type: String, required: true, default: "/sample_avatar.png"}
 });
 
 const User = mongoose.model("User", UserSchema);
