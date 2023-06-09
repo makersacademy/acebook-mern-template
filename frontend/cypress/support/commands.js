b438d1b2cf9f2425de1f8acac40cc8e93a399b1d
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-06-09 17:08:21 +0100

    signing up test to be fixed

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -1,4 +1,3 @@
-var mongoose = require("mongoose");
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -11,13 +10,10 @@ var mongoose = require("mongoose");
 //
 //
 // -- This is a parent command --
-Cypress.Commands.add('signup', (email, password, firstName, lastName, userName) => {
+Cypress.Commands.add('signup', (email, password) => {
   cy.visit("/signup");
   cy.get("#email").type(email);
   cy.get("#password").type(password);
-  cy.get("#firstName").type(firstName);
-  cy.get("#lastName").type(lastName);
-  cy.get("#userName").type(userName);
   cy.get("#submit").click();
 })
 //
@@ -32,32 +28,3 @@ Cypress.Commands.add('signup', (email, password, firstName, lastName, userName) 
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
-Cypress.Commands.add('clearDatabase', () => {
-
-  const uri = 'mongodb://localhost:27017/acebook_test';
-
-  // Esegui la cancellazione dei dati dalla collezione degli utenti
-  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-    // Puoi eseguire altre operazioni dopo la connessione al database qui
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
-
-    const db = client.db();
-    const usersCollection = db.collection('users');
-
-    usersCollection.deleteMany({}, (err, result) => {
-      if (err) {
-        console.error('Error deleting documents from users collection:', err);
-        client.close();
-        return;
-      }
-
-      console.log('Deleted documents from users collection:', result.deletedCount);
-      client.close();
-    });
-  });
