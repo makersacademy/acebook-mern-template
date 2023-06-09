173948beb1e259c26fa90f4e0f5056595249912b
Barney Wilton <B.Wilton1993@gmail.com>
2023-06-09 14:32:07 +0100

    Created frontend react component for updating a post - still need to write tests for this

--- frontend/src/components/update-post/UpdatePost.js
+++ frontend/src/components/update-post/UpdatePost.js
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router";
 
 const UpdatePost = ({ navigate }) => {
-  const [postContent, setPostContent] = useState([]);
+  const [placeholderContent, setplaceholderContent] = useState([]);
+  const [message, setMessage] = useState("");
   const [token, setToken] = useState(window.localStorage.getItem("token"));
 
   const post_id = useParams();
-  // console.log(post_id.id);
   
   useEffect(() => {
     if(token) {
@@ -19,25 +19,38 @@ const UpdatePost = ({ navigate }) => {
         .then(async data => {
           window.localStorage.setItem("token", data.token)
           setToken(window.localStorage.getItem("token"))
-          setPostContent(data.post.message)
+          setplaceholderContent(data.post.message)
         })
     }
   }, [])
     
-  const handleSubmit = () => {
-
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // below: this is the method that handles the post request
+      await fetch('/posts/' + post_id.id + "/update", {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+      },
+      body: JSON.stringify({
+        message: message
+      })
+    })
+    // navigate('/posts/' + post_id.id + "/update")
+    navigate('/posts');
   }
 
-  const handleMessageChange = () => {
-
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);    
   }
   
   return(
     <>
       <h2>Update post: {`${post_id.id}`}</h2>
-      <p>{postContent}</p>
+      <p>{placeholderContent}</p>
       <form onSubmit={handleSubmit}>
-      <input placeholder={postContent} id="message" type="text" onChange={handleMessageChange} />
+      <input placeholder={placeholderContent} id="message" type="text" value={message} onChange={handleMessageChange} />
       <input role='submit-button' id='submit' type="submit" value="Post!" />
     </form>
     </>
