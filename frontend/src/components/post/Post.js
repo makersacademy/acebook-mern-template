2cb1c7763e78abb4c57798034e4505d6253e2799
awdem <wdemontmollin@gmail.com>
2023-06-09 14:27:56 +0100

    added textarea to post with form scaffolding

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Post = ({post}) => {
   return(
-    <article data-cy="post" key={ post._id }>{ post.message }</article>
+    <>
+      <article data-cy="post" key={post._id}>{post.message}</article>
+      <form>
+        <textarea></textarea>
+        <button>Submit</button>
+      </form>
+    </>
   )
 }
 
