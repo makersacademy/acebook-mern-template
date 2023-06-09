ff5779e606750e58edc321afb4596c1ee2c48b56
Barney Wilton <B.Wilton1993@gmail.com>
2023-06-09 14:26:36 +0100

    Can extract the placeholder content to put in the form, so the user knows what they are updating

--- frontend/src/components/update-post/UpdatePost.js
+++ frontend/src/components/update-post/UpdatePost.js
@@ -2,16 +2,44 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router";
 
 const UpdatePost = ({ navigate }) => {
-  // const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [postContent, setPostContent] = useState([]);
+  const [token, setToken] = useState(window.localStorage.getItem("token"));
+
   const post_id = useParams();
-  console.log(post_id.id);
+  // console.log(post_id.id);
+  
+  useEffect(() => {
+    if(token) {
+      fetch("/posts/" + post_id.id , {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
+        .then(response => response.json())
+        .then(async data => {
+          window.localStorage.setItem("token", data.token)
+          setToken(window.localStorage.getItem("token"))
+          setPostContent(data.post.message)
+        })
+    }
+  }, [])
+    
+  const handleSubmit = () => {
+
+  }
+
+  const handleMessageChange = () => {
+
+  }
   
   return(
     <>
-      <h2>Post {`${post_id.id}`}</h2>
-      <div id='feed' role="feed">
-          <p>test</p>
-      </div>
+      <h2>Update post: {`${post_id.id}`}</h2>
+      <p>{postContent}</p>
+      <form onSubmit={handleSubmit}>
+      <input placeholder={postContent} id="message" type="text" onChange={handleMessageChange} />
+      <input role='submit-button' id='submit' type="submit" value="Post!" />
+    </form>
     </>
   )
 }
