7e160cb220909302f20f62697a2336b48419cb72
Michael Szczepanski <m.szczepanski90@gmail.com>
2023-06-09 12:13:36 +0100

    like button and user_id retrieval complete

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Post from '../post/Post';
 import PostCreateForm from '../post/PostCreateForm';
+import jwt_decode from "jwt-decode";
 
 const Feed = ({ navigate }) => {
   const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token")); // Retrieves a token from the browser storage
+  const [userId, setUserId] = useState("");
 
   useEffect(() => {
     // Will send a fetch request if a valid token is found
@@ -19,6 +21,7 @@ const Feed = ({ navigate }) => {
         .then(async data => {
           window.localStorage.setItem("token", data.token)
           setToken(window.localStorage.getItem("token"))
+          setUserId(jwt_decode(token).user_id)
           setPosts(data.posts);
         })
     }
@@ -40,7 +43,7 @@ const Feed = ({ navigate }) => {
           <PostCreateForm />
           <div id='feed' role="feed">
             {posts.map(
-              (post) => ( <Post post={ post } key={ post._id } /> )
+              (post) => ( <Post post={ post } key={ post._id } userId={ userId }/> )
             )}
           </div>
         </>
--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
-const Post = ({post}) => {
+const Post = ({post, userId}) => {
+
+  const postLiked = (event) => {
+    console.log("You like this: " + post.message + "\nUserId: + " + userId);
+  }
+
   return(
-    <article data-cy="post" key={ post._id }>{ post.message }</article>
+    <article data-cy="post" key={ post._id }>
+      <p>{ post.message }</p>
+      <button onClick={postLiked}>Like</button>
+    </article>
   )
 }
 
