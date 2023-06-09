9bd95221f124cfe0b6db14edd6620dfb255c74d4
Alice Wood <alicesophiewood@gmail.com>
2023-06-09 15:12:40 +0100

    backend tests are updated to include and test for username and name - frontend tests need reviewing

--- api/spec/controllers/posts.spec.js
+++ api/spec/controllers/posts.spec.js
@@ -10,7 +10,7 @@ let token;
 
 describe("/posts", () => {
   beforeAll( async () => {
-    const user = new User({email: "test@test.com", password: "12345678"});
+    const user = new User({email: "test@test.com", password: "12345678", username: "username1", name: "User Name"});
     await user.save();
 
     token = JWT.sign({
