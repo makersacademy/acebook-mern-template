9bb2b5ddf3204d57c548f47f7756e000bb34deef
CyrylG <cyrylgotkowicz@gmail.com>
2023-06-12 15:08:07 +0100

    Revert "Changed username required to false, allowing login/signup to work on front end"
    This reverts commit 40594712b58f195c4fa3dba0cedc0fe9f8e6b864.
    
    pls fix

--- api/models/user.js
+++ api/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
-  username: { type: String, required: false}, //// Defines the 'username' field as a string. username is not a string, Mongoose will throw error
+  username: { type: String, required: true}, //// Defines the 'username' field as a string. username is not a string, Mongoose will throw error
 });
 
 const User = mongoose.model("User", UserSchema);
