43b2ee57af4adbeae671063dc0a802e7a322f846
Matt Hammond <mjhammond94@gmail.com>
2023-06-12 15:59:47 +0100

    Complete inital like button functionality

--- frontend/src/components/LikeButton/LikeButton.js
+++ frontend/src/components/LikeButton/LikeButton.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
-
 const LikeButton = ({ onLike }) => {
-  return <button data-cy="likeButton" onClick={onLike}>I am the lord of the likes said he!!
+  return <button data-cy="likeButton" onClick={onLike}>Isenlike!
   </button>;
 };
-
 
 export default LikeButton;
--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,37 +1,20 @@
-// import React from 'react';
-
-// const Post = ({post}) => {
-//   return(
-//     <article data-cy="post" key={ post._id }>{ post.message }</article>
-//   )
-// }
-
-// export default Post;
-
-// post.comment. i need an area for someone to make a comment
-// this is also where we can add our likes
-
-// add text area
-// add submit button
-// setup handleSubmit
-
-// make a like component 
-
-// research making a folder called fetchers
-
 import React, { useState } from 'react';
 import LikeButton from '../LikeButton/LikeButton';
+import { handleSendingNewLike } from '../../fetchers';
 
 const Post = ({post}) => {
   const [likeCount, setLikeCount] = useState(post.likeCount);
+  const [token, setToken] = useState(window.localStorage.getItem("token"));
 
-  const handleLike = () => {
-    setLikeCount(likeCount + 1); // Update the like count when the button is clicked
-  };
+  const handleLike = async () => {
+    let newCount = likeCount + 1
+    await handleSendingNewLike(token, post, '/posts/add-like');
+    setLikeCount(newCount);
+  }
 
   return (
     <article data-cy="post" key={post._id}>
-      {post.message} {likeCount}
+      {post.message} - Likes: {likeCount}
       <LikeButton onLike={handleLike} />
     </article>
   );
