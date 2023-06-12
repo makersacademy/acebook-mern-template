d930a1f54023237ab221e18205fafba9db56ec5b
Barney Wilton <B.Wilton1993@gmail.com>
2023-06-12 20:52:58 +0100

    Set up userschema.pre

--- api/models/user.js
+++ api/models/user.js
@@ -6,6 +6,13 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: false}, //// Defines the 'username' field as a string. username is not a string, Mongoose will throw error
 });
 
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
