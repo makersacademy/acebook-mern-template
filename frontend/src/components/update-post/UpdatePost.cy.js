4baa5d0aeea176cbade5505cb1cf504174f64fc7
Barney Wilton <B.Wilton1993@gmail.com>
2023-06-13 15:48:22 +0100

    Created unit test for update post page on the front end

---
+++ frontend/src/components/update-post/UpdatePost.cy.js
@@ -0,0 +1,13 @@
+import UpdatePost from "./UpdatePost";
+const navigate = () => {}
+
+describe("Visiting the update page", () => {
+  it("should show the form to update a post", () => {
+    cy.mount(<UpdatePost navigate={navigate} />)
+
+    cy.get("form").should("be.visible")
+    
+    cy.get("#submit").should("be.visible")
+    .should('contain.text', "Update!")
+  })
+})
