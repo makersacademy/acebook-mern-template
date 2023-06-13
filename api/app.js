541c8a32b34774c6aeb6b751048b2f6b738db662
wimdavies <william.johnston.davies@gmail.com>
2023-06-13 13:12:30 +0100

    erased cloudinary dependencies on backend

--- api/app.js
+++ api/app.js
@@ -3,13 +3,6 @@ const express = require("express");
 const path = require("path");
 const logger = require("morgan");
 const JWT = require("jsonwebtoken");
-const cloudinary = require('cloudinary');
-
-cloudinary.config({ 
-  cloud_name: 'acebook', 
-  api_key: '991882213457318', 
-  api_secret: 'yfQu3l56kX1eMpHoUYSFNjxbtgU' 
-});
 
 const postsRouter = require("./routes/posts");
 const tokensRouter = require("./routes/tokens");
