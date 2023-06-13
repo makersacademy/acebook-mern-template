762ece95ad703fb9245edc413df249ebc53ba561
Barney Wilton <B.Wilton1993@gmail.com>
2023-06-13 15:38:34 +0100

    Homepage buttons integrated and fully tested, still need to run back-end tests to purge DB before frontend tests can pass

--- frontend/cypress/e2e/homepage_integration.cy.js
+++ frontend/cypress/e2e/homepage_integration.cy.js
@@ -10,4 +10,22 @@ describe("Visiting homepage", () => {
     cy.get("#signup-btn").click();
     cy.url().should("include", "/signup");
   })
+
+  it("redirects to posts if user is logged in", () => {
+    cy.visit("/signup");
+    cy.get("#email").type("test@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+    cy.url().should("include", "/login");
+    
+    cy.visit("/login");
+    cy.get("#email").type("test@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+    cy.url().should("include", "/posts");
+    
+    cy.visit("/");
+    cy.wait(1000);
+    cy.url().should("include", "/posts");
+  })
 })
--- frontend/src/components/homepage/Homepage.js
+++ frontend/src/components/homepage/Homepage.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Homepage = ({ navigate }) => {
+  const [token,] = useState(window.localStorage.getItem("token"));
+
+  useEffect(() => {
+    if(token) {
+      navigate('/posts')
+    }
+  }, [])
+
   const navigateToLogin = () => {
     navigate('/login')
   }
@@ -10,11 +18,12 @@ const Homepage = ({ navigate }) => {
   }
 
   return(
-    <>
-      <button type="button" id="login-btn" onClick={navigateToLogin}>Log in!</button>
-      <button type="button" id="signup-btn" onClick={navigateToSignup}>Sign up!</button>
-    </>
-  )
+      <>
+        <p>{token}</p>
+        <button type="button" id="login-btn" onClick={navigateToLogin}>Log in!</button>
+        <button type="button" id="signup-btn" onClick={navigateToSignup}>Sign up!</button>
+      </>
+    )
 }
 
 export default Homepage;
