61d02dbf776e82e712a39118017b4820acb6c632
Charlie Parker <cshjp@MASTUDENT-Charlie-Parker-C02G4FQVQ6L4.local>
2023-06-15 11:04:26 +0100

    changed something not very nice

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -35,7 +35,7 @@ const Navbar = ({navigate}) => {
           <img className="navImage" src='logo-airbook-removebg.png' alt="logo" />
         </Link>
       </div>
-      <button type="button" id="create-post-button" onClick={createPost}>Create Your Own Damn Post, Asshole</button>
+      <button type="button" id="create-post-button" onClick={createPost}>Create Post</button>
       <button type="button" id="logout" onClick={logout}>
         Logout
       </button>
