9f357d18eef442359641b23c41c2e0d8d6dfffb9
Cameron <cameronjamesbishop@googlemail.com>
2023-06-15 14:35:56 +0100

    Figuring out how to mock an image upload

--- frontend/cypress/e2e/signing_up.cy.js
+++ frontend/cypress/e2e/signing_up.cy.js
@@ -9,6 +9,17 @@ describe("Signing up", () => {
     cy.url().should("include", "/login");
   });
 
+  it("with valid credentials and a profile picture , redirects to '/login' and shows on '/posts'", () => {
+    // cy.visit("/signup");
+    // cy.get("#name").type("Person")
+    // cy.get("#email").type("someone@example.com");
+
+    // cy.get("#password").type("password");
+    // cy.get("#submit").click();
+
+    // cy.url().should("include", "/login");
+  });
+
   it("with missing password, redirects to '/signup'", () => {
     cy.visit("/signup");
     cy.get("#email").type("test@example.com");
--- frontend/cypress/e2e/using_feed_page.cy.js
+++ frontend/cypress/e2e/using_feed_page.cy.js
@@ -7,19 +7,21 @@ describe("Signing in", () => {
   
     it("creates a new post", () => {
       cy.get("#newPost").type("Let's chat!");
-      cy.get("#submit").click();
+      cy.get("#post-submit").click();
   
-      cy.contains('div.message', "Let's chat!").should('be.visible');
+      // cy.contains('div.message', "Let's chat!").should('be.visible');
+      cy.contains("Let's chat!");
     });
   
     it("throws an error if the user posts nothing", () => {
-      cy.get("#submit").click();
+      cy.get("#post-submit").click();
   
-      cy.contains('p.validation-error', "Please enter a post").should('be.visible');
+      cy.contains('p.validation-error', "Please enter a message to post").should('be.visible');
     })
+
     it("displays your like when you like a post", () => {
       cy.get("#newPost").type("Let's chat!");
-      cy.get("#submit").click();
+      cy.get("#post-submit").click();
       cy.get(".like-button:first").click();
   
       cy.contains('div.likes', "1").should('be.visible');
@@ -30,4 +32,53 @@ describe("Signing in", () => {
   
       cy.url().should("include", "/login")
     })
+
+    it("you can write a comment on a post", () => {
+
+    });
+
+    it("you can make a post with a picture", () => {
+      cy.get("#newPost").type("Let's chat!");
+      cy.get().includes("https://res.cloudinary.com/acebook/image/upload/v1686824029/tnckfiyuwxsqigfztbuv.webp")
+      cy.get("#post-submit").click();
+
+      cy.contains("Let's chat!");
+      cy.contains()
+    });
+
+    it("stops you from posting a picture if it doesn't have a caption", () => {
+
+    });
   });
+
+/*
+import cloudinaryUpload from 'cypress-cloudinary-upload';
+
+cloudinaryUpload.configure({
+  cloudName: 'YOUR_CLOUD_NAME',
+});
+
+describe('Image Upload', () => {
+  it('should mock image upload', () => {
+    cy.visit('/your-page-with-image-upload');
+
+    const mockResponse = {
+      public_id: 'your-mocked-public-id',
+      url: 'your-mocked-url',
+      // Add any other desired properties
+    };
+
+    cy.route('POST', 'https://api.cloudinary.com/v1_1/YOUR_CLOUD_NAME/image/upload', mockResponse).as('imageUpload');
+
+    cy.get('input[type="file"]').attachFile('test-image.jpg');
+
+    // Wait for the image upload to complete
+    cy.wait('@imageUpload').then((xhr) => {
+      // Access the mocked response
+      const response = xhr.responseBody;
+      
+      // Assertions or further test steps after the upload
+    });
+  });
+});
+*/
