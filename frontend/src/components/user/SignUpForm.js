cd768b9e56c8e456032430d05b9e6d36e44a45d1
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-07-03 16:40:06 +0100

    add username for signup page

--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -1,45 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const SignUpForm = ({ navigate }) => {
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    fetch( '/users', {
-      method: 'post',
+    fetch("/users", {
+      method: "post",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email: email, password: password })
-    })
-      .then(response => {
-        if(response.status === 201) {
-          navigate('/login')
-        } else {
-          navigate('/signup')
-        }
-      })
-  }
+      body: JSON.stringify({ email: email, password: password, username: username }),
+    }).then((response) => {
+      if (response.status === 201) {
+        navigate("/login");
+      } else {
+        navigate("/signup");
+      }
+    });
+  };
 
   const handleEmailChange = (event) => {
-    setEmail(event.target.value)
-  }
+    setEmail(event.target.value);
+  };
 
   const handlePasswordChange = (event) => {
-    setPassword(event.target.value)
-  }
+    setPassword(event.target.value);
+  };
 
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
 
-    return (
-      <form onSubmit={handleSubmit}>
-          <input placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
-          <input placeholder="Password" id="password" type='password' value={ password } onChange={handlePasswordChange} />
-        <input id='submit' type="submit" value="Submit" />
-      </form>
-    );
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Email"
+        id="email"
+        type="text"
+        value={email}
+        onChange={handleEmailChange}
+      />
+      <input
+        placeholder="Password"
+        id="password"
+        type="password"
+        value={password}
+        onChange={handlePasswordChange}
+      />
+      <input
+        placeholder="Username"
+        id="username"
+        type="text"
+        value={username}
+        onChange={handleUsernameChange}
+      />
+      <input id="submit" type="submit" value="Submit" />
+    </form>
+  );
+};
 
 export default SignUpForm;
