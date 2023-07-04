ac8d240fbc273e3fd0cc655da2314a352371adcd
jannisjr <joe_hannis@hotmail.com>
2023-07-04 11:44:24 +0100

    added userId and timestamp to posts

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -1,25 +1,37 @@
 const Post = require("../models/post");
 const TokenGenerator = require("../models/token_generator");
 
+const time = () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1; // Months are zero-based, so add 1
+  const day = now.getDate();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  return `${hours}:${minutes}:${seconds} ${day}-${month}-${year}`;
+};
+
 const PostsController = {
   Index: (req, res) => {
     Post.find(async (err, posts) => {
       if (err) {
         throw err;
       }
-      const token = await TokenGenerator.jsonwebtoken(req.user_id)
+      const token = await TokenGenerator.jsonwebtoken(req.user_id);
       res.status(200).json({ posts: posts, token: token });
     });
   },
   Create: (req, res) => {
-    const post = new Post(req.body);
+    const current_time = time();
+    const post = new Post(req.user_id, current_time, req.body);
     post.save(async (err) => {
       if (err) {
         throw err;
       }
 
-      const token = await TokenGenerator.jsonwebtoken(req.user_id)
-      res.status(201).json({ message: 'OK', token: token });
+      const token = await TokenGenerator.jsonwebtoken(req.user_id);
+      res.status(201).json({ message: "OK", token: token });
     });
   },
 };
--- api/models/post.js
+++ api/models/post.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-  message: String
+  userId: String,
+  time: Date,
+  message: String,
 });
 
 const Post = mongoose.model("Post", PostSchema);
