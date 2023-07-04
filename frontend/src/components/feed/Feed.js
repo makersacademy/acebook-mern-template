9fca77bdaec3bed9d8f95ff0b442392b8d953861
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-07-04 15:37:56 +0100

    add new post functionality, not tested

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -4,11 +4,10 @@ import Post from "../post/Post";
 const Feed = ({ navigate }) => {
   const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [newPost, setNewPost] = useState("");
 
   useEffect(() => {
-    console.log('Token before fetch:', token);
     if (token) {
-      
       fetch("/posts", {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -30,9 +29,49 @@ const Feed = ({ navigate }) => {
     navigate("/login"); // This is correctly placed
   };
 
+  const handleNewPostChange = (event) => {
+    setNewPost(event.target.value);
+  };
+
+  const createNewPost = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ message: newPost }),
+      });
+
+      if (response.status === 201) {
+        const data = await response.json();
+        setPosts([data.post, ...posts]);
+        setNewPost("");
+      } else {
+        console.log("Failed to create new post");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   if (token) {
     return (
       <>
+        <h2>Create New Post</h2>
+        <form onSubmit={createNewPost}>
+          <input
+            type="text"
+            placeholder="Enter your post"
+            value={newPost}
+            onChange={handleNewPostChange}
+          />
+          <button type="submit">Post</button>
+        </form>
+
         <h2>Posts</h2>
         <button onClick={logout}>Logout</button>
         <div id="feed" role="feed">
