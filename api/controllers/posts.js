9b3c6ea77d917c7b2cf5700cfd0940ac616ced30
jannisjr <joe_hannis@hotmail.com>
2023-07-04 13:32:39 +0100

    fix routes

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -8,8 +8,7 @@ const time = () => {
   const day = now.getDate();
   const hours = now.getHours();
   const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
-  return `${hours}:${minutes}:${seconds} ${day}-${month}-${year}`;
+  return `${hours}:${minutes} ${day}-${month}-${year}`;
 };
 
 const PostsController = {
--- api/routes/users.js
+++ api/routes/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const UsersController = require("../controllers/users");
 
-router.get("/", UsersController.Index);
 router.post("/", UsersController.Create);
 
 module.exports = router;
