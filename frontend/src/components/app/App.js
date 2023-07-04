bd5829ad1278195426391462cbc7947f912ae35a
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-07-04 11:53:26 +0100

    add basic html structure

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,22 +1,57 @@
-import './App.css';
-import LoginForm from '../auth/LoginForm'
-import SignUpForm from '../user/SignUpForm'
-import React, { useState } from 'react';
-import Feed from '../feed/Feed'
-import {
-  useNavigate,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import LoginForm from "../auth/LoginForm";
+import SignUpForm from "../user/SignUpForm";
+import Feed from "../feed/Feed";
+import Navbar from "../navbar/Navbar";
+import SearchBar from "../searchbar/SearchBar";
+import NotificationButton from "../notification/NotificationButton";
+import ProfileButton from "../profile/ProfileButton";
+import "./App.css";
 
 const App = () => {
-    return (
-        <Routes>
-          <Route path='/posts'  element={<Feed navigate={ useNavigate() }/>}/>
-          <Route path='/login'  element={<LoginForm  navigate={ useNavigate() }/>}/>
-          <Route path='/signup' element={<SignUpForm navigate={ useNavigate() }/>}/>
-        </Routes>
-    );
-}
+  const [showLoginForm, setShowLoginForm] = useState(false);
+  const [showSignUpForm, setShowSignUpForm] = useState(false);
+
+  const navigate = useNavigate();
+
+  const openLoginForm = () => setShowLoginForm(true);
+  const closeLoginForm = () => setShowLoginForm(false);
+
+  const openSignUpForm = () => setShowSignUpForm(true);
+  const closeSignUpForm = () => setShowSignUpForm(false);
+
+  return (
+    <div className="app-container">
+      <Navbar />
+
+      <div className="content">
+        <div className="top-bar">
+          <div className="top-left">
+            <SearchBar />
+          </div>
+          <div className="top-right">
+            <NotificationButton />
+            <button onClick={openLoginForm}>Log In</button>
+            <button onClick={openSignUpForm}>Sign Up</button>
+            <ProfileButton />
+          </div>
+        </div>
+
+        <div className="feed-container">
+          <Feed navigate={navigate} />
+        </div>
+      </div>
+
+      {showLoginForm && (
+        <LoginForm navigate={navigate} onClose={closeLoginForm} />
+      )}
+
+      {showSignUpForm && (
+        <SignUpForm navigate={navigate} onClose={closeSignUpForm} />
+      )}
+    </div>
+  );
+};
 
 export default App;
--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,49 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import Post from '../post/Post'
+import React, { useEffect, useState } from "react";
+import Post from "../post/Post";
 
 const Feed = ({ navigate }) => {
   const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token"));
 
   useEffect(() => {
-    if(token) {
+    if (token) {
       fetch("/posts", {
         headers: {
-          'Authorization': `Bearer ${token}`
-        }
+          Authorization: `Bearer ${token}`,
+        },
       })
-        .then(response => response.json())
-        .then(async data => {
-          window.localStorage.setItem("token", data.token)
-          setToken(window.localStorage.getItem("token"))
+        .then((response) => response.json())
+        .then(async (data) => {
+          window.localStorage.setItem("token", data.token);
+          setToken(window.localStorage.getItem("token"));
           setPosts(data.posts);
-        })
+        });
+    } else {
+      navigate("/login"); // Moved to useEffect
     }
-  }, [])
-    
+  }, [token, navigate]); // Added token and navigate as dependencies
 
   const logout = () => {
-    window.localStorage.removeItem("token")
-    navigate('/login')
+    window.localStorage.removeItem("token");
+    navigate("/login"); // This is correctly placed
+  };
+
+  if (token) {
+    return (
+      <>
+        <h2>Posts</h2>
+        <button onClick={logout}>Logout</button>
+        <div id="feed" role="feed">
+          {posts.map((post) => (
+            <Post post={post} key={post._id} />
+          ))}
+        </div>
+      </>
+    );
   }
-  
-    if(token) {
-      return(
-        <>
-          <h2>Posts</h2>
-            <button onClick={logout}>
-              Logout
-            </button>
-          <div id='feed' role="feed">
-              {posts.map(
-                (post) => ( <Post post={ post } key={ post._id } /> )
-              )}
-          </div>
-        </>
-      )
-    } else {
-      navigate('/login')
-    }
-}
+  // Removed the navigate('/login') call here as it's now handled in the useEffect
+};
 
 export default Feed;
