856b20b26a7f8b2d0d2483abf6ade822351fb613
Arbnor <arbnor-sfarca@hotmail.com>
2023-07-06 14:14:42 +0100

    some comments

--- frontend/cypress/e2e/making_a_post.cy.js
+++ frontend/cypress/e2e/making_a_post.cy.js
@@ -2,14 +2,14 @@ describe("making a post", () => {
 
     before(() => {
         cy.signup("user@email.com", "12345678")
-        cy.login("user@email.com", "12345678") // double check this
+        //cy.login("user@email.com", "12345678") // double check this
     })
 
     it(" creates a post and submits to the backend", () => {
-        cy.visit("/Post");
+        cy.visit("/Posts");
         cy.get("#message").type("somestring");
         cy.get("#submit").click();
     
-        cy.contains().should("somestring", "/Posts"); // need to figure what to put here!!!!!!!!
+        cy.contains("somestring").should("exist"); // need to figure what to put here!!!!!!!!
       });
 });
