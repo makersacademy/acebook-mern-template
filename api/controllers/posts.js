a821fc9939e934b3aa80f438421d0d089d518747
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-07-07 16:03:37 +0100

    fix

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -3,6 +3,7 @@ const Notification = require("../models/notification");
 const TokenGenerator = require("../models/token_generator");
 const User = require("../models/user");
 const fs = require("fs");
+const fs = require("fs");
 
 const PostsController = {
   Index: (req, res) => {
@@ -15,6 +16,8 @@ const PostsController = {
     });
   },
   Create: async (req, res) => {
+    console.log(req);
+
     const timeCalc = () => {
       const now = new Date();
       const year = now.getFullYear();
@@ -26,6 +29,7 @@ const PostsController = {
     };
 
     try {
+      console.log(req.body);
       const user = await User.findById(req.user_id);
 
       const username = user.username;
@@ -39,16 +43,19 @@ const PostsController = {
 
       if (req.file) {
         // Handle image upload if a file is provided
-        post.image.data = req.file.buffer; // You should access the buffer directly since you are using multer.memoryStorage()
+        post.image.data = fs.readFileSync(req.file.path);
         post.image.contentType = req.file.mimetype;
+        fs.unlinkSync(req.file.path); // Remove the temporary file after reading its data
       }
 
       await post.save();
+      console.log(req.body);
       const mentionedUsernames = req.body.message?.match(/@(\w+)/g) || [];
       for (let mentionedUsername of mentionedUsernames) {
         const mentionedUser = await User.findOne({
           username: mentionedUsername.replace("@", ""),
         });
+        console.log(req.body);
         if (mentionedUser) {
           const notification = new Notification({
             type: "mention",
@@ -60,6 +67,7 @@ const PostsController = {
           await notification.save();
         }
       }
+      console.log(req.body);
       console.log(req.body);
       const token = await TokenGenerator.jsonwebtoken(req.user_id);
       res.status(201).json({ message: "OK", token: token, post: post }); // Return the post
--- api/models/post.js
+++ api/models/post.js
@@ -8,7 +8,6 @@ const PostSchema = new mongoose.Schema({
     data: Buffer,
     contentType: String,
   },
-  authorId: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
 });
 
 const Post = mongoose.model("Post", PostSchema);
