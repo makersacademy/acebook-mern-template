adb3ec0f9203c8bccf4baa348d29a9d784b6d73e
Sean Peters <sean@snptrs.dev>
2023-07-07 10:41:53 +0100

    Add frontend for SinglePost

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const Post = ({post}) => {
-  return(
-    <article data-cy="post" key={ post._id }>{ post.message }</article>
-  )
-}
+const Post = ({ post }) => {
+  return (
+    <article data-cy="post" key={post._id}>
+      {post.message}
+      <br />
+      {/* Posted by: {post.user.email} */}
+      <br />
+      <a href={`/posts/${post._id}`}>View post</a>
+      <br />
+      <br />
+    </article>
+  );
+};
 
 export default Post;
