c9a5ed67f5b55c1fceff89554b61cdbb1b53b915
jannisjr <joe_hannis@hotmail.com>
2023-07-10 14:05:06 +0100

    all changes

--- api/controllers/likes.js
+++ api/controllers/likes.js
@@ -13,33 +13,48 @@ const LikesController = {
     });
   },
   Create: (req, res) => {
-    console.log(req.user_id);
-    const findUser = () => {
-      return User.findById(req.user_id)
-        .then((user) => {
-          return user.username;
-        })
+    const userId = req.user_id;
+    const postId = req.postId;
+    console.log(postId);
+
+    // Check if the user has already liked the post
+    Like.findOne({ username: userId, postId: postId }, (err, existingLike) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Internal Server Error" });
+      }
+
+      if (existingLike) {
+        // User has already liked the post
+        return res
+          .status(400)
+          .json({ message: "User has already liked the post" });
+      }
+
+      // User has not liked the post, proceed to create the new like
+      User.findById(userId)
+        .then((user) => user.username)
         .then((username) => {
           const like = new Like({
             username: username,
-            postId: req.body.postId,
+            postId: postId,
             likes: req.body.likes,
           });
           like.save(async (err) => {
             if (err) {
-              throw err;
+              console.error(err);
+              return res.status(500).json({ message: "Internal Server Error" });
             }
 
-            const token = await TokenGenerator.jsonwebtoken(req.user_id);
-            res.status(201).json({ message: "OK", token: token, likes: like }); // Return the comment
+            const token = await TokenGenerator.jsonwebtoken(userId);
+            res.status(201).json({ message: "OK", token: token, likes: like });
           });
         })
         .catch((err) => {
           console.error(err);
-          return err;
+          return res.status(500).json({ message: "Internal Server Error" });
         });
-    };
-    findUser();
+    });
   },
 };
 
