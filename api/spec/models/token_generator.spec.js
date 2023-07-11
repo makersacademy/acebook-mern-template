3f9a981f7040e7f35f04557208c2b8364b3008c4
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-07-11 14:24:20 +0100

    token test, doesnt work

--- api/spec/models/token_generator.spec.js
+++ api/spec/models/token_generator.spec.js
@@ -1,14 +1,23 @@
 const TokenGenerator = require("../../models/token_generator");
-const JWT = require("jsonwebtoken")
+const JWT = require("jsonwebtoken");
 
 describe("TokenGenerator", () => {
-  describe("jsonwebtoken", () => {
-    test("returns a token containing user_id that is valid for 10 minutes", () => {
-      const user_id = 1;
-      const token = TokenGenerator.jsonwebtoken(user_id);
-      const payload = JWT.decode(token, process.env.JWT_SECRET);
-      expect(payload.user_id).toEqual(user_id);
-      expect(payload.exp - payload.iat).toEqual(600);
-    })
-  })
-})
+  const FAKE_SECRET = "fake-secret";
+
+  test("Generates a JWT for a given user id", () => {
+    const userId = "12345";
+    const token = TokenGenerator.jsonwebtoken(userId, FAKE_SECRET);
+
+    expect(token).toBeDefined();
+
+    // Verify token and check the payload
+    const decodedToken = JWT.verify(token, FAKE_SECRET);
+    expect(decodedToken).toBeDefined();
+    expect(decodedToken.user_id).toBe(userId);
+
+    // Check that the token expires in roughly 10 minutes
+    const currentUnixTime = Math.floor(Date.now() / 1000);
+    const expectedExpiry = currentUnixTime + 100 * 60;
+    expect(Math.abs(decodedToken.exp - expectedExpiry)).toBeLessThanOrEqual(2);
+  });
+});
