418d13c94ec6ecdfa4ece057c2382087ecfec3aa
Ezana Adugna <eadugna3005@gmail.com>
2023-07-11 10:29:15 +0100

    Squashed commit of the following:
    commit 4e44f8e02c196044d497a54211cc7a5921ecf699
    Author: Ezana Adugna <eadugna3005@gmail.com>
    Date:   Mon Jul 10 16:00:37 2023 +0100
    
        finalised password encryptions
    
    commit 3c24c6b2f289b212606093902c1be921f42f71dc
    Author: Ezana Adugna <eadugna3005@gmail.com>
    Date:   Mon Jul 10 11:54:28 2023 +0100
    
        encrypted password

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const fs = require("fs");
 const path = require("path");
+const bcrypt = require("bcrypt");
 
 const defaultImagePath = path.join(
   __dirname,
@@ -11,18 +12,16 @@ const defaultImage = {
   data: fs.readFileSync(defaultImagePath),
   contentType: "image/png", // Adjust the content type based on your default image format
 };
+
+const saltRounds = 10; // Number of salt rounds for bcrypt hashing
 const JWT = require("jsonwebtoken");
 
 const UsersController = {
   Create: (req, res) => {
-    // Create a new user with default image
-    const user = new User({
-      email: req.body.email,
-      password: req.body.password,
-      username: req.body.username,
-      image: defaultImage,
-    });
-    user.save((err) => {
+    const { email, password, username } = req.body;
+
+    // Generate a hash for the password
+    bcrypt.hash(password, saltRounds, (err, hashedPassword) => {
       if (err) {
         return res.status(500).json({ message: "Internal server error" });
       }
