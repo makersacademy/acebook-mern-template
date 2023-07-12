5bc464239221be89698f54617a9db2ace0afb430
Sean Peters <sean@snptrs.dev>
2023-07-12 15:29:40 +0100

    Add GET route to retrieve posts by user

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -15,6 +15,18 @@ const PostsController = {
         res.status(200).json({ posts: posts, token: token });
       });
   },
+  PostsByUser: (req, res) => {
+    Post.find({ user: req.params.id })
+      .populate("user")
+      .sort({ created_at: -1 })
+      .exec(async (err, posts) => {
+        if (err) {
+          throw err;
+        }
+        const token = await TokenGenerator.jsonwebtoken(req.user_id);
+        res.status(200).json({ posts: posts, token: token });
+      });
+  },
   SinglePost: (req, res) => {
     Post.findById(req.params.id)
       .populate("user")
@@ -35,14 +47,14 @@ const PostsController = {
         photo: req.file ? req.file.filename : undefined,
       });
 
+      User.findById(req.user_id).exec(async (err, user) => {
+        post.user = user._id;
 
-    User.findById(req.user_id).exec(async (err, user) => {
-      post.user = user._id;
+        await post.save();
 
-      await post.save();
-
-      const token = await TokenGenerator.jsonwebtoken(req.user_id);
-      res.status(201).json({ message: "OK", token: token });});
+        const token = await TokenGenerator.jsonwebtoken(req.user_id);
+        res.status(201).json({ message: "OK", token: token });
+      });
     } catch (error) {
       next(error);
     }
