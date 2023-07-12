1d0774725a838a249292d18d9c1d834b286da402
Sean Peters <sean@snptrs.dev>
2023-07-12 15:36:46 +0100

    Add user's posts to their profile page

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -6,6 +6,7 @@ const Profile = ({ navigate }) => {
   const [user, setUser] = useState({
     email: "",
   });
+  const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token"));
   const params = useParams();
 
@@ -22,6 +23,19 @@ const Profile = ({ navigate }) => {
           setToken(window.localStorage.getItem("token"));
           setUser(data.user);
         });
+
+      fetch(`/posts/user/${params.id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => response.json())
+        .then(async (data) => {
+          window.localStorage.setItem("token", data.token);
+          setToken(window.localStorage.getItem("token"));
+          setPosts(data.posts);
+          // setRefreshFeed(false)
+        });
     }
   }, []);
 
@@ -30,6 +44,16 @@ const Profile = ({ navigate }) => {
       <>
         <h1>Profile page</h1>
         <h2>Email: {user.email}</h2>
+        <div id="feed" role="feed" className="posts">
+          {posts.map((post) => (
+            <Post
+              post={post}
+              key={post._id}
+              // onLike={handleLike}
+              // onUnlike={handleUnlike}
+            />
+          ))}
+        </div>
       </>
     );
   } else {
