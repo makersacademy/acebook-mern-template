3c91f91c1ef21bc4a58e39ab4adab480965853f1
Ezana Adugna <eadugna3005@gmail.com>
2023-07-13 15:44:00 +0100

    notification test

---
+++ frontend/cypress/e2e/notifications.cy.js
@@ -0,0 +1,18 @@
+describe("getting a notification", () => {
+  it("shows lastest notification", () => {
+    cy.visit("/");
+    cy.contains("Sign Up").click();
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#username").type("username");
+    cy.get("#submit").click();
+    cy.contains("Log In").click();
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+    cy.get(".create-post-container").type("@username");
+    cy.get("#post-submit").click();
+    cy.contains("Notifications").click();
+    cy.contains("You have been mentioned in a post by the user @username").should("be.visible");
+  });
+});
