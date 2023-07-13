3170fae785f040ebe255a5a82ff5f1684af8aebe
Ezana Adugna <eadugna3005@gmail.com>
2023-07-13 19:39:20 +0100

    making image post completed

--- frontend/cypress/e2e/making_a_post.cy.js
+++ frontend/cypress/e2e/making_a_post.cy.js
@@ -17,30 +17,21 @@ describe("Making a post", () => {
     cy.contains("This is a test post").should("be.visible");
   });
 
-  // it("login and make an image post", () => {
-  //   cy.visit("/");
-  //   cy.contains("Sign Up").click();
-  //   cy.get("#email").type("someone@example.com");
-  //   cy.get("#password").type("password");
-  //   cy.get("#username").type("username");
-  //   cy.get("#submit").click();
-  //   cy.contains("Log In").click();
-  //   cy.get("#email").type("someone@example.com");
-  //   cy.get("#password").type("password");
-  //   cy.get("#submit").click();
+  it("login and make an image post", () => {
+    cy.visit("/");
+    cy.contains("Sign Up").click();
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#username").type("username");
+    cy.get("#submit").click();
+    cy.contains("Log In").click();
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
   
-  //   cy.fixture('DND-2000-Xilus.png').then(fileContent => {
-  //     cy.get('input[type="file"]').attachFile({
-  //       fileContent,
-  //       fileName: 'DND-2000-Xilus.png',
-  //       mimeType: 'image/png'
-  //     });
-  //   });
-  
-  //   cy.get("#post-submit").click();
-  //   cy.contains('DND-2000-Xilus.png').should("exist");
-  // });
+    cy.get("input[type='file']").attachFile("../fixtures/VOID");
+    cy.get("#post-submit").click();
+    cy.get(".post-image").should("be.visible");
+  });
   
 });
-
-
