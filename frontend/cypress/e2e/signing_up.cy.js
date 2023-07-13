1bfae9f39a499d710dee56c5d889da5eb3d0a737
Ezana Adugna <eadugna3005@gmail.com>
2023-07-13 12:30:13 +0100

    one more change to the sign up cypress test

--- frontend/cypress/e2e/signing_up.cy.js
+++ frontend/cypress/e2e/signing_up.cy.js
@@ -10,13 +10,13 @@ describe("Signing up", () => {
     cy.url().should("include", "/");
   });
 
-  it("with missing password, redirects to '/signup'", () => {
+  it("with missing password, submit button remains visible", () => {
     cy.visit("/");
     cy.contains("Sign Up").click();
     cy.get("#email").type("someone@example.com");
     cy.get("#submit").click();
     
-    cy.get("#submit").should('be.visible');
+    cy.get("#submit").should('be.visible'); //passes 
   });
 
   it("with missing email, redirects to '/signup'", () => {
