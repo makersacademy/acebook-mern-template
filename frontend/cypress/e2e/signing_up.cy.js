a2af10c8cd6fe90123c4805c0a87a4a12706b318
fatih-celebi <celebifatih.c1@gmail.com>
2023-08-03 16:28:06 +0100

    added username to signup page

--- frontend/cypress/e2e/signing_up.cy.js
+++ frontend/cypress/e2e/signing_up.cy.js
@@ -1,26 +1,36 @@
 describe("Signing up", () => {
+
   it("with valid credentials, redirects to '/login'", () => {
     cy.visit("/signup");
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("password");
+    cy.get("#username").type("usernamename");
     cy.get("#submit").click();
-
     cy.url().should("include", "/login");
+    cy.screenshot()
   });
 
   it("with missing password, redirects to '/signup'", () => {
     cy.visit("/signup");
     cy.get("#email").type("someone@example.com");
+    cy.get("#username").type("usernamename")
     cy.get("#submit").click();
-
     cy.url().should("include", "/signup");
   });
 
   it("with missing email, redirects to '/signup'", () => {
     cy.visit("/signup");
     cy.get("#password").type("password");
+    cy.get("#username").type("usernamename")
     cy.get("#submit").click();
+    cy.url().should("include", "/signup");
+  });
 
+  it("with missing username, redirects to '/signup'", () => {
+    cy.visit("/signup");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
     cy.url().should("include", "/signup");
   });
 });
--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -4,6 +4,7 @@ const SignUpForm = ({ navigate }) => {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -13,7 +14,7 @@ const SignUpForm = ({ navigate }) => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ email: email, password: password })
+      body: JSON.stringify({ email: email, password: password, username: username })
     })
       .then(response => {
         if(response.status === 201) {
@@ -32,11 +33,18 @@ const SignUpForm = ({ navigate }) => {
     setPassword(event.target.value)
   }
 
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  
+
 
     return (
       <form onSubmit={handleSubmit}>
           <input placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
           <input placeholder="Password" id="password" type='password' value={ password } onChange={handlePasswordChange} />
+          <input placeholder="Username" id="username" type='text' value={ username } onChange={handleUsernameChange} />
         <input id='submit' type="submit" value="Submit" />
       </form>
     );
