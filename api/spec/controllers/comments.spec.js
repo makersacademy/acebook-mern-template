0f7fb8e4b92c994f0467123c17e4ec3df1534f0f
Fahim Islam <fahim.islam@outlook.com>
2023-08-03 12:14:44 +0100

    Added comments to Posts

---
+++ api/spec/controllers/comments.spec.js
@@ -0,0 +1,106 @@
+
+const app = require("../../app");
+const request = require("supertest");
+require("../mongodb_helper");
+const Post = require('../../models/post');
+const User = require('../../models/user');
+const Comment = require('../../models/comment')
+const JWT = require("jsonwebtoken");
+const { post } = require("superagent");
+const secret = process.env.JWT_SECRET;
+
+let token;
+let post_id;
+let user_id;
+
+describe("/comments", () => {
+  beforeAll( async () => {
+    const user = new User({
+      email: "test31@test.com", 
+      username: "test31username", 
+      password: "1234567890"
+		});
+		user_id = user.id;
+    const post = new Post({
+    	message: "testing comments"
+    })
+		await user.save();
+		post_id = post._id;
+		await post.save();
+		
+    token = JWT.sign({
+      user_id: user.id,
+      // Backdate this token of 5 minutes
+      iat: Math.floor(Date.now() / 1000) - (5 * 60),
+      // Set the JWT token to expire in 10 minutes
+      exp: Math.floor(Date.now() / 1000) + (10 * 60)
+    }, secret);
+  });
+
+  beforeEach( async () => {
+    await Comment.deleteMany({});
+  })
+
+  afterAll( async () => {
+    await User.deleteMany({});
+    await Post.deleteMany({});
+    await Comment.deleteMany({});
+  })
+
+  describe("Comment, when token is present", () => {
+      test("responds with a 201", async () => {
+      let response = await request(app)
+        .post("/comments")
+        .set("Authorization", `Bearer ${token}`)
+        .send({post: post_id, user: user_id, comment: "testing comments", token: token });
+				expect(response.status).toEqual(201);
+
+			});
+		
+			test("creates a new comment", async () => {
+				await request(app)
+					.post("/comments")
+					.set("Authorization", `Bearer ${token}`)
+					.send({post: post_id, user: user_id, comment: "hello world", token: token  });
+				let comments = await Comment.find();
+				expect(comments.length).toEqual(1);
+				expect(comments[0].comment).toEqual("hello world");
+			});
+			
+		
+			test("creates a comment linked to the user", async () => {
+				await request(app)
+					.post("/comments")
+					.set('Authorization', `Bearer ${token}`)
+					.send({post: post_id, user: user_id, comment: "hello world", token: token  })
+				const comments = await Comment.findOne();
+				// Assert comment is linked to user
+				expect(JSON.stringify(user_id)).toEqual(JSON.stringify(comments.user));
+			});
+		
+			test("creates a comment linked to the post", async () => {
+				await request(app)
+					.post("/comments")
+					.set('Authorization', `Bearer ${token}`)
+					.send({post: post_id, user: user_id, comment: "hello world", token: token  })
+				const comments = await Comment.findOne();
+				// Assert comment is linked to user
+				expect(JSON.stringify(post_id)).toEqual(JSON.stringify(comments.post));
+			});
+		
+			test("returns every comment in to ", async () => {
+				let post1 = new Post({message: "howdy!"});
+				let post2 = new Post({message: "hola!"});
+				await post1.save();
+				await post2.save();
+				let response = await request(app)
+					.get("/posts")
+					.set("Authorization", `Bearer ${token}`)
+					.send({token: token});
+				let messages = response.body.posts.map((post) => ( post.message ));
+				expect(messages).toEqual(["howdy!", "hola!"]);
+			})
+  
+ 
+  })
+});
