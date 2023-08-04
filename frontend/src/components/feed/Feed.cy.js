068b1d3ba9eeb074d3e3753efb7ecd44955e96f0
Asha Ali <ashaali@Ashas-MacBook-Air.local>
2023-08-04 13:38:55 +0100

    some changes

--- frontend/src/components/feed/Feed.cy.js
+++ frontend/src/components/feed/Feed.cy.js
@@ -15,13 +15,41 @@ describe("Feed", () => {
         })
       }
     ).as("getPosts")
-
     cy.mount(<Feed navigate={navigate}/>)
-    
     cy.wait("@getPosts").then(() =>{
       cy.get('[data-cy="post"]')
       .should('contain.text', "Hello, world")
       .and('contain.text', "Hello again, world")
     })
   })
+it("it displays a list of all the post links", () => {
+  window.localStorage.setItem("token", "fakeToken")
+  cy.intercept('GET', '/posts', (req) => {
+    req.reply({
+      statusCode: 200,
+      body: { posts: [
+        {_id: 1, message: "Hello, world"}
+        // {_id: 2, message: "Hello again, world"}
+      ] }
+    })
+  }
+).as("getPosts")
+
+cy.mount(<Feed navigate={navigate}/>)
+
+cy.wait("@getPosts").then(() =>{
+  // get the message
+  // click the first message
+  // cy.get('[data-testid="assertions-link"]')
+  cy.get('[data-cy="post"]')
+  cy.screenshot()
+  //should have a link contain the id of the message
+  .should('contain.text', "Hello, world")
+  
+      .and('have.attr', 'href')
+      .and('include', '/1')
 })
+})
+})
+
+
--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -36,10 +36,19 @@ const Feed = ({ navigate }) => {
             </button>
           <div id='feed' role="feed">
               {posts.map(
-                (post) => ( <Post post={ post } key={ post._id } /> )
+                (post) => ( <a href= {`#${post._id}`} key={post._id}><Post post={ post } key={ post._id } /></a> )
               )}
           </div>
         </>
+//<div id='feed' role="feed">
+// {posts.map((post) => (
+//   <a href={`#${post._id}`} key={post._id}>
+//       <Post post={post} key={post._id} />
+//   </a>
+// ))}
+// </div>
+
+
       )
     } else {
       navigate('/login')
