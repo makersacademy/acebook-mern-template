fa493fa32c44b65d3f5bed5e2ebd86822101a346
Sam <sam.ryan101@gmail.com>
2023-08-04 14:04:21 +0100

    tests passing, added test to go back/forth and click both times

--- frontend/src/components/feed/Feed.cy.js
+++ frontend/src/components/feed/Feed.cy.js
@@ -22,14 +22,15 @@ describe("Feed", () => {
       .and('contain.text', "Hello again, world")
     })
   })
+
 it("it displays a list of all the post links", () => {
   window.localStorage.setItem("token", "fakeToken")
   cy.intercept('GET', '/posts', (req) => {
     req.reply({
       statusCode: 200,
       body: { posts: [
-        {_id: 1, message: "Hello, world"}
-        // {_id: 2, message: "Hello again, world"}
+        {_id: 1, message: "Hello, world"},
+        {_id: 2, message: "Hello again, world"}
       ] }
     })
   }
@@ -38,16 +39,12 @@ it("it displays a list of all the post links", () => {
 cy.mount(<Feed navigate={navigate}/>)
 
 cy.wait("@getPosts").then(() =>{
-  // get the message
-  // click the first message
-  // cy.get('[data-testid="assertions-link"]')
   cy.get('[data-cy="post"]')
-  cy.screenshot()
-  //should have a link contain the id of the message
-  .should('contain.text', "Hello, world")
-  
-      .and('have.attr', 'href')
-      .and('include', '/1')
+  cy.contains('Hello, world').click()
+  cy.url().should('include', '#1')
+  cy.go('back')
+  cy.contains('Hello again, world').click()
+  cy.url().should('include', '#2')
 })
 })
 })
