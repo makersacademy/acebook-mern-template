ad4e4280280963809f96b1539966b9a0b5d5f277
Sam <sam.ryan101@gmail.com>
2023-08-04 16:53:08 +0100

    added an edit post form component

---
+++ frontend/src/components/post/editPostForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const EditPostForm = ({post}) => {
+  return(
+
+    //form containing post message and submit button
+    <form data-cy="editPostForm">
+        <label>
+            Message:
+            <input  type="text" name="message"/>
+        </label>
+        <input type="submit" value="Submit"/>
+    </form>
+
+
+
+  )
+}
+
+export default EditPostForm;
