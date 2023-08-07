d51abaa006915415659f05598da72c33b88cb310
Usama Amir <117232010+usamaamir92@users.noreply.github.com>
2023-08-07 10:40:59 +0100

    Update Feed.js
    removing comments

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -40,13 +40,6 @@ const Feed = ({ navigate }) => {
               )}
           </div>
         </>
-//<div id='feed' role="feed">
-// {posts.map((post) => (
-//   <a href={`#${post._id}`} key={post._id}>
-//       <Post post={post} key={post._id} />
-//   </a>
-// ))}
-// </div>
 
 
       )
