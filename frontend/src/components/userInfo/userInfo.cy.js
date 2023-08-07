7cd6f3ada43f3f3dec996518a68e3236d0fcbb4a
Amina <amina.begum.01@gmail.com>
2023-08-07 17:56:03 +0100

    Second test added to check redirect - not passing yet

--- frontend/src/components/userInfo/userInfo.cy.js
+++ frontend/src/components/userInfo/userInfo.cy.js
@@ -6,7 +6,6 @@ describe("User information page", () => {
 
 	it("calls the users/userID endpoint and lists the users info", () => {
     window.localStorage.setItem("token", "fakeToken")
-
     cy.intercept('GET', '/users/1', { email: "testEmail", username: "testUserName", token: 'fakeToken' }).as('getUserInfo');
 
     cy.mount(
@@ -23,4 +22,22 @@ describe("User information page", () => {
     cy.get('[data-cy=username]').should('contain.text', "Username: testUserName");
 
 	})
+
+  it('Throws an error if user is not authorized, or no user exists', () => {
+    window.localStorage.setItem("token", "fakeToken")
+    cy.intercept('GET', '/users/1', { statusCode: 400, token: 'fakeToken' }).as('getUserInfo');
+
+    cy.mount(
+      <MemoryRouter initialEntries={['/users/1']}>  
+        <Routes>
+          <Route path="/users/:userId" element={<UserInfo />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    cy.wait("@getUserInfo")
+    cy.url().should('include', 'login')
+
+  })
+
 })
--- frontend/src/components/userInfo/userInfo.js
+++ frontend/src/components/userInfo/userInfo.js
@@ -1,42 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-const UserInfo = () => {
+const UserInfo = ({navigate}) => {
     const [userInfo, setUserInfo] = useState([])
     const [token, setToken] = useState(window.localStorage.getItem("token"));
     const { userId } = useParams();
-    console.log(userId)
 
-    useEffect(() => {
+    useEffect( () => {
         if(token) {
             fetch(`/users/${userId}`, {
-              headers: {
-                  'Authorization': `Bearer ${token}`
-              }
+              headers: {'Authorization': `Bearer ${token}`}
             })
-              .then(response => response.json())
-              .then(async data => {
-                window.localStorage.setItem("token", data.token)
-                setToken(window.localStorage.getItem("token"))
+            .then (response => response.json())
+            .then(data => {
+                window.localStorage.setItem("token", data.token);
+                setToken(window.localStorage.getItem("token"));
                 setUserInfo(data);
             })
-        }
-    }, [])
+        }    
+    }, [])     
 
-    // add an else block to redirect to somewhere 
-    // or show message if no authorisation
-    // redirect to posts maybe
-
-        return(
-            <>
-            <h2>Your Account Information</h2>
-            <div>
-                <p data-cy="username">Username: {userInfo.username}</p>
-                <p data-cy="email">Email: {userInfo.email}</p>
-            </div>
-            {/* add redirect link to post page  */}
-            </>
+    if (token){
+    return(
+        <>
+        <h2>Your Account Information</h2>
+        <div>
+            <p data-cy="username">Username: {userInfo.username}</p>
+            <p data-cy="email">Email: {userInfo.email}</p>
+        </div>
+        <a href="/posts">Go back to my feed </a>
+        </>
         ) 
+    } else {
+        navigate('/login')
+    }
 }
 
 export default UserInfo;
