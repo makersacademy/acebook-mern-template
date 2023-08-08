a693fd7b4814913a5106efeceaceb002c46a8754
Usama <usama_amir92@hotmail.com>
2023-08-08 12:20:22 +0100

    removed the token for this pagee

--- frontend/src/components/post/post_id.cy.js
+++ frontend/src/components/post/post_id.cy.js
@@ -5,65 +5,21 @@ import {
     Route,
 } from "react-router-dom";
 
-
-// describe("My app", () => {
-//     it("renders correctly", () => {
-//       let renderer = create(
-//         <MemoryRouter initialEntries={["/users/mjackson"]}>
-//           <Routes>
-//             <Route path="users" element={<Users />}>
-//               <Route path=":id" element={<UserProfile />} />
-//             </Route>
-//           </Routes>
-//         </MemoryRouter>
-//       );
-  
-//       expect(renderer.toJSON()).toMatchSnapshot();
-//     });
-//   });
-
-
-
-
-
 describe("info from post id", () => {
     it('renders correctly', () => {
-        
-        window.localStorage.setItem("token", "fakeToken")
-        cy.intercept('GET', '/posts/64cbba05e72b5857555a24a2', {token: 'fakeToken',
+        // window.localStorage.setItem("token", "fakeToken")
+        cy.intercept('GET', '/posts/64cbba05e72b5857555a24a2', {
                 statusCode: 200,
                 body: {message: 'Hello world'}
             }).as("getPost")
         cy.mount(
             <MemoryRouter initialEntries={['/posts/64cbba05e72b5857555a24a2']}>  
               <Routes>
-                <Route path="/posts/:PostId" element={<PostId />} />
+                <Route path="/posts/:id" element={<PostId />} />
               </Routes>
             </MemoryRouter>
           );
-        cy.screenshot()
         cy.wait("@getPost")
         cy.get('[data-cy=post]').should('contain.text', 'Hello world')
         })
     })
-
-    // describe("User information page", () => {
-
-    //     it("calls the users/userID endpoint and lists the users info", () => {
-    //     window.localStorage.setItem("token", "fakeToken")
-    //     cy.intercept('GET', '/users/1', { email: "testEmail", username: "testUserName", token: 'fakeToken' }).as('getUserInfo');
-    
-    //     cy.mount(
-    //       <MemoryRouter initialEntries={['/users/1']}>  
-    //         <Routes>
-    //           <Route path="/users/:userId" element={<UserInfo />} />
-    //         </Routes>
-    //       </MemoryRouter>
-    //     );
-        
-    //     cy.wait('@getUserInfo')
-    
-    //     cy.get('[data-cy=email]').should("contain.text", "Email: testEmail");
-    //     cy.get('[data-cy=username]').should('contain.text', "Username: testUserName");
-    
-    //     })
--- frontend/src/components/post/post_id.js
+++ frontend/src/components/post/post_id.js
@@ -11,45 +11,23 @@ import { useParams } from "react-router-dom";
 
 const PostId = () => {
     const [post, setPost] = useState([]);
-    const [token, setToken] = useState(window.localStorage.getItem("token"));
+    // const [token, setToken] = useState(window.localStorage.getItem("token"));
     const { id } = useParams();
 
-
-
     useEffect(() => {
-        // if (token) {
-            fetch(`/posts/${id}`, {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            })
+            fetch(`/posts/${id}`)
                 .then(response => response.json())
                 .then(async data => {
-                    window.localStorage.setItem("token", data.token)
-                    setToken(window.localStorage.getItem("token"))
                     setPost(data);
                 })
-        // }
-    }, [id, token]);
-
-
-    // if(token) {
+            
+        }, [id]);
+        
         return(
-          <>
             <div >
                 <p data-cy='post'>{post.message}</p>
-                
             </div>
-          </>
         )
-    // }
-    // component for editing post
-    // <div>
-    //         <p data-cy="username">Username: {userInfo.username}</p>
-    //         <p data-cy="email">Email: {userInfo.email}</p>
-    //     </div>
-
-    // const editPostForm 
 }
 
 export default PostId;
