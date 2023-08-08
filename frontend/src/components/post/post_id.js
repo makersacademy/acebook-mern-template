71bbe569556c696543e3812a927a83b8ed3d403a
Asha-Ali <133216061+Asha-Ali@users.noreply.github.com>
2023-08-08 14:40:45 +0100

    Update post_id.js
    removed comments

--- frontend/src/components/post/post_id.js
+++ frontend/src/components/post/post_id.js
@@ -2,13 +2,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 
-// component for displaying post
-
-// const Feed = ({ navigate }) => {
-//     const [posts, setPosts] = useState([]);
-//     const [token, setToken] = useState(window.localStorage.getItem("token"));
-// const [editPostForm, ]
-
 const PostId = () => {
     const [post, setPost] = useState([]);
     // const [token, setToken] = useState(window.localStorage.getItem("token"));
