b9b551a1a8cb138f705c54f0db4df2b0a4f3b2ee
Sam <sam.ryan101@gmail.com>
2023-08-09 18:11:35 +0100

    app functions as intended but the test isnt working yet

--- frontend/src/components/post/post_id.cy.js
+++ frontend/src/components/post/post_id.cy.js
@@ -1,25 +1,64 @@
-import PostId from "./post_id"
-import {
-    MemoryRouter,
-    Routes,
-    Route,
-} from "react-router-dom";
+import PostId from "./post_id";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
 
 describe("info from post id", () => {
-    it('renders correctly', () => {
-        cy.intercept('GET', '/posts/64cbba05e72b5857555a24a2', {
-                statusCode: 200,
-                body: {message: 'Hello world', author: 'person1'}
-            }).as("getPost")
+    it("renders correctly", () => {
+        cy.intercept("GET", "/posts/64cbba05e72b5857555a24a2", {
+            statusCode: 200,
+            body: { message: "Hello world", author: "person1" },
+        }).as("getPost");
         cy.mount(
-            <MemoryRouter initialEntries={['/posts/64cbba05e72b5857555a24a2']}>  
-              <Routes>
-                <Route path="/posts/:id" element={<PostId />} />
-              </Routes>
+            <MemoryRouter initialEntries={["/posts/64cbba05e72b5857555a24a2"]}>
+                <Routes>
+                    <Route path="/posts/:id" element={<PostId />} />
+                </Routes>
             </MemoryRouter>
-          );
-        cy.wait("@getPost")
-        cy.get('[data-cy=post]').should('contain.text', 'Hello world')
-        cy.get('[data-cy=author]').should('contain.text', 'person1')
-        })
-    })
+        );
+        cy.wait("@getPost");
+        cy.get("[data-cy=post]").should("contain.text", "Hello world");
+        cy.get("[data-cy=author]").should("contain.text", "person1");
+    });
+    it("renders an edit form", () => {
+        cy.intercept("GET", "/posts/64cbba05e72b5857555a24a2", {
+            statusCode: 200,
+            body: { message: "Hello world", author: "person1" },
+        }).as("getPost");
+        cy.mount(
+            <MemoryRouter initialEntries={["/posts/64cbba05e72b5857555a24a2"]}>
+                <Routes>
+                    <Route path="/posts/:id" element={<PostId />} />
+                </Routes>
+            </MemoryRouter>
+        );
+        cy.wait("@getPost");
+        cy.get("[data-cy=editPostForm]").should("exist");
+    });
+    it("renders an edit form and updates the post", () => {
+        cy.intercept("GET", "/posts/64cbba05e72b5857555a24a2", {
+            statusCode: 200,
+            body: { message: "Hello world", author: "person1" },
+        }).as("getPost");
+
+        cy.mount(
+            <MemoryRouter initialEntries={["/posts/64cbba05e72b5857555a24a2"]}>
+                <Routes>
+                    <Route path="/posts/:id" element={<PostId />} />
+                </Routes>
+            </MemoryRouter>
+        );
+        cy.wait("@getPost");
+        cy.get("[data-cy=editPost]").type("edited post");
+        cy.get("[data-cy=editPostForm]").submit();
+
+        cy.intercept("PUT", "/posts/64cbba05e72b5857555a24a2", {
+            statusCode: 200,
+            body: {
+                message: "OK",
+                token: "token",
+                post: { message: "edited post", author: "person1" },
+            },
+        }).as("editPost");
+        cy.wait("@editPost");
+        cy.get("[data-cy=post]").should("contain.text", "edited post");
+    });
+});
--- frontend/src/components/post/post_id.js
+++ frontend/src/components/post/post_id.js
@@ -1,37 +1,76 @@
-
-import React, { useEffect, useState } from 'react';
+import NavigationBar from "../navigation/Navigation";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+// import EditPostForm from "./editPostForm"; // don't have this in at the moment, we could add it back later to make the code more DRY
 
 const PostId = () => {
-    const [post, setPost] = useState({});
+    const [post, setPost] = useState({ message: "", author: "" }); // Initial state with empty values
     const [token, setToken] = useState(window.localStorage.getItem("token"));
+    const [editPostValue, setEditPostValue] = useState(""); // State for form input value
 
     const { id } = useParams();
 
     useEffect(() => {
-            fetch(`/posts/${id}`,{
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            })
-                .then(response => response.json())
-                .then(async data => {
-                    window.localStorage.setItem("token", data.token)
-                    setToken(window.localStorage.getItem("token"))
-                    setPost(
-                        {
-                        message: data.message,
-                        author: data.author
-                    });
+        fetch(`/posts/${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((response) => response.json())
+            .then(async (data) => {
+                window.localStorage.setItem("token", data.token);
+                setToken(window.localStorage.getItem("token"));
+                setPost({
+                    message: data.message,
+                    author: data.author,
                 });
-            }, {})
-    
-        return(
-            <div >
-                <p data-cy='post'>{post.message}</p>
-                <p data-cy='author'>{post.author}</p>
+            });
+    }, {});
+
+    const editPost = () => {
+        fetch(`/posts/${id}`, {
+            method: "PUT",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                message: editPostValue,
+            }),
+        })
+            .then((response) => response.json())
+            .then(async (data) => {
+                window.localStorage.setItem("token", data.token);
+                setToken(window.localStorage.getItem("token"));
+                setPost({ ...post, message: data.message }); // spread operator to update the message but keep the author
+                setEditPostValue("");
+            });
+    };
+
+    return (
+        <>
+            <NavigationBar />
+            <div>
+                <p data-cy="post">{post.message}</p>
+                <p data-cy="author">{post.author}</p>
             </div>
-        )
-}
+            <div>
+                <form data-cy="editPostForm" onSubmit={editPost}>
+                    <label>
+                        Message:
+                        <input
+                            data-cy="editPost"
+                            type="text"
+                            name="message"
+                            value={editPostValue} // Use the state value here
+                            onChange={(e) => setEditPostValue(e.target.value)}
+                        />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        </>
+    );
+};
 
 export default PostId;
