8da8b2e0e7f3b1ce0a0fccbe1c39e78ae7c8e35f
Usama <usama_amir92@hotmail.com>
2023-08-10 14:02:54 +0100

    missing file committed

--- frontend/src/components/post/post_id.js
+++ frontend/src/components/post/post_id.js
@@ -1,10 +1,13 @@
 
 import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
+import NavigationBar from '../navigation/Navigation';
 
-const PostId = () => {
+
+const PostId = ({}) => {
     const [post, setPost] = useState({});
     const [token, setToken] = useState(window.localStorage.getItem("token"));
+    const [isLiked, setIsLiked] = useState(false);
 
     const { id } = useParams();
 
@@ -21,17 +24,51 @@ const PostId = () => {
                     setPost(
                         {
                         message: data.message,
-                        author: data.author
+                        author: data.author,
+                        likedBy: data.likes,
+                        likes: data.likes.length,
+                        logged_in_user: data.logged_in_user
                     });
                 });
-            }, {})
+            }, []);
+
+
+
+    const handleLike = async () => {
+        const response = await fetch(`/posts/${id}/likeUnlike`,{
+            method: 'post',
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+        if (response.status === 201) {
+        const newLikesCount = post.likes + 1;
+        setPost(prevPost => ({ ...prevPost, likes: newLikesCount }));
+        setIsLiked(true)
+        }
+        
+        if (response.status === 200) {
+        const newLikesCount = post.likes - 1; 
+        setPost(prevPost => ({ ...prevPost, likes: newLikesCount }));
+        setIsLiked(false)
+        }
+    }
     
-        return(
+    return(
             <div >
+                <NavigationBar />
                 <p data-cy='post'>{post.message}</p>
-                <p data-cy='author'>{post.author}</p>
+                <p data-cy='author'>Posted by: {post.author}</p>
+                <p data-cy='likes'>{post.likes} Likes</p>
+                <div>
+                    <button id='likeUnlike' onClick={handleLike}>
+                        {isLiked ? '🧡' : '🤍'}
+                    </button>
+                    {isLiked ? <p>Unlike this post</p> : <p>Like this post</p>}
+                </div>
             </div>
-        )
-}
+        ); 
+    }
 
 export default PostId;
