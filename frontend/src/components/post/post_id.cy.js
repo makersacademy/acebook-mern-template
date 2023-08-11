2b81609a51ac279c3ddf70f3a4bf2276b273990c
Sam <sam.ryan101@gmail.com>
2023-08-11 10:16:21 +0100

    added testing for the comments feature

--- frontend/src/components/post/post_id.cy.js
+++ frontend/src/components/post/post_id.cy.js
@@ -1,25 +1,26 @@
-import PostId from "./post_id"
-import {
-    MemoryRouter,
-    Routes,
-    Route,
-} from "react-router-dom";
+import PostId from "./post_id";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
 
 describe("info from post id", () => {
-    it('renders correctly', () => {
-        cy.intercept('GET', '/posts/64cbba05e72b5857555a24a2', {
-                statusCode: 200,
-                body: {message: 'Hello world', author: 'person1'}
-            }).as("getPost")
+    it("renders correctly", () => {
+        cy.intercept("GET", "/posts/64cbba05e72b5857555a24a2", {
+            statusCode: 200,
+            body: {
+                message: "Hello world",
+                author: "person1",
+                comments: [{ user_id: "person2", comment: "comment1" }],
+            },
+        }).as("getPost");
         cy.mount(
-            <MemoryRouter initialEntries={['/posts/64cbba05e72b5857555a24a2']}>  
-              <Routes>
-                <Route path="/posts/:id" element={<PostId />} />
-              </Routes>
+            <MemoryRouter initialEntries={["/posts/64cbba05e72b5857555a24a2"]}>
+                <Routes>
+                    <Route path="/posts/:id" element={<PostId />} />
+                </Routes>
             </MemoryRouter>
-          );
-        cy.wait("@getPost")
-        cy.get('[data-cy=post]').should('contain.text', 'Hello world')
-        cy.get('[data-cy=author]').should('contain.text', 'person1')
-        })
-    })
+        );
+        cy.wait("@getPost");
+        cy.get("[data-cy=post]").should("contain.text", "Hello world");
+        cy.get("[data-cy=author]").should("contain.text", "person1");
+        cy.get("[data-cy=comments]").should("contain.text", "comment1");
+    });
+});
--- frontend/src/components/post/post_id.js
+++ frontend/src/components/post/post_id.js
@@ -2,91 +2,92 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const PostId = () => {
-  const [post, setPost] = useState({});
-  const [token, setToken] = useState(window.localStorage.getItem("token"));
-  const [comment, setComment] = useState("");
-  const [allPostComments, setAllPostComments] = useState(["This post has no comments"]);
+    const [post, setPost] = useState({});
+    const [token, setToken] = useState(window.localStorage.getItem("token"));
+    const [comment, setComment] = useState("");
+    const [allPostComments, setAllPostComments] = useState([
+        "This post has no comments",
+    ]);
 
-  const { id } = useParams();
+    const { id } = useParams();
 
-  const handleCommentChange = (event) => {
-    setComment(event.target.value);
-  };
+    const handleCommentChange = (event) => {
+        setComment(event.target.value);
+    };
 
-  const handleCommentSubmit = async () => {
-    let response = await fetch(`/posts/${id}`, {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({ comment: comment }),
-    });
-
-    const responseStatus = response.status;
-    response = await response.json();
-
-    if (responseStatus === 201) {
-      window.localStorage.setItem("token", response.token);
-      setToken(window.localStorage.getItem("token"));
-      
-      setPost((prevPost) => ({
-        ...prevPost,
-        comments: response.post.comments,
-      }));
-    }
-  };
-
-
-
-  useEffect(() => {
-    setAllPostComments(post.comments)
-  }, [post]);
-
-  useEffect(() => {
-    fetch(`/posts/${id}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((response) => response.json())
-      .then(async (data) => {
-        window.localStorage.setItem("token", data.token);
-        setToken(window.localStorage.getItem("token"));
-        setPost({
-          message: data.message,
-          author: data.author,
-          comments: data.comments,
+    const handleCommentSubmit = async () => {
+        let response = await fetch(`/posts/${id}`, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({ comment: comment }),
         });
-      });
-  }, []);
 
-  return (
-    <div>
-      <div>
-        <p data-cy="post">{post.message}</p>
-        <p data-cy="author">by {post.author}</p>
-      </div>
+        const responseStatus = response.status;
+        response = await response.json();
 
-      <div>
-        <ul data-cy="comments">
-          {allPostComments &&
-            allPostComments.map((commentObject, index) => {
-              return <li key={index}>{commentObject.comment}</li>;
-            })}
-        </ul>
-      </div>
-      <div>
-        <input
-          type="text"
-          value={comment}
-          onChange={handleCommentChange}
-          placeholder="Add a comment..."
-        />
-        <button onClick={handleCommentSubmit}>Submit Comment</button>
-      </div>
-    </div>
-  );
+        if (responseStatus === 201) {
+            window.localStorage.setItem("token", response.token);
+            setToken(window.localStorage.getItem("token"));
+
+            setPost((prevPost) => ({
+                ...prevPost,
+                comments: response.post.comments,
+            }));
+        }
+    };
+
+    useEffect(() => {
+        setAllPostComments(post.comments);
+    }, [post]);
+
+    useEffect(() => {
+        fetch(`/posts/${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((response) => response.json())
+            .then(async (data) => {
+                window.localStorage.setItem("token", data.token);
+                setToken(window.localStorage.getItem("token"));
+                setPost({
+                    message: data.message,
+                    author: data.author,
+                    comments: data.comments,
+                });
+            });
+    }, []);
+
+    return (
+        <div>
+            <div>
+                <p data-cy="post">{post.message}</p>
+                <p data-cy="author">by {post.author}</p>
+            </div>
+
+            <div>
+                <ul data-cy="comments">
+                    {allPostComments &&
+                        allPostComments.map((commentObject, index) => {
+                            return <li key={index}>{commentObject.comment}</li>;
+                        })}
+                </ul>
+            </div>
+            <div>
+                <input
+                    data-cy="commententry"
+                    type="text"
+                    value={comment}
+                    onChange={handleCommentChange}
+                    placeholder="Add a comment..."
+                />
+                <button onClick={handleCommentSubmit}>Submit Comment</button>
+            </div>
+        </div>
+    );
 };
 
 export default PostId;
