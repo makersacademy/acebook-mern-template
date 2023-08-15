4d1293680be53d0948f77e1d06f48aa45d6027db
Liam_Coles <lcoles1988@gmail.com>
2023-08-15 13:42:43 +0100

    changed test database name to match that used by ci test script

--- api/spec/mongodb_helper.js
+++ api/spec/mongodb_helper.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 
 beforeAll(function (done) {
-  mongoose.connect("mongodb://0.0.0.0/acebook_test", {
+  mongoose.connect("mongodb://0.0.0.0/tripPlanner_test", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
