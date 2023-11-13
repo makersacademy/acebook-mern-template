5a16653d1df8fa940c47e6e4375dd1329d0d2559
ClairePeng <clairepeng94@gmail.com>
2023-11-13 16:22:52 +0000

    adds preliminary new_post page -- paired with Sam

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import LoginForm from '../auth/LoginForm'
 import SignUpForm from '../user/SignUpForm'
+import NewPostForm from '../post_create/NewPostForm';
 import React, { useState } from 'react';
 import Feed from '../feed/Feed'
 import {
@@ -17,6 +18,8 @@ const App = () => {
           <Route path='/posts'  element={<Feed navigate={ useNavigate() }/>}/>
           <Route path='/login'  element={<LoginForm  navigate={ useNavigate() }/>}/>
           <Route path='/signup' element={<SignUpForm navigate={ useNavigate() }/>}/>
+          <Route path='/new_post' element={<NewPostForm navigate={ useNavigate() }/>}/>
+
         </Routes>
     );
 }
