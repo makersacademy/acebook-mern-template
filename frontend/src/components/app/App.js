13c39f3b26b534db45cf88372d307911b98a653e
Sam Ford <samueljamesford@googlemail.com>
2023-11-14 14:13:59 +0000

    Adds navigation bar component

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import LoginForm from '../auth/LoginForm'
 import SignUpForm from '../user/SignUpForm'
 import React, { useState } from 'react';
-import Feed from '../feed/Feed'
+import Feed from '../feed/Feed';
+import Navbar  from '../navbar/navbar';
 import {
   useNavigate,
   Routes,
@@ -15,6 +16,7 @@ const App = () => {
           <Route path='/posts'  element={<Feed navigate={ useNavigate() }/>}/>
           <Route path='/login'  element={<LoginForm  navigate={ useNavigate() }/>}/>
           <Route path='/signup' element={<SignUpForm navigate={ useNavigate() }/>}/>
+          <Route path='/navigate' element={<Navbar navigate={ useNavigate() }/>}/>
         </Routes>
     );
 }
