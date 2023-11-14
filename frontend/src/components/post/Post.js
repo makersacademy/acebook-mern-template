e84d34f08fe94dbc76a14646a6385c216b3e9626
ClairePeng <clairepeng94@gmail.com>
2023-11-14 14:49:56 +0000

    adds formatting to src/post/Post.js to display user_id. display name to follow

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Post = ({post}) => {
   return(
-    <article data-cy="post" key={ post._id }>{ post.message }</article>
+    <article data-cy="post" key={ post._id }>
+      {/* change below to display name later */}
+      <h2 className='user-display-name'>{ post.user_id }</h2> 
+      <p className='post-message'>{ post.message }</p>
+    </article>
   )
 }
 
