fc0cc527c38c5072e23e052988cb1f7d40f121d7
Perran Thomas <perran.thomas@icloud.com>
2023-11-14 16:15:43 +0000

    Added typing an example display name to Cypress component test

--- frontend/src/components/auth/LoginForm.cy.js
+++ frontend/src/components/auth/LoginForm.cy.js
@@ -7,6 +7,7 @@ describe("Logging in", () => {
 
     cy.intercept('POST', '/tokens', { token: "fakeToken" }).as("loginRequest")
 
+    cy.get("#displayname").type("Perfect Person");
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("password");
     cy.get("#submit").click();
