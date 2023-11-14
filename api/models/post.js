1eaa0d433a18571ebbf40db1c606f7e34bccdc84
Catherine Russell <catherinerussell123@hotmail.co.uk>
2023-11-14 16:19:40 +0000

    adds user_id and date fields to the posts collection (hopefully)

--- api/models/post.js
+++ api/models/post.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-  message: String
+  message: String,
+
+  // Modification of posts schema to add user_id
+  // will need to get it from the token/login info when post is made... 
+  user_id: Number,
+
+  // modification to posts to add user_id - in theory will input current date and time when post is created
+  time_posted: Date
 });
 
 const Post = mongoose.model("Post", PostSchema);
