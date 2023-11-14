d57c4b31b2e45622efe4bbd7235c35daea2c15f5
ClairePeng <clairepeng94@gmail.com>
2023-11-14 16:30:37 +0000

    add type and ref for user_id

--- api/models/post.js
+++ api/models/post.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
   message: String,
-  user_id: String
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 });
 
 const Post = mongoose.model("Post", PostSchema);
