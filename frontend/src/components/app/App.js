69d69cf6bb8a690b5afb151476706b8669f0f119
Megan Folsom <mfolsom@gmail.com>
2023-11-14 11:22:24 +0000

    create a root and redirect root to login if user not logged in

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import LoginForm from '../auth/LoginForm'
 import SignUpForm from '../user/SignUpForm'
 import NewPostForm from '../post_create/NewPostForm';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Feed from '../feed/Feed'
 import {
   useNavigate,
@@ -11,17 +11,27 @@ import {
 } from "react-router-dom";
 
 const App = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = window.localStorage.getItem('token');
+    if (token) {
+      navigate('/posts');
+    } else {
+      navigate('/login');
+    }
+  }
+  );
 
   // Routes for each page of the website
   return (
-        <Routes>
-          <Route path='/posts'  element={<Feed navigate={ useNavigate() }/>}/>
-          <Route path='/login'  element={<LoginForm  navigate={ useNavigate() }/>}/>
-          <Route path='/signup' element={<SignUpForm navigate={ useNavigate() }/>}/>
-          <Route path='/new_post' element={<NewPostForm navigate={ useNavigate() }/>}/>
-
-        </Routes>
-    );
+    <Routes>
+      <Route path='/' element={<LoginForm navigate={useNavigate()} />} />
+      <Route path='/posts' element={<Feed navigate={useNavigate()} />} />
+      <Route path='/login' element={<LoginForm navigate={useNavigate()} />} />
+      <Route path='/signup' element={<SignUpForm navigate={useNavigate()} />} />
+      <Route path='/new_post' element={<NewPostForm navigate={useNavigate()} />} />
+    </Routes>
+  );
 }
 
 export default App;
