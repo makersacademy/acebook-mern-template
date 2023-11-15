7e5d28f78be79f269a0811c593875d9855419958
Zahra Hassan <zahrajeylani@icloud.com>
2023-11-15 11:42:33 +0000

    auth controller test updated

--- api/spec/controllers/authentication.spec.js
+++ api/spec/controllers/authentication.spec.js
@@ -1,43 +1,46 @@
-// const app = require("../../app");
-// const request = require("supertest");
-// require("../mongodb_helper");
-// const User = require('../../models/user');
+const app = require("../../app");
+const request = require("supertest");
+require("../mongodb_helper");
+const User = require('../../models/user');
 
-// describe("/tokens", () => {
-//   beforeAll(async () => {
-//     const user = new User({ email: "test@test.com", password: "12345678" })
-describe("",() => {
-    test("", () => {})
-}) 
-//     // We need to use `await` so the
-//     // "beforeAll" setup function waits for the
-//     // asynchronous user.save() to be done before exiting.
-//     // Otherwise, the tests below
-//     // could run without the user being actually saved.
-//     // This could cause tests to fail inconsistently.
-//     await user.save()
-//   });
+describe("/tokens", () => {
+  beforeAll(async () => {
+    const user = new User({ 
+        email: "test@gmail.com",
+        password: "password",
+        username: "TestUser",
+        followers: [],
+        photograph: "",
+        posts: [],
+        comments: []
+      });
 
-//   afterAll(async () => {
-//     await User.deleteMany({})
-//   })
+    await user.save();
+  });
 
-//   test("a token is returned when creds are valid", async () => {
-//     let response = await request(app)
-//       .post("/tokens")
-//       .send({email: "test@test.com", password: "12345678"})
-//     expect(response.status).toEqual(201)
-//     expect(response.body.token).not.toEqual(undefined)
-//     expect(response.body.message).toEqual("OK")
-//   })
+  afterAll(async () => {
+    await User.deleteMany({})
+  })
+  it("", () => {})
 
 
-//   test("a token is not returned when creds are invalid", async () => {
-//     let response = await request(app)
-//       .post("/tokens")
-//       .send({email: "test@test.com", password: "1234"})
-//     expect(response.status).toEqual(401)
-//     expect(response.body.token).toEqual(undefined)
-//     expect(response.body.message).toEqual("auth error")
-//   })
-// })
+  test("a token is returned when creds are valid", async () => {
+    let response = await request(app)
+      .post("/tokens")
+      .send({email: "test@gmail.com", password: "password"})
+    expect(response.status).toEqual(201)
+    expect(response.body.token).not.toEqual(undefined)
+    expect(response.body.message).toEqual("OK")
+  })
+
+
+  test("a token is not returned when creds are invalid", async () => {
+    let response = await request(app)
+      .post("/tokens")
+      .send({email: "test@test.com", password: "1234"})
+    expect(response.status).toEqual(401)
+    expect(response.body.token).toEqual(undefined)
+    expect(response.body.message).toEqual("auth error")
+  })
+
+})
