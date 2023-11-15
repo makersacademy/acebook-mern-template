72096a678dc65acec4fde372e6a059d23d559c8b
Catherine Russell <catherinerussell123@hotmail.co.uk>
2023-11-15 11:28:19 +0000

    updates post model so that user_id is added and refers to Users collection

--- api/models/post.js
+++ api/models/post.js
@@ -1,14 +1,13 @@
+// const { Schema } = require("mongoose")
+
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
   message: String,
 
   // Modification of posts schema to add user_id
-  // will need to get it from the token/login info when post is made... 
-  user_id: Number,
 
-  // modification to posts to add user_id - in theory will input current date and time when post is created
-  time_posted: Date
+  user_id: {type: mongoose.Types.ObjectId, ref: 'Users'},
 });
 
 const Post = mongoose.model("Post", PostSchema);
