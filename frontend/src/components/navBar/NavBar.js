2462dc2e6e3c8ebf881ab2176f7903e48c97e5c8
David O'Donnell <dave@soundmotives.net>
2023-11-16 13:11:19 +0000

    NavBar and NavBar tests working

--- frontend/src/components/navBar/NavBar.js
+++ frontend/src/components/navBar/NavBar.js
@@ -4,34 +4,20 @@ import { useNavigate } from 'react-router-dom';
 const NavBar = () => {
   const navigate = useNavigate();
 
-  const navigateTo = (path) => {
+  const urlTo = (path) => {
     navigate(path);
   };
 
   return(
     <nav data-cy="navbar">
-      <h2 data-cy="header">Acebook</h2>
-      <button data-cy="home-button" onClick={() => navigateTo('/feed')}>Home</button>
-      <button data-cy="profile-button" onClick={() => navigateTo('/profile')}>Profile</button>
-      <button data-cy="search-button" onClick={() => navigateTo('/search')}>Search</button>
-      <button data-cy="logout-button" onClick={() => navigateTo('/login')}>Log Out</button>
+      <h2 id="header">Acebook</h2>
+      <button data-cy="home-button" onClick={() => urlTo('/feed')}>Home</button>
+      {/* should this be /home instead? */}
+      <button data-cy="profile-button" onClick={() => urlTo('/profile')}>Profile</button>
+      <button data-cy="search-button" onClick={() => urlTo('/search')}>Search</button>
+      <button data-cy="logout-button" onClick={() => urlTo('/login')}>Log Out</button>
 
     </nav>
-
-// <nav data-cy="navbar">
-// <div className="nav-left">
-//   <button data-cy="home-button" onClick={() => navigateTo('/feed')}>Home</button>
-// </div>
-// <div className="nav-center">
-//   <h2 data-cy="header">Acebook</h2>
-// </div>
-// <div className="nav-right">
-//   <button data-cy="profile-button" onClick={() => navigateTo('/profile')}>Profile</button>
-//   <button data-cy="search-button" onClick={() => navigateTo('/search')}>Search</button>
-//   <button data-cy="logout-button" onClick={() => navigateTo('/login')}>Log Out</button>
-// </div>
-// </nav>
-
   )
 }
 
