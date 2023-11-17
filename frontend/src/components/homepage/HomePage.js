079b8c2b1f25ebaa7db9bf4dfc187d416fa7212f
Catherine Russell <catherinerussell123@hotmail.co.uk>
2023-11-17 15:41:46 +0000

    adds links to buttons

--- frontend/src/components/homepage/HomePage.js
+++ frontend/src/components/homepage/HomePage.js
@@ -1,9 +1,26 @@
-import React from "react;"
+import React from "react";
+import { useNavigate, } from "react-router-dom";
+
 
 const HomePage = () => {
+	const navigate = useNavigate()
+	const handleLoginClick = () => {
+		navigate('/login')
+	}
+
+	const handleSignUpClick = () => {
+		navigate('/signup')
+	}
     return (
-        <>
-        <p>This is a test</p>
-        </>
-    )
+		<>
+		<h1 className="welcome-banner" style={{ textAlign: "center" }} > Welcome to AceBook</h1>
+		<div>
+				<button onClick={handleLoginClick} id="login-button" >Log in</button> <br></br>
+				<button onClick={handleSignUpClick} id="signup-button">Create an account</button>
+
+		</div>
+		</>
+)
 }
+
+export default HomePage;
