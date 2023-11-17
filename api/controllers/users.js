2ca46ac88a408f57d0fa81061b6c11841c21c90f
Zahra Hassan <zahrajeylani@icloud.com>
2023-11-17 16:27:29 +0000

    email authentication

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,7 +1,21 @@
 const User = require("../models/user");
 
 const UsersController = {
-  Create: (req, res) => {
+  Create: async (req, res) => {
+    const { email, password, retypePassword, firstName, lastName } = req.body;
+
+    if (!firstName || !lastName || !email || !password || !retypePassword) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: 'Email is already in use' });
+    }
+
+
+
     const user = new User(req.body);
     user.save((err) => {
       if (err) {
@@ -13,7 +27,7 @@ const UsersController = {
   },
 };
 
-// Check if user exists with email 
+// stop user from signing up with existing email 
 // fill all fields to signup
 // password conditions, password match 
 // return 400 error
--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -3,18 +3,25 @@ import React, { useState } from 'react';
 
 const SignUpForm = ({ navigate }) => {
 
-  const [FirstName, setFirstName] = useState("");
-  const [LastName, setLastName] = useState("");
-  const [Email, setEmail] = useState("");
-  const [Password, setPassword] = useState("");
-  const [RetypePassword, setRetypePassword] = useState("");
-  
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [retypePassword, setRetypePassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [emptyFieldsError, setEmptyFieldsError] = useState("");
   
 
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
+    if (!firstName || !lastName || !email || !password || !retypePassword) {
+      setErrorMessage("All fields must be filled"); 
+      return;
+    } else {
+      setEmptyFieldsError("");
+    } 
 
 
     fetch( 'api/users', {
@@ -22,16 +29,25 @@ const SignUpForm = ({ navigate }) => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ firstName: FirstName, lastName: LastName, email: Email, password: Password, retypePassword: RetypePassword })
+      body: JSON.stringify({ firstName, lastName, email, password, retypePassword })
     })
       .then(response => {
         if(response.status === 201) {
-          navigate('/login')
+          navigate('/login');
+        } else if (response.status === 400) {
+          return response.json();
         } else {
           navigate('/signup')
         }
       })
-  }
+      .then(data => {
+        if (data && data.message === 'Email is already in use') {
+          setErrorMessage('Email is already registered');
+        } else {
+          setErrorMessage('');
+        }
+      });
+  };
 
 
   const handleFirstNameChange = (event) => {
@@ -57,12 +73,14 @@ const SignUpForm = ({ navigate }) => {
     return (
       <form onSubmit={handleSubmit}>
         <h2>Sign Up</h2> 
-          <input placeholder="First Name" id="First Name" type='text' value={ FirstName } onChange={handleFirstNameChange} />
-          <input placeholder="Last Name" id="Last Name" type='text' value={ LastName } onChange={handleLastNameChange} />
-          <input placeholder="Email" id="Email" type='text' value={ Email } onChange={handleEmailChange} />
-          <input placeholder="Password" id="Password" type='text' value={ Password } onChange={handlePasswordChange} />
-          <input placeholder="Retype Password" id="RetypePassword" type='text' value={ RetypePassword } onChange={handleRetypePasswordChange} />
+          <input placeholder="First Name" id="First Name" type='text' value={ firstName } onChange={handleFirstNameChange} />
+          <input placeholder="Last Name" id="Last Name" type='text' value={ lastName } onChange={handleLastNameChange} />
+          <input placeholder="Email" id="Email" type='text' value={ email } onChange={handleEmailChange} />
+          <input placeholder="Password" id="Password" type='text' value={ password } onChange={handlePasswordChange} />
+          <input placeholder="Retype Password" id="RetypePassword" type='text' value={ retypePassword } onChange={handleRetypePasswordChange} />
           <input role='submit' id='submit' className='primary-btn' type="submit" value="Sign Up" />
+          {emptyFieldsError && <p style={{ color: 'red' }}>{emptyFieldsError}</p>}
+          {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
           <p>Already have an account? <a href="/Login">Login</a></p>
       </form>
     );
