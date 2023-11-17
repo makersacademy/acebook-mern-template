25c9694848778d363292ea7f1635622e8f5be796
Piotr Surowiec <piotrek.scgn@gmail.com>
2023-11-17 11:12:47 +0000

    Updates Post object

--- api/models/post.js
+++ api/models/post.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema({
   content: { type: String, required: true },
   created: { type: Date, default: Date.now() },
-  likes: Number,
+  likes: [{
+    user: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
+    liked: {type: Boolean, required: true}
+  }],
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   comments: [{type: mongoose.Schema.Types.ObjectId, ref: "Comment"}]
 });
