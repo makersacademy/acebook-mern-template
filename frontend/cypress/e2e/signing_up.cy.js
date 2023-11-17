90abf7216aa9458cdc5f3061d0d5427d007830b7
xAmiBa <aminaba666@gmail.com>
2023-11-17 15:40:48 +0000

    password validation completed and handled fully in fronend - popup message

--- frontend/cypress/e2e/signing_up.cy.js
+++ frontend/cypress/e2e/signing_up.cy.js
@@ -3,18 +3,19 @@ describe("Signing up", () => {
     cy.visit("/signup");
     cy.get("#username").type("James Bond");
     cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
+    cy.get("#password").type("password1!");
+    cy.get("#password_confirmation").type("password1!");
     cy.get("#submit").click();
 
     cy.url().should("include", "/choose-avatar");
   });
 
-  it("with missing password, redirects to '/signup'", () => {
+  it("with missing password redirect to signup", () => {
     cy.visit("/signup");
     cy.get("#username").type("James Bond");
     cy.get("#email").type("someone@example.com");
     cy.get("#submit").click();
-
+    
     cy.url().should("include", "/signup");
   });
 
@@ -33,5 +34,17 @@ describe("Signing up", () => {
     cy.get("#submit").click();
 
     cy.url().should("include", "/signup");
+  });
+
+  it("with invalid password, shows a popup message'", () => {
+    cy.visit("/signup");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#password_confirmation").type("password");
+    cy.get("#submit").click();
+
+    cy.on('window:confirm', message => {
+      expect(message).t.equal("Password must be at least 8 characters long\nPassword must contain at least one number and special sign\n")
+    })
   });
 });
