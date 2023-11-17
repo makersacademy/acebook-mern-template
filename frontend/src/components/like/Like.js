351321239e85c6a7aa4f9d489fee142c6ce5ad63
Onuora <onuora.wright@gmail.com>
2023-11-17 11:16:12 +0000

    Code for LikeButton component in Like.js file

---
+++ frontend/src/components/like/Like.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LikeButton = () => {
+  return (
+    <div className="like-container">
+      <i className="fas fa-thumbs-up"></i>
+    </div>
+  );
+};
+
+export default LikeButton;
