e083405ff9b94f492b90de60e0b01a51bcba1b01
Onuora <onuora.wright@gmail.com>
2023-11-20 12:47:32 +0000

    Updated Like.js to display the like count from the length of the like list in post

--- frontend/src/components/like/Like.js
+++ frontend/src/components/like/Like.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 const Like = (post) => {
-  console.log(post)
   return (
     <div className="flex-align-vertical post-like-gap">
       <img src='thumb-icon.png' className='like-btn' alt='thumb' />
-      {/* <span class="like-count" data-cy="post-likes">{post.likes.length}</span> */}
+      <span class="like-count" data-cy="post-likes">{post.post.likes.length}</span>
     </div>
   );
 };
 
 export default Like;
-
