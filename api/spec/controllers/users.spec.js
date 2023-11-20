2469991141f6ac7833c8423ca7e1807cecf3d814
Perran Thomas <perran.thomas@icloud.com>
2023-11-20 17:32:02 +0000

    Wrote two tests, one for checking when the token is present, and one for when it is absent.

--- api/spec/controllers/users.spec.js
+++ api/spec/controllers/users.spec.js
@@ -118,8 +118,8 @@ describe("/users", () => {
     });
   });
 
-  describe("GET, when token is missing", () => {
-    it("does not return a user", async () => {
+  describe("GET, when token is present", () => {
+    it("returns a user if there is a match", async () => {
       // Define info for two users.
       const userInfo1 = {
         displayName: "User One",
@@ -137,13 +137,46 @@ describe("/users", () => {
       await createTestUser(userInfo2);
       const user2 = await getMostRecentlyCreatedUser();
       // Log in as User One.
-      let token = JWT.sign({
-        user_id: user.id,
-        // Backdate this token of 5 minutes
-        iat: Math.floor(Date.now() / 1000) - (5 * 60),
-        // Set the JWT token to expire in 10 minutes
-        exp: Math.floor(Date.now() / 1000) + (10 * 60)
-      }, secret);
+      let token1 = logInAndGetTokenAs(user1);
+      // As User One, search for User Two.
+      let response = await request(app)
+        .get("/users")
+        .set("Authorization", `Bearer ${token1}`)
+        .send({ token: token1, userIdToFind: user2.id });
+      let userFound = response.body.user;
+      expect(userFound.displayName).toEqual("User Two");
+      expect(userFound.email).toEqual("user-two@example.com");
     });
   });
+
+  describe("GET, when token is missing", () => {
+    it("returns no user", async () => {
+      // Define info for two users.
+      const userInfo1 = {
+        displayName: "User One",
+        email: "user-one@example.com",
+        password: "11111111",
+      };
+      const userInfo2 = {
+        displayName: "User Two",
+        email: "user-two@example.com",
+        password: "22222222",
+      };
+      // Create the two users.
+      await createTestUser(userInfo1);
+      const user1 = await getMostRecentlyCreatedUser();
+      await createTestUser(userInfo2);
+      const user2 = await getMostRecentlyCreatedUser();
+      // Log in as User One.
+      let token1 = logInAndGetTokenAs(user1);
+      // As User One, search for User Two.
+      let response = await request(app)
+        .get("/users")
+        // "Forget" to set token:
+        /*.set("Authorization", `Bearer ${token1}`)*/
+        .send({ token: token1, userIdToFind: user2.id });
+      let userFound = response.body.user;
+      expect(userFound).toEqual(undefined);
+    })
+  })
 });
