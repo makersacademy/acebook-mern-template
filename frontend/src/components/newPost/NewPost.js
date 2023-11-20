5835bb9d0aa4bdb66451b94423dc6a8dc5296529
Ray Brown <raybrown.dev@gmail.com>
2023-11-20 16:03:59 +0000

    add image to new post

--- frontend/src/components/newPost/NewPost.js
+++ frontend/src/components/newPost/NewPost.js
@@ -5,11 +5,15 @@ import './NewPost.css';
 const NewPost = ({ user_id }) => {
     const [message, setMessage] = useState("");
     const [token, setToken] = useState(window.localStorage.getItem("token"));
-    // TODO  const [image, setImage] = useState("");
+    //ADDED
+    const [selectedImage, setSelectedImage] = useState(null); 
+    const [characterCount, setCharacterCount] = useState(0);
 
     const handleSubmit = async (event) => { 
         event.preventDefault(); // prevent default stops the page from reloading
         setMessage(""); //clears message once sent
+        setSelectedImage(null)
+
 
     fetch('/posts', {
         method: 'post',
@@ -31,27 +35,61 @@ const NewPost = ({ user_id }) => {
 
     const handleMessageChange = (event) => {
         setMessage(event.target.value);
+        const inputValue = event.target.value;
+        setCharacterCount(inputValue.length);
 
     };
 
-    // const handleImageChange = (event) => {
-    //     setImage(event.target.value);
-    // };
+    const handleImageChange = (event) => {
+        setSelectedImage(event.target.files[0]);
+    };
+
+    const handleReset = () => { 
+        window.location.reload();
+        };
+
 
     return(
         <>
             <div className="new_post">
+                
+            {/* IMAGE PREVIEW */}
+                {selectedImage && (
+                    <div class="image-preview">
+                    <img
+                        alt="not found"
+                        width={"250px"}
+                        src={URL.createObjectURL(selectedImage)}
+                    />
+                    <br />
+                    <button onClick={() => setSelectedImage(null)}>Remove</button>
+                    </div>
+                )}
+            {/* IMAGE PREVIEW */}
+
                 <form onSubmit={handleSubmit}>
-                <textarea placeholder="write your post"
-                className="new-post-message" 
-                rows="4" cols="50"
-                value={message} 
-                onChange={handleMessageChange}
-                />
+                    <textarea 
+                    placeholder="write your post"
+                    className="new-post-message"
+                    maxlength="300"
+                    rows="4" cols="50"
+                    value={message} 
+                    onChange={handleMessageChange}
+                    />
                 <br />
-                <input type="url" id="post-image" name="post-image" placeholder="add image" />
+                <div id="the-count">
+                    <span id="current">{characterCount}</span>
+                    <span id="maximum">/ 300</span>
+                </div>
+
+                {/* IMAGE UPLOAD */}
+                <input type="file" 
+                        id="postImage" 
+                        name="post-image" 
+                        onChange={handleImageChange}/>
+                {/* IMAGE UPLOAD */}
                 <br />
-                <input id='submit' type="submit" value="Post" className='custom-btn'/>
+                    <input id='submit' type="submit" value="Post" className='custom-btn' onClick={handleReset}/>
                 </form>
             </div>
         </>
@@ -62,3 +100,4 @@ const NewPost = ({ user_id }) => {
 
 
 export default NewPost;
+
