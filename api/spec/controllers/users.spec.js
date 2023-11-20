cb61fae9a646ae8a6eaee7e464cb0ae6f856cdfe
Harry Rhodes <hjtrhodes@gmail.com>
2023-11-20 21:31:37 +0000

    Perran's changes user.spec.js merged before commit

--- api/spec/controllers/users.spec.js
+++ api/spec/controllers/users.spec.js
@@ -30,34 +30,12 @@ const logInAndGetTokenAs = (user) => {
 };
 
 describe("/users", () => {
-<<<<<<< HEAD
-    beforeEach( async () => {
-=======
   beforeEach(async () => {
->>>>>>> 7742d4ca051bcdeba9c835babbdbc362fcc1a68f
     await User.deleteMany({});
     });
 
     describe("POST, when display name, email, and password are provided", () => {
     test("the response code is 201", async () => {
-<<<<<<< HEAD
-        let response = await request(app)
-        .post("/users")
-        .send({displayName: "Poppy Python", email: "poppy@email.com", password: "1234"})
-        expect(response.statusCode).toBe(201)
-    })
-
-    test("a user is created", async () => {
-        await request(app)
-        .post("/users")
-        .send({displayName: "Scarlett Scala", email: "scarlett@email.com", password: "1234"})
-        let users = await User.find()
-        let newUser = users[users.length - 1]
-        expect(newUser.displayName).toEqual("Scarlett Scala")
-        expect(newUser.email).toEqual("scarlett@email.com")
-    })
-    })
-=======
       const userInfo = {
         displayName: "Poppy Python",
         email: "poppy@email.com",
@@ -79,26 +57,9 @@ describe("/users", () => {
       expect(newUser.email).toEqual("scarlett@email.com");
     });
   });
->>>>>>> 7742d4ca051bcdeba9c835babbdbc362fcc1a68f
 
     describe("POST, when password is missing", () => {
     test("response code is 400", async () => {
-<<<<<<< HEAD
-        let response = await request(app)
-        .post("/users")
-        .send({displayName: "Skye Swift", email: "skye@email.com"})
-        expect(response.statusCode).toBe(400)
-    });
-
-    test("does not create a user", async () => {
-        await request(app)
-        .post("/users")
-        .send({displayName: "Skye Swift", email: "skye@email.com"})
-        let users = await User.find()
-        expect(users.length).toEqual(0)
-    });
-    })
-=======
       const userInfo = {
         displayName: "Skye Swift",
         email: "skye@email.com",
@@ -116,44 +77,9 @@ describe("/users", () => {
       expect(await numberOfExistingUsers()).toEqual(0);
     });
   });
->>>>>>> 7742d4ca051bcdeba9c835babbdbc362fcc1a68f
 
     describe("POST, when display name is missing", () => {
     test("response code is 400", async () => {
-<<<<<<< HEAD
-        let response = await request(app)
-        .post("/users")
-        .send({email: "james@email.com", password: "1234"})
-        expect(response.statusCode).toBe(400)
-    });
-
-    test("does not create a user", async () => {
-        await request(app)
-        .post("/users")
-        .send({email: "james@email.com", password: "1234"})
-        let users = await User.find()
-        expect(users.length).toEqual(0)
-    })
-    })
-
-    describe("POST, when email is missing", () => {
-    test("response code is 400", async () => {
-        let response = await request(app)
-        .post("/users")
-        .send({displayName: "Emily Erlang", password: "1234"})
-        expect(response.statusCode).toBe(400)
-    });
-
-    test("does not create a user", async () => {
-        await request(app)
-        .post("/users")
-        .send({displayName: "Emily Erlang", password: "1234"})
-        let users = await User.find()
-        expect(users.length).toEqual(0)
-    });
-    })
-})
-=======
       const userInfo = {
         email: "james@email.com",
         password: "1234",
@@ -254,4 +180,3 @@ describe("/users", () => {
     })
   })
 });
->>>>>>> 7742d4ca051bcdeba9c835babbdbc362fcc1a68f
