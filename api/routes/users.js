5f8b6655251ab075196df2525f86d23e4600efd5
Perran Thomas <perran.thomas@icloud.com>
2023-11-22 08:40:50 +0000

    Add GET /users route which calls UsersController.Index

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,6 +5,7 @@ const UsersController = require("../controllers/users");
 
 router.post("/", UsersController.Create);
 router.get("/display-name", UsersController.FindSingleDisplayNameById);
+router.get("/", UsersController.Index);
 
   // NEED TO RETURN TO THIS (TODO)
   // router.get("/users", UsersController.FindAll);
