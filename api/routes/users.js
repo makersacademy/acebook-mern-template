17c3c4323219702b564fd8c8c800b503b0ee0a9b
Rachel Roberts <rachelroberts_sw@yahoo.co.uk>
2023-11-23 13:12:58 +0000

    UPDATE - saved users.js in api > routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,22 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const UsersController = require("../controllers/users");
-
 // creates PostsController in order to use getPostsByUsername
 const PostsController = require("../controllers/posts");
 
 router.post("/", UsersController.Create);
 router.get("/", UsersController.GetUserEmails);
-
-
-
-// new router for backend of frontend users/my_account and users/userProfileFeed for GET request to obtain user data based on user_id
-// router.get("/data/:user_id", UsersController.DisplayUserData);
-// localhost:8080/users/data/:user_id/data/:user_id
-
-
-// router.get("/profile/:user_id", PostsController.FindPostsByUserId);
-
 // new router for backend users/avatar for POST request changing the chosen avatar
 router.post("/avatar", UsersController.ChangeAvatar);
 
