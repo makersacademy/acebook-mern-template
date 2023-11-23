c5a3f7f36e8d30aba8059412d8c7516dd7856886
Catherine Russell <catherinerussell123@hotmail.co.uk>
2023-11-23 12:56:25 +0000

    adds header to comment

--- frontend/src/components/comment/Comment.js
+++ frontend/src/components/comment/Comment.js
@@ -1,10 +1,13 @@
 // when doing this, pass the post ID through to the backend !! comment.post_id or post._id
+import MessageHeader from "../messageHeader/MessageHeader";
 
 import React from "react";
 
 const Comment = ({ comment }) => {
   return (
     <article data-cy="comment" key={comment._id}>
+      <MessageHeader user_id={comment.user_id}/>
+
       {comment.message}
     </article>
   );
