27f04db354280da7f6c1b47283ab59bfa12c04c6
xAmiBa <aminaba666@gmail.com>
2023-11-23 19:06:24 +0000

    one variable name changed

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -54,7 +54,7 @@ const Feed = ({ navigate }) => {
       // If post creation is successful, proceed to file upload
       if (PostPostResponse.ok) {
         const data = await PostPostResponse.json();
-        newPostId = data.post_id;
+        newPostId = data._id;
         window.localStorage.setItem("token", data.token);
         setToken(window.localStorage.getItem("token"));
         setPosts(data.posts);
