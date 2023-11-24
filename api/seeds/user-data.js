4bc2394f71c5a42c42c21a661899d3dddadcba26
Piotr Surowiec <piotrek.scgn@gmail.com>
2023-11-24 12:32:26 +0000

    Seed Users updated

--- api/seeds/user-data.js
+++ api/seeds/user-data.js
@@ -1,14 +1,15 @@
 const USERS = [
-    { email: "Alice@email.com", password: "P@ssw0rd" },
-    { email: "Bob@email.com", password: "Secret123" },
-    { email: "Charlie@email.com", password: "SecurePwd" },
-    { email: "David@email.com", password: "Passw0rd!123" },
-    { email: "Eve@email.com", password: "EvePwd@2023" },
-    { email: "Frank@email.com", password: "Frankie123" },
-    { email: "Grace@email.com", password: "GracePass" },
-    { email: "Henry@email.com", password: "H@nk789" },
-    { email: "Ivy@email.com", password: "IvyPassword" },
-    { email: "Jack@email.com", password: "J@ckPass456" },
-  ];
+  { firstName: "Alice", lastName: "Doe", email: "Alice@email.com", password: "P@ssw0rd" },
+  { firstName: "Bob", lastName: "Smith", email: "Bob@email.com", password: "Secret123" },
+  { firstName: "Charlie", lastName: "Brown", email: "Charlie@email.com", password: "SecurePwd" },
+  { firstName: "David", lastName: "Jones", email: "David@email.com", password: "Passw0rd!123" },
+  { firstName: "Eve", lastName: "Johnson", email: "Eve@email.com", password: "EvePwd@2023" },
+  { firstName: "Frank", lastName: "Miller", email: "Frank@email.com", password: "Frankie123" },
+  { firstName: "Grace", lastName: "Taylor", email: "Grace@email.com", password: "GracePass" },
+  { firstName: "Henry", lastName: "Martin", email: "Henry@email.com", password: "H@nk789" },
+  { firstName: "Ivy", lastName: "Clark", email: "Ivy@email.com", password: "IvyPassword" },
+  { firstName: "Jack", lastName: "Williams", email: "Jack@email.com", password: "J@ckPass456" },
+];
+
 
   module.exports = USERS;
